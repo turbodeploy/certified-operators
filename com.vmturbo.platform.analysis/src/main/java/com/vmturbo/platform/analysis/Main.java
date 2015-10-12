ddcd095f51df05986453fe1ac0d8c86895cc8f24
vaptistis_anogeianakis <vaptistis_anogeianakis@3bb2320e-05c5-11dd-9ba1-b9e69423245e>
2015-10-12 12:19:47 +0000

    Revert "Made some changes to pom and main to integrate with and test Checker Framework."
    This reverts previous commit.
    
    After first examination, the Checker Framework does not seem to be mature
    enough for production use. Most of its warnings are good, but I've found
    incorrect suggestions for pure annotations, cases where a non-deterministic
    method was allowed to be marked deterministic, and some cases where there
    seemed to be no way to annotate things so that a lambda expression would be
    assigned to a variable without warnings. Also the IGJ Checker could not be made
    to work at all.
    
    I will continue to annotate code with Checker Framework annotations for
    documentation purposes and in the hope it will improve with time. I may also
    continue using the Eclipse plug-in to check the annotations.
    
    Reviewed at https://rbcommons.com/s/VMTurbo/r/5938/
    
    git-svn-id: https://vcs.turbonomic.com/svn/analysis/trunk@10 3bb2320e-05c5-11dd-9ba1-b9e69423245e

--- com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/Main.java
+++ com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/Main.java
@@ -4,12 +4,6 @@ import static com.google.common.base.Preconditions.*;
 
 import java.util.function.Supplier;
 
-import org.checkerframework.checker.javari.qual.Mutable;
-import org.checkerframework.checker.javari.qual.ReadOnly;
-import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.dataflow.qual.Pure;
-import org.checkerframework.dataflow.qual.SideEffectFree;
-
 /**
  * The Main class for the application.
  *
@@ -21,40 +15,9 @@ import org.checkerframework.dataflow.qual.SideEffectFree;
  * </p>
  */
 public final class Main {
-    @SideEffectFree Main() {}
-
-    public static int y = 0;
-
-    private static class Test {
-        public double x;
-
-        @Pure int foo(@ReadOnly Test this) {
-            y = 2;
-            x = 1;
-            return y;
-        }
-    }
 
     public static void main(String[] args) {
-        @NonNull String s = null;
-        //final double x = 0;
-        //x = 1; // error
-        //final Test ft = null;
-        //ft = new Test(); // error
-        @ReadOnly Test t = null;
-        t = new Test();
-        t.x = 1;
-        Test[][] table = new Test[][]{new Test[10],new Test[5]};
-        @ReadOnly Test @ReadOnly [] @ReadOnly [] test = null;
-        test = table;
-        test[0] = new Test[3];
-        test[0][0] = new Test();
-        test[0][0].x = 5.0;
-        test[0][0].foo();
-
-        System.out.println(t.x);
-
-        Supplier<@Mutable String> greeter = () -> "Hello Java 8 world!!!";
+        Supplier<String> greeter = () -> "Hello Java 8 world!!!";
         System.out.println(greeter.get());
     }
 
@@ -64,7 +27,6 @@ public final class Main {
      * @param n The natural number whose factorial should be returned.
      * @return n!
      */
-    @Pure
     public static long factorial(int n) {
         checkArgument(n >= 0);
 
