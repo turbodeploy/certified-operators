9811edbd14533f25674eb78b9b70d36ddd384adb
vaptistis_anogeianakis <vaptistis_anogeianakis@3bb2320e-05c5-11dd-9ba1-b9e69423245e>
2015-10-12 12:19:33 +0000

    Made some changes to pom and main to integrate with and test Checker Framework.
    Different command-line options and checkers where tested.
    The pom.xml uses an environment variable (that has to be visible from within
    Eclipse for the launch configuration to work) and an external file with
    compiler command-line options. Both are documented in Checker Framework's site,
    but are not considered good practices.
    
    Reviewed at https://rbcommons.com/s/VMTurbo/r/5937/
    
    git-svn-id: https://vcs.turbonomic.com/svn/analysis/trunk@9 3bb2320e-05c5-11dd-9ba1-b9e69423245e

--- com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/Main.java
+++ com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/Main.java
@@ -4,6 +4,12 @@ import static com.google.common.base.Preconditions.*;
 
 import java.util.function.Supplier;
 
+import org.checkerframework.checker.javari.qual.Mutable;
+import org.checkerframework.checker.javari.qual.ReadOnly;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.dataflow.qual.Pure;
+import org.checkerframework.dataflow.qual.SideEffectFree;
+
 /**
  * The Main class for the application.
  *
@@ -15,9 +21,40 @@ import java.util.function.Supplier;
  * </p>
  */
 public final class Main {
+    @SideEffectFree Main() {}
+
+    public static int y = 0;
+
+    private static class Test {
+        public double x;
+
+        @Pure int foo(@ReadOnly Test this) {
+            y = 2;
+            x = 1;
+            return y;
+        }
+    }
 
     public static void main(String[] args) {
-        Supplier<String> greeter = () -> "Hello Java 8 world!!!";
+        @NonNull String s = null;
+        //final double x = 0;
+        //x = 1; // error
+        //final Test ft = null;
+        //ft = new Test(); // error
+        @ReadOnly Test t = null;
+        t = new Test();
+        t.x = 1;
+        Test[][] table = new Test[][]{new Test[10],new Test[5]};
+        @ReadOnly Test @ReadOnly [] @ReadOnly [] test = null;
+        test = table;
+        test[0] = new Test[3];
+        test[0][0] = new Test();
+        test[0][0].x = 5.0;
+        test[0][0].foo();
+
+        System.out.println(t.x);
+
+        Supplier<@Mutable String> greeter = () -> "Hello Java 8 world!!!";
         System.out.println(greeter.get());
     }
 
@@ -27,6 +64,7 @@ public final class Main {
      * @param n The natural number whose factorial should be returned.
      * @return n!
      */
+    @Pure
     public static long factorial(int n) {
         checkArgument(n >= 0);
 
