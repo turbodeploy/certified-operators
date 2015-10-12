d8ca7d2dc0343322d7214ef3b2a4a987a1d3e716
vaptistis_anogeianakis <vaptistis_anogeianakis@3bb2320e-05c5-11dd-9ba1-b9e69423245e>
2015-10-12 12:20:35 +0000

    Started working on a second version of the CommodityType.
    This takes into account the following facts:
    * The market doesn't need to know what the commodity type is, but rather
      whether two commodity types match.
    * The commodity type will have to be serialized to pass from the Mediation
      server to the Economy server anyway.
    * The Economy and Mediation servers may have a different class for commodity
      type or a different version of the same class.
    * It important that the internal representation of commodity types in the
      Economy is efficient, since it is used in time-critical operations.
    
    This code may be incomplete/erroneous and subject to change, but was committed
    so that other parts of the team may start using the interface.
    
    Reviewed at https://rbcommons.com/s/VMTurbo/r/5941/
    
    git-svn-id: https://vcs.turbonomic.com/svn/analysis/trunk@13 3bb2320e-05c5-11dd-9ba1-b9e69423245e

--- com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/economy/CommodityType.java
+++ com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/economy/CommodityType.java
@@ -1,5 +1,8 @@
 package com.vmturbo.platform.analysis.economy;
 
+import static com.vmturbo.platform.analysis.economy.NumericCommodityType.*;
+
+import org.checkerframework.checker.javari.qual.ReadOnly;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 /**
@@ -9,35 +12,81 @@ import org.checkerframework.checker.nullness.qual.NonNull;
  *  Together with the quality, this uniquely identifies a commodity sold from a single trader.
  * </p>
  */
-public enum CommodityType implements Comparable<CommodityType> {
-    // Enumerators
-    CPU("MHz"),
-    MEMORY("MB"),
-    STORAGE("MiB"),
-    VCPU("MHz"),
-    VMEMORY("MB"),
-    VSTORAGE("MiB"),
-
-    ACCESS(""),
-    CLUSTER(""),
-    SEGMENT("");
+public final class CommodityType implements Comparable<CommodityType> {
 
     // Fields
-    private final String unitOfMeasurement_;
+    private final String kind_; // the human readable kind of this commodity. e.g. CPU or Memory
+    private final String unitOfMeasurement_; // the unit of measurement used for commodities of this kind.
+    private final long numericalRepresentation_;
 
     // Constructors
-    private CommodityType(String unitOfMeasurement) {
+
+    /**
+     * Constructs a new CommodityType object with the specified properties.
+     *
+     * @param kind The kind of the new CommodityType. e.g. CPU or Memory.
+     * @param unitOfMeasurement The unit of measurement used to measure quantities of commodities of
+     *                          this kind.
+     * @param numericalRepresentation The numerical representation of the new commodity type.
+     */
+    CommodityType(@NonNull @ReadOnly String kind, @NonNull @ReadOnly String unitOfMeasurement, long numericalRepresentation) {
+        kind_ = kind;
         unitOfMeasurement_ = unitOfMeasurement;
+        numericalRepresentation_ = numericalRepresentation;
     }
 
     // Methods
 
+
     /**
-     * Returns the unit of measurement used to express amounts and capacities for commodities of this
+     * Returns the kind of {@code this} commodity type. e.g. Memory or CPU.
+     */
+    public @NonNull @ReadOnly String getKind(@ReadOnly CommodityType this) {
+        return kind_;
+    }
+
+    /**
+     * Returns the unit of measurement used to express quantities and capacities for commodities of this
      * type. e.g. MHz or MB
      */
-    public @NonNull String getUnit() {
+    public @NonNull @ReadOnly String getUnit(@ReadOnly CommodityType this) {
         return unitOfMeasurement_;
+    }
+
+    /**
+     * Returns the lower bound on the quality provided or requested.
+     */
+    public int getQualityLowerBound(@ReadOnly CommodityType this) {
+        return (int)extractQualityLowerBound(numericalRepresentation_);
+    }
+
+    /**
+     * Returns the upper bound on the quality provided or requested.
+     */
+    public int getQualityUpperBound(@ReadOnly CommodityType this) {
+        return (int)extractQualityUpperBound(numericalRepresentation_);
+    }
+
+    /**
+     * Returns the numerical representation of {@code this} commodity type, used for efficient operations.
+     */
+    long numericalRepresentation(@ReadOnly CommodityType this) {
+        return numericalRepresentation_;
+    }
+
+    public boolean isSatisfiedBy(@ReadOnly CommodityType this, @NonNull @ReadOnly CommodityType other) {
+        return isSatisfiedBy(this.numericalRepresentation_, other.numericalRepresentation_);
+    }
+
+    static boolean isSatisfiedBy(long requestedType, long providedType) {
+        return extractKind(requestedType) == extractKind(providedType)
+            && extractQualityLowerBound(requestedType) <= extractQualityUpperBound(providedType)
+            && extractQualityUpperBound(requestedType) >= extractQualityLowerBound(providedType);
+    }
+
+    @Override
+    public int compareTo(CommodityType other) {
+        return (int)(this.numericalRepresentation_ - other.numericalRepresentation_);
     }
 
 } // end CommodityType enumeration
---
+++ com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/economy/CommodityTypeFactory.java
@@ -0,0 +1,37 @@
+package com.vmturbo.platform.analysis.economy;
+
+import static com.vmturbo.platform.analysis.economy.NumericCommodityType.composeNumericalRepresentation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public final class CommodityTypeFactory {
+    // Fields
+    private Map<String,Short> numericalKinds = new TreeMap<String,Short>(); // the association between human
+        // readable strings representing the kind of commodity and numerical kinds used internally
+        // for performance.
+    private List<String> stringKinds = new ArrayList<String>(); // the inverse association between
+        // numerical kinds and human readable strings.
+
+    // Constructors
+    /**
+     * Constructs a new CommodityTypeFactory. CommodityTypes should usually be constructed from the
+     * same CommodityTypeFactory instance.
+     */
+    public CommodityTypeFactory()
+    {
+        // empty body
+    }
+
+    // Methods
+
+    public CommodityType create(String kind, String unitOfMeasurement, int lowerQualityBound, int upperQualityBound) {
+        if(!numericalKinds.containsKey(kind)) {
+            numericalKinds.put(kind, (short)stringKinds.size());
+            stringKinds.add(kind);
+        }
+        return new CommodityType(kind,unitOfMeasurement,composeNumericalRepresentation(numericalKinds.get(kind),lowerQualityBound,upperQualityBound));
+    }
+} // end class CommodityTypeFactory
