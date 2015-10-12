032acb6db00d3b40a218b35b3d4aeaf3bf5e1487
vaptistis_anogeianakis <vaptistis_anogeianakis@3bb2320e-05c5-11dd-9ba1-b9e69423245e>
2015-10-12 12:20:06 +0000

    Removed UnmodifiableCommoditySold and moved its code to CommoditySold.
    At least for now, I'm going to use @ReadOnly annotations to mark read-only
    interfaces to avoid the blow-up in the number of classes. In the future we may
    need to write those classes if there is no descent static analyzer that checks
    them, but at least we'll know how many of them we'll need.
    
    Reviewed at https://rbcommons.com/s/VMTurbo/r/5995/
    
    git-svn-id: https://vcs.turbonomic.com/svn/analysis/trunk@15 3bb2320e-05c5-11dd-9ba1-b9e69423245e

--- com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/economy/CommoditySold.java
+++ com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/economy/CommoditySold.java
@@ -1,11 +1,76 @@
 package com.vmturbo.platform.analysis.economy;
 
+import java.util.List;
+
+import org.checkerframework.checker.javari.qual.ReadOnly;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.dataflow.qual.Pure;
 
 /**
  * A commodity sold by a single {@link Trader}.
  */
-public interface CommoditySold extends UnmodifiableCommoditySold {
+public interface CommoditySold {
+    /**
+     * Returns an unmodifiable list of traders buying {@code this} commodity.
+     */
+    @Pure
+    @NonNull @ReadOnly List<@NonNull @ReadOnly Trader> getBuyers(@ReadOnly CommoditySold this);
+
+    /**
+     * Returns the (unique) trader selling {@code this} commodity.
+     */
+    @Pure
+    @NonNull @ReadOnly Trader getSeller(@ReadOnly CommoditySold this);
+
+    /**
+     * The {@link CommoditySoldSettings settings} controlling {@code this} commodity's behavior.
+     */
+    @Pure
+    @NonNull CommoditySoldSettings getSettings();
+
+    /**
+     * Returns the <b>type</b> of {@code this} commodity.
+     */
+    @Pure
+    @NonNull CommodityType getType();
+
+    /**
+     * Returns the <b>quality</b> of {@code this} commodity.
+     *
+     * <p>
+     *  A buyer interested in a commodity of a given quality, will not accept a commodity of a
+     *  lesser quality. e.g. A buyer may request a CPU of at least 8 cores.
+     * </p>
+     */
+    // double getQuality(); // this will probably replace the concept of key soon.
+
+    /**
+     * Returns the <b>key</b> of {@code this} commodity.
+     *
+     * <p>
+     *  A buyer interested in a commodity with a given key, will not accept a commodity with any
+     *  other key. e.g. This is used for access commodities.
+     * </p>
+     */
+    @Pure
+    long getKey();
+
+    /**
+     * Returns the <b>capacity</b> for {@code this} commodity.
+     */
+    @Pure
+    double getCapacity();
+
+    /**
+     * Returns whether {@code this} commodity is <b>thin</b> provisioned.
+     *
+     * <p>
+     *  This is related to <a href="https://en.wikipedia.org/wiki/Thin_provisioning">thin provisioning</a>.
+     * </p>
+     */
+    @Pure
+    boolean isThin();
+
     /**
      * Sets the value of the <b>capacity</b> field.
      *
