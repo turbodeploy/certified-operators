34fe22209890a163319dddb9a21c7c6960ecf664
vaptistis_anogeianakis <vaptistis_anogeianakis@3bb2320e-05c5-11dd-9ba1-b9e69423245e>
2015-10-27 18:46:12 +0000

    Tested the rest of Market methods except removeBuyerParticipation.
    * Coverage is 80% for instructions and 100% for branches.
    * The only untested method in Market is removeBuyerParticipation.
    
    * Still not sure what's the best way to test removeBuyerParticipation, or how
      to decouple it from Economy.
    
    Reviewed at https://rbcommons.com/s/VMTurbo/r/6393/
    
    git-svn-id: https://vcs.turbonomic.com/svn/analysis/trunk@51 3bb2320e-05c5-11dd-9ba1-b9e69423245e

--- com.vmturbo.platform.analysis/src/test/java/com/vmturbo/platform/analysis/economy/MarketTest.java
+++ com.vmturbo.platform.analysis/src/test/java/com/vmturbo/platform/analysis/economy/MarketTest.java
@@ -3,12 +3,11 @@ package com.vmturbo.platform.analysis.economy;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-
 import org.checkerframework.checker.javari.qual.ReadOnly;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -27,12 +26,24 @@ public class MarketTest {
     private static final CommoditySpecification A = new CommoditySpecification((short)0,0,0);
     private static final CommoditySpecification B = new CommoditySpecification((short)0,0,100);
     private static final CommoditySpecification C = new CommoditySpecification((short)1,0,100);
-    private static final Basket[] baskets = {
-        new Basket(),
-        new Basket(A),
-        new Basket(A,B),
-        new Basket(A,B,C)
-    };
+
+    private static final TraderWithSettings T0 = new TraderWithSettings(0, 0, TraderState.ACTIVE, new Basket());
+    private static final TraderWithSettings T0A = new TraderWithSettings(0, 0, TraderState.ACTIVE, new Basket(A));
+    private static final TraderWithSettings T0C = new TraderWithSettings(0, 0, TraderState.ACTIVE, new Basket(C));
+    private static final TraderWithSettings T0AB = new TraderWithSettings(0, 0, TraderState.ACTIVE, new Basket(A,B));
+    private static final TraderWithSettings T0AC = new TraderWithSettings(0, 0, TraderState.ACTIVE, new Basket(A,C));
+    private static final TraderWithSettings T1A = new TraderWithSettings(1, 0, TraderState.ACTIVE, new Basket(A));
+    private static final TraderWithSettings T1B = new TraderWithSettings(1, 0, TraderState.ACTIVE, new Basket(B));
+    private static final TraderWithSettings T1C = new TraderWithSettings(1, 0, TraderState.ACTIVE, new Basket(C));
+    private static final TraderWithSettings T1AB = new TraderWithSettings(1, 0, TraderState.ACTIVE, new Basket(A,B));
+    private static final TraderWithSettings T1ABC = new TraderWithSettings(1, 0, TraderState.ACTIVE, new Basket(A,B,C));
+    private static final TraderWithSettings T2 = new TraderWithSettings(2, 0, TraderState.ACTIVE, new Basket());
+    private static final TraderWithSettings T2AC = new TraderWithSettings(2, 0, TraderState.ACTIVE, new Basket(A,C));
+    private static final TraderWithSettings T2ABC = new TraderWithSettings(2, 0, TraderState.ACTIVE, new Basket(A,B,C));
+
+    private static final TraderWithSettings IT0 = new TraderWithSettings(0, 0, TraderState.INACTIVE, new Basket());
+    private static final TraderWithSettings IT0A = new TraderWithSettings(0, 0, TraderState.INACTIVE, new Basket(A));
+    private static final TraderWithSettings IT1B = new TraderWithSettings(1, 0, TraderState.INACTIVE, new Basket(B));
 
     private Market fixture_;
 
@@ -46,14 +57,19 @@ public class MarketTest {
 
     @Test
     @Parameters
-    @TestCaseName("Test #{index}: new Basket({0}).getBasket() == {0}")
+    @TestCaseName("Test #{index}: new Market({0}).getBasket() == {0}")
     public final void testMarket_GetBasket(@NonNull Basket basket) {
         assertSame(basket, new Market(basket).getBasket());
     }
 
     @SuppressWarnings("unused") // it is used reflectively
     private static Object[] parametersForTestMarket_GetBasket() {
-        return baskets;
+        return new Basket[] {
+            new Basket(),
+            new Basket(A),
+            new Basket(A,B),
+            new Basket(A,B,C)
+        };
     }
 
     @Test // That the returned list is indeed unmodifiable (part 1)
@@ -141,15 +157,113 @@ public class MarketTest {
     }
 
     @Test
-    @Ignore
-    public final void testAddSeller() {
-        fail("Not yet implemented");// TODO
+    @Parameters
+    @TestCaseName("Test #{index}: new Market({0}).(add|remove)Seller(...) sequence")
+    // TODO (Vaptistis): may need to check cases when a trader is added to more than one market.
+    // May also need to check more complex sequences that include arbitrary interleaving.
+    public final void testAddRemoveSeller_NormalInput(@NonNull Basket basket, TraderWithSettings[] tradersToAdd,
+                                                      TraderWithSettings[] tradersToRemove) {
+        final Market market = new Market(basket);
+
+        for (TraderWithSettings trader : tradersToAdd) {
+            market.addSeller(trader);
+        }
+
+        assertEquals(tradersToAdd.length, market.getSellers().size());
+        for (TraderWithSettings trader : tradersToAdd) {
+            assertTrue(market.getSellers().contains(trader));
+            assertTrue(trader.getMarketsAsSeller().contains(market));
+        }
+
+        for (TraderWithSettings trader : tradersToRemove) {
+            market.removeSeller(trader);
+        }
+
+        assertEquals(tradersToAdd.length - tradersToRemove.length, market.getSellers().size());
+
+        HashSet<TraderWithSettings> remainingTraders = new HashSet<>(Arrays.asList(tradersToAdd));
+        remainingTraders.removeAll(Arrays.asList(tradersToRemove));
+        for (TraderWithSettings trader : remainingTraders) {
+            assertTrue(market.getSellers().contains(trader));
+            assertTrue(trader.getMarketsAsSeller().contains(market));
+        }
+
+        for (TraderWithSettings trader : tradersToRemove) {
+            assertTrue(!market.getSellers().contains(trader));
+            assertTrue(!trader.getMarketsAsSeller().contains(market));
+        }
     }
 
-    @Test
-    @Ignore
-    public final void testRemoveSeller() {
-        fail("Not yet implemented");// TODO
+    @SuppressWarnings("unused") // it is used reflectively
+    private static Object[] parametersForTestAddRemoveSeller_NormalInput() {
+        return new Object[][] {
+            {new Basket(), new TraderWithSettings[]{T0}, new TraderWithSettings[]{}},
+            {new Basket(), new TraderWithSettings[]{T0}, new TraderWithSettings[]{T0}},
+            {new Basket(A), new TraderWithSettings[]{T0AB}, new TraderWithSettings[]{}},
+            {new Basket(A), new TraderWithSettings[]{T0AB}, new TraderWithSettings[]{T0AB}},
+            {new Basket(), new TraderWithSettings[]{T0,T1AB},new TraderWithSettings[]{}},
+            {new Basket(), new TraderWithSettings[]{T0,T1AB},new TraderWithSettings[]{T0}},
+            {new Basket(), new TraderWithSettings[]{T0,T1AB},new TraderWithSettings[]{T0,T1AB}},
+            {new Basket(), new TraderWithSettings[]{T0,T1AB},new TraderWithSettings[]{T1AB}},
+            {new Basket(A), new TraderWithSettings[]{T0A,T1ABC},new TraderWithSettings[]{}},
+            {new Basket(A), new TraderWithSettings[]{T0A,T1ABC},new TraderWithSettings[]{T0A}},
+            {new Basket(A), new TraderWithSettings[]{T0A,T1ABC},new TraderWithSettings[]{T0A,T1ABC}},
+            {new Basket(A), new TraderWithSettings[]{T0A,T1ABC},new TraderWithSettings[]{T1ABC}},
+            {new Basket(), new TraderWithSettings[]{T0AC,T1A,T2},new TraderWithSettings[]{}},
+            {new Basket(), new TraderWithSettings[]{T0AC,T1A,T2},new TraderWithSettings[]{T0AC}},
+            {new Basket(), new TraderWithSettings[]{T0AC,T1A,T2},new TraderWithSettings[]{T1A}},
+            {new Basket(), new TraderWithSettings[]{T0AC,T1A,T2},new TraderWithSettings[]{T2}},
+            {new Basket(), new TraderWithSettings[]{T0AC,T1A,T2},new TraderWithSettings[]{T0AC,T1A}},
+            {new Basket(), new TraderWithSettings[]{T0AC,T1A,T2},new TraderWithSettings[]{T0AC,T2}},
+            {new Basket(), new TraderWithSettings[]{T0AC,T1A,T2},new TraderWithSettings[]{T1A,T2}},
+            {new Basket(), new TraderWithSettings[]{T0AC,T1A,T2},new TraderWithSettings[]{T0AC,T1A,T2}},
+            {new Basket(A), new TraderWithSettings[]{T0A,T1B,T2AC},new TraderWithSettings[]{}},
+            {new Basket(A), new TraderWithSettings[]{T0A,T1B,T2AC},new TraderWithSettings[]{T2AC,T1B}},
+            {new Basket(A), new TraderWithSettings[]{T0A,T1B,T2AC},new TraderWithSettings[]{T2AC,T0A}},
+            {new Basket(A), new TraderWithSettings[]{T0A,T1B,T2AC},new TraderWithSettings[]{T1B,T0A}},
+            {new Basket(A), new TraderWithSettings[]{T0A,T1B,T2AC},new TraderWithSettings[]{T2AC,T1B,T0A}},
+            {new Basket(A,B), new TraderWithSettings[]{T0AB,T1AB,T2ABC},new TraderWithSettings[]{}},
+        };
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @Parameters
+    @TestCaseName("Test #{index}: new Market({0}).(add|remove)Seller(...) sequence")
+    public final void testAddRemoveSeller_InvalidInput(@NonNull Basket basket, TraderWithSettings[] tradersToAdd,
+                                                       TraderWithSettings[] tradersToRemove) {
+        final Market market = new Market(basket);
+
+        for (TraderWithSettings trader : tradersToAdd) {
+            market.addSeller(trader);
+        }
+
+        for (TraderWithSettings trader : tradersToRemove) {
+            market.removeSeller(trader);
+        }
+    }
+
+    @SuppressWarnings("unused") // it is used reflectively
+    private static Object[] parametersForTestAddRemoveSeller_InvalidInput() {
+        return new Object[][] {
+            {new Basket(), new TraderWithSettings[]{IT0}, new TraderWithSettings[]{}},
+            {new Basket(A), new TraderWithSettings[]{T0}, new TraderWithSettings[]{}},
+            {new Basket(A), new TraderWithSettings[]{IT0A}, new TraderWithSettings[]{}},
+            {new Basket(A), new TraderWithSettings[]{IT0}, new TraderWithSettings[]{}},
+            {new Basket(), new TraderWithSettings[]{T0,IT1B}, new TraderWithSettings[]{}},
+            {new Basket(), new TraderWithSettings[]{IT0,T1B}, new TraderWithSettings[]{}},
+            {new Basket(A), new TraderWithSettings[]{T0,T1ABC}, new TraderWithSettings[]{}},
+            {new Basket(A), new TraderWithSettings[]{T0A,T1C}, new TraderWithSettings[]{}},
+            {new Basket(), new TraderWithSettings[]{T0C,T0C}, new TraderWithSettings[]{}},
+            {new Basket(A), new TraderWithSettings[]{T0A,T0A}, new TraderWithSettings[]{}},
+
+            {new Basket(), new TraderWithSettings[]{}, new TraderWithSettings[]{T0}},
+            {new Basket(), new TraderWithSettings[]{}, new TraderWithSettings[]{IT0}},
+            {new Basket(A), new TraderWithSettings[]{T0AB}, new TraderWithSettings[]{T0}},
+            {new Basket(A), new TraderWithSettings[]{T0AB}, new TraderWithSettings[]{T0AB,T0A}},
+            {new Basket(), new TraderWithSettings[]{T0,T1AB},new TraderWithSettings[]{T0A}},
+            {new Basket(), new TraderWithSettings[]{T0,T1AB},new TraderWithSettings[]{T0,T0A}},
+            {new Basket(), new TraderWithSettings[]{T0,T1AB},new TraderWithSettings[]{T0,T0A,T1AB}},
+        };
     }
 
     @Test // That the returned list is indeed unmodifiable (part 1)
@@ -237,15 +351,62 @@ public class MarketTest {
     }
 
     @Test
-    @Ignore
-    public final void testAddBuyer() {
-        fail("Not yet implemented");// TODO
+    @Parameters
+    @TestCaseName("Test #{index}: new Market({0}).(add|remove)Buyer(...) sequence")
+    // TODO (Vaptistis): may need to check cases when a trader is added to more than one market.
+    // May also need to check more complex sequences that include arbitrary interleaving.
+    public final void testAddRemoveBuyer_NormalInput(@NonNull Basket basket, TraderWithSettings[] tradersToAdd,
+                                                      TraderWithSettings[] tradersToRemove) {
+        final Market market = new Market(basket);
+        final BuyerParticipation[] participations = new BuyerParticipation[tradersToAdd.length];
+
+        for (int i = 0 ; i < tradersToAdd.length ; ++i) {
+            participations[i] = market.addBuyer(tradersToAdd[i]);
+        }
+
+        assertEquals(tradersToAdd.length, market.getBuyers().size());
+        for (int i = 0 ; i < tradersToAdd.length ; ++i) {
+            assertTrue(market.getBuyers().contains(participations[i]));
+            assertTrue(tradersToAdd[i].getMarketsAsBuyer().containsEntry(market, participations[i]));
+        }
+
+        // TODO (Vaptistis): test removal as well
     }
 
-    @Test
-    @Ignore
-    public final void testRemoveBuyerParticipation() {
-        fail("Not yet implemented");// TODO
+    @SuppressWarnings("unused") // it is used reflectively
+    private static Object[] parametersForTestAddRemoveBuyer_NormalInput() {
+        return new Object[][] {
+            {new Basket(), new TraderWithSettings[]{T0}, new TraderWithSettings[]{}},
+            {new Basket(), new TraderWithSettings[]{T0A}, new TraderWithSettings[]{}},
+            {new Basket(A), new TraderWithSettings[]{T0}, new TraderWithSettings[]{}},
+            {new Basket(A), new TraderWithSettings[]{T0A}, new TraderWithSettings[]{}},
+            {new Basket(A), new TraderWithSettings[]{T0,T0},new TraderWithSettings[]{}},
+            {new Basket(), new TraderWithSettings[]{T0A,T0A}, new TraderWithSettings[]{}},
+            {new Basket(A), new TraderWithSettings[]{T0A,T1B,T2AC}, new TraderWithSettings[]{}},
+            {new Basket(A), new TraderWithSettings[]{T0A,T1B,T2AC,T1B}, new TraderWithSettings[]{}},
+        };
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @Parameters
+    @TestCaseName("Test #{index}: new Market({0}).(add|remove)Buyer(...) sequence")
+    public final void testAddRemoveBuyer_InvalidInput(@NonNull Basket basket, TraderWithSettings[] tradersToAdd,
+                                                       TraderWithSettings[] tradersToRemove) {
+        final Market market = new Market(basket);
+
+        for (TraderWithSettings trader : tradersToAdd) {
+            market.addBuyer(trader);
+        }
+
+        // TODO (Vaptistis): test removal as well
+    }
+
+    @SuppressWarnings("unused") // it is used reflectively
+    private static Object[] parametersForTestAddRemoveBuyer_InvalidInput() {
+        return new Object[][] {
+            {new Basket(), new TraderWithSettings[]{IT0}, new TraderWithSettings[]{}},
+            {new Basket(), new TraderWithSettings[]{IT0A}, new TraderWithSettings[]{}},
+        };
     }
 
 } // end MarketTest class
