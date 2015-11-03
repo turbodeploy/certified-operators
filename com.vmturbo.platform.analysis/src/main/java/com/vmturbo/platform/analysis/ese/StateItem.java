dec6092a2023251f10a3565e99b1981a3ebc96d6
apostolos_dailianas <apostolos_dailianas@3bb2320e-05c5-11dd-9ba1-b9e69423245e>
2015-11-03 20:43:03 +0000

    RB 6490 Trader state capturing decisions by the economic scheduling engine (ese) in Market2
    git-svn-id: https://vcs.turbonomic.com/svn/analysis/trunk@58 3bb2320e-05c5-11dd-9ba1-b9e69423245e

---
+++ com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/ese/StateItem.java
@@ -0,0 +1,84 @@
+package com.vmturbo.platform.analysis.ese;
+
+import com.vmturbo.platform.analysis.economy.Trader;
+
+/**
+ * A collection of attributes capturing decisions of the {@link Ese Economic Scheduling Engine} for
+ * a {@link Trader}
+ */
+public class StateItem {
+
+	// Fields
+
+    // the Trader associated with the StateItem
+	private final Trader trader_;
+
+	// times capturing decisions for the Trader
+	private long cloneOnlyAfterThisTime_;
+	private long suspendOnlyAfterThisTime_;
+	private long moveFromOnlyAfterThisTime_;
+	private long moveToOnlyAfterThisTime_;
+	private long moveOnlyAfterThisTime_;
+
+	// Constructors
+
+	/**
+	 * Constructs a State Item and associates it with a Trader
+	 *
+	 * @param correspondingTrader - the Trader to which the State Item corresponds
+	 */
+	public StateItem(Trader correspondingTrader) {
+	    trader_ = correspondingTrader;
+	}
+
+	// Methods
+
+	public Trader getTrader() {
+        return trader_;
+    }
+
+	public long getCloneOnlyAfterThisTime() {
+		return cloneOnlyAfterThisTime_;
+	}
+
+	public StateItem setCloneOnlyAfterThisTime(long time) {
+		cloneOnlyAfterThisTime_ = time;
+		return this;
+	}
+
+	public long getSuspendOnlyAfterThisTime() {
+		return suspendOnlyAfterThisTime_;
+	}
+
+	public StateItem setSuspendOnlyAfterThisTime(long time) {
+		suspendOnlyAfterThisTime_ = time;
+		return this;
+	}
+
+	public long getMoveFromOnlyAfterThisTime() {
+		return moveFromOnlyAfterThisTime_;
+	}
+
+	public StateItem setMoveFromOnlyAfterThisTime(long time) {
+		moveFromOnlyAfterThisTime_ = time;
+		return this;
+	}
+
+	public long getMoveToOnlyAfterThisTime() {
+		return moveToOnlyAfterThisTime_;
+	}
+
+	public StateItem setMoveToOnlyAfterThisTime(long time) {
+		moveToOnlyAfterThisTime_ = time;
+		return this;
+	}
+
+	public long getMoveOnlyAfterThisTime() {
+		return moveOnlyAfterThisTime_;
+	}
+
+	public StateItem setMoveOnlyAfterThisTime(long time) {
+		moveOnlyAfterThisTime_ = time;
+		return this;
+	}
+}
