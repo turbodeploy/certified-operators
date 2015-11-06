60d8922c18ff51e3e6303ad8df104205bf58627b
apostolos_dailianas <apostolos_dailianas@3bb2320e-05c5-11dd-9ba1-b9e69423245e>
2015-11-06 00:09:03 +0000

    RB 6498 Recommendation items now include the economy indexes for the buyer the current supplier and the new supplier
    git-svn-id: https://vcs.turbonomic.com/svn/analysis/trunk@65 3bb2320e-05c5-11dd-9ba1-b9e69423245e

--- com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/recommendations/RecommendationItem.java
+++ com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/recommendations/RecommendationItem.java
@@ -4,16 +4,20 @@ package com.vmturbo.platform.analysis.recommendations;
  * A set of fields describing the recommendation.
  *
  * <p>
- * For now it only contains two strings, description and reason. In the future it will
- * contain information that will allow a module that is between the market and mediation to
- * translate the entities involved to human readable representations, and information to create
- * the recommendation explanation.
+ * For now it contains two strings, description and reason, and the economy trader indexes 
+ * for the buyer, the current supplier and the new supplier.
  * </p>
  */
 public class RecommendationItem {
 
 	// Fields
 
+    // buyer
+    int buyer_;
+    // current supplier
+    int currentSupplier_;
+    // new supplier
+    int newSupplier_;
     // description of the recommendation
     private String description_ = null;
     // reason for the recommendation
@@ -26,9 +30,13 @@ public class RecommendationItem {
 	 * @param description - String describing the recommendation
 	 * @param reason - String describing the reason for the recommendation
 	 */
-    public RecommendationItem(String description, String reason) {
+    public RecommendationItem(String description, String reason, int buyerIndex,
+                    int currentSupplierIndex, int newSupplierIndex) {
         description_ = description;
         reason_ = reason;
+        buyer_ = buyerIndex;
+        currentSupplier_ = currentSupplierIndex;
+        newSupplier_ = newSupplierIndex;
     }
 
     // Methods
@@ -45,5 +53,26 @@ public class RecommendationItem {
      */
     public String getReason() {
     	return reason_;
+    }
+
+    /**
+     * Returns the buyer index for a {@link RecommendationItem recommendation item}.
+     */
+    public int getBuyer() {
+        return buyer_;
+    }
+
+    /**
+     * Returns the current supplier index for a {@link RecommendationItem recommendation item}.
+     */
+    public int getCurrentSupplier() {
+        return currentSupplier_;
+    }
+
+    /**
+     * Returns the new supplier index for a {@link RecommendationItem recommendation item}.
+     */
+    public int getNewSupplier() {
+        return newSupplier_;
     }
 }
