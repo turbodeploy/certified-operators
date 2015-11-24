f2960e52f591d53a6bd38f742d24d640caa456d5
apostolos_dailianas <apostolos_dailianas@3bb2320e-05c5-11dd-9ba1-b9e69423245e>
2015-11-24 11:38:37 +0000

    RB 6500 - https://rbcommons.com/s/VMTurbo/r/6500/ (also as part of this commit, I am committing Placement.java to update the corresponding method calls to EdeCommon) Addressed comments: - fixes in javadoc - formatting - renamed to EdeCommon and made it 'final' - renamed calcQuote to quote - added missing 'final' in vars in quote - changed quote argument from market to basket etc.
    git-svn-id: https://vcs.turbonomic.com/svn/analysis/trunk@82 3bb2320e-05c5-11dd-9ba1-b9e69423245e

--- com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/ese/EdeCommon.java
+++ com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/ese/EdeCommon.java
@@ -1,5 +1,9 @@
 package com.vmturbo.platform.analysis.ese;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.dataflow.qual.Pure;
+
+import com.vmturbo.platform.analysis.economy.Basket;
 import com.vmturbo.platform.analysis.economy.BuyerParticipation;
 import com.vmturbo.platform.analysis.economy.CommoditySold;
 import com.vmturbo.platform.analysis.economy.CommoditySpecification;
@@ -9,56 +13,62 @@ import com.vmturbo.platform.analysis.economy.Trader;
 import com.vmturbo.platform.analysis.pricefunction.PriceFunction;
 import com.vmturbo.platform.analysis.recommendations.RecommendationItem;
 
-public class EseCommon {
+/**
+ * EdeCommon contains a number of methods that are common across decisions algorithms in the engine.
+ *
+ */
+public final class EdeCommon {
 
     /**
      * Calculate the quote of a seller for a basket bought by a buyer.
      *
      * @param buyerParticipation - participation buying specific quantities of the basket commodities
-     * @param market - the market trading the basket bought by the buyer participation
-     * @param economy - the economy where the market belongs
+     * @param basket - the basket bought by the buyer participation
+     * @param currentSupplier - the current supplier for the buyerParticipation. Can be null.
      * @param seller - the seller that will give the quote
      */
-    public static double calcQuote(BuyerParticipation buyerParticipation, Market market,
-                                   Economy economy, Trader seller) {
+    @Pure
+    public static double quote(@NonNull BuyerParticipation buyerParticipation, @NonNull Basket basket,
+                    Trader currentSupplier, @NonNull Trader seller) {
         //TODO (Apostolos): we have not dealt with equivalent commodities
         double quote = 0.0;
-        boolean isCurrentSupplier = seller.equals(economy.getSupplier(buyerParticipation));
-        int commSoldIndex = 0;
+        boolean isCurrentSupplier = seller == currentSupplier;
 
         // get the quantity and peak quantity to buy for each commodity of the basket
-        double[] quantities = buyerParticipation.getQuantities();
-        double[] peakQuantities = buyerParticipation.getPeakQuantities();
+        final double[] quantities = buyerParticipation.getQuantities();
+        final double[] peakQuantities = buyerParticipation.getPeakQuantities();
 
         // go over all commodities in basket
-        for (int index = 0; index < market.getBasket().size(); index++) {
-            CommoditySpecification basketCommSpec = market.getBasket().get(index);
+        for (int boughtIndex = 0, soldIndex = 0; boughtIndex < basket.size();
+                        boughtIndex++, soldIndex++) {
+            CommoditySpecification basketCommSpec = basket.get(boughtIndex);
 
             // Find corresponding commodity sold. Commodities sold are ordered the same way as the
             // basket commodities, so iterate once (O(N)) as opposed to searching each time (O(NLog(N))
-            while (!basketCommSpec.isSatisfiedBy(seller.getBasketSold().get(commSoldIndex))) {
-                commSoldIndex++;
+            while (!basketCommSpec.isSatisfiedBy(seller.getBasketSold().get(soldIndex))) {
+                soldIndex++;
             }
-            CommoditySold commSold = seller.getCommoditiesSold().get(commSoldIndex);
+            CommoditySold commSold = seller.getCommoditiesSold().get(soldIndex);
 
             // add quantities bought by buyer, to quantities already used at seller
-            double capacity = commSold.getCapacity();
-            double utilization = isCurrentSupplier ? commSold.getUtilization()
-                            : (quantities[index] + commSold.getQuantity()) / capacity;
-            double peakUtilization = isCurrentSupplier ? commSold.getPeakUtilization()
-                            : (peakQuantities[index] + commSold.getPeakQuantity()) / capacity;
-            double utilUpperBound = commSold.getSettings().getUtilizationUpperBound();
+            final double capacity = commSold.getCapacity();
+            final double utilization = isCurrentSupplier ? commSold.getUtilization()
+                            : (quantities[boughtIndex] + commSold.getQuantity()) / capacity;
+            final double peakUtilization = isCurrentSupplier ? commSold.getPeakUtilization()
+                            : (peakQuantities[boughtIndex] + commSold.getPeakQuantity()) / capacity;
+            final double utilUpperBound = commSold.getSettings().getUtilizationUpperBound();
 
             // calculate the price per unit for quantity and peak quantity
-            PriceFunction pf = commSold.getSettings().getPriceFunction();
-            double priceUsed = pf.unitPrice(utilization, utilUpperBound);
-            double pricePeak = pf.unitPeakPrice(utilization, peakUtilization, utilUpperBound);
+            final PriceFunction pf = commSold.getSettings().getPriceFunction();
+            final double priceUsed = pf.unitPrice(utilization, utilUpperBound);
+            final double pricePeak = pf.unitPeakPrice(utilization, peakUtilization, utilUpperBound);
 
             // calculate quote
             // TODO: consider removing 100 from everywhere in the code. It is redundant
             // TODO: decide what to do if peakQuantity is less than quantity
-            quote += 100.0 * (quantities[index] / capacity * priceUsed
-                              + Math.max(0.0, peakQuantities[index] - quantities[index]) / capacity * pricePeak);
+            quote += 100.0 * (quantities[boughtIndex] * priceUsed
+                              + Math.max(0.0, peakQuantities[boughtIndex] - quantities[boughtIndex]) * pricePeak)
+                            / capacity;
 
         }
         return quote;
--- com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/ese/Placement.java
+++ com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/ese/Placement.java
@@ -54,7 +54,7 @@ public class Placement {
                 double currentQuote = Double.MAX_VALUE;
                 List<Trader> sellers = market.getSellers();
                 if (sellers == null) {
-                    EseCommon.recommendReconfigure(buyerParticipation, market, economy);
+                    EdeCommon.recommendReconfigure(buyerParticipation, market, economy);
                     continue;
                 }
                 for (Trader seller : sellers) {
@@ -64,7 +64,8 @@ public class Placement {
                         && time < state.get(sellerIndex).getMoveToOnlyAfterThisTime()) {
                         continue;
                     }
-                    quote = EseCommon.calcQuote(buyerParticipation, market, economy, seller);
+                    quote = EdeCommon.quote(buyerParticipation, market.getBasket(), currentSupplier,
+                                    seller);
                     if (seller.equals(currentSupplier)) {
                         currentQuote = quote;
                         if (cheapestQuote == Double.MAX_VALUE) {
@@ -84,7 +85,7 @@ public class Placement {
                 if (currentQuote > cheapestQuote) { // + market.getBasket().size() * 2.0) {
                     //TODO (Apostolos): use economy.getSettings().getQuoteFactor() above
                     // create recommendation and add it to the result list
-                    recommendationList.add(EseCommon.recommendPlace(buyerParticipation, economy,
+                    recommendationList.add(EdeCommon.recommendPlace(buyerParticipation, economy,
                                                                     market, cheapestSeller));
                     // update the economy to reflect the decision
                     moveTraderAndUpdateQuantitiesSold(buyerParticipation, currentSupplier,
@@ -154,3 +155,196 @@ public class Placement {
         }
     }
 }
+
+
+    /**
+     *
+     */
+    // Lets call a 'partition' the set of resources (traders) that are accessible from each
+    // other.
+    // Every trader buys several baskets of goods, e.g., mem/cpu from a PM and storage amount from
+    // a storage. The traders it buys these baskets from have to belong to the same partition,
+    // otherwise they wont be accessible from each other.
+    // Let's say also that within every partition, we further create 'trader groups' of traders
+    // that can satisfy the same basket request. The number of trader groups is equal to the
+    // number of baskets the buyer is buying.
+    // Finally, for every buyer, we associate the partitions it is buying from, and within each
+    // of them the group of traders, one group per relevant basket it is buying.
+    //
+    // As an example, let's say we have a VM that needs to buy mem and cpu from a PM, and
+    // storage amount from two different storages. Let's also say there are two partitions  of PMs
+    // and storages, the first containing PM1, PM2, and ST1, ST2, ST3, and the second containing
+    // PM3 and ST3 and ST4. Then partition 1 for the VM would contain trader group 1 = {PM1, PM2},
+    // trader group 2 = {ST1, ST2, ST3}, and trader group 3 = {ST1, ST2, and ST3}. Partition 2 for
+    // the VM would contain trader group 1 = {PM3}, trader group 2 = {ST3, ST4}, and trader
+    // group 3 = {ST3, ST4}.
+    //
+/*    public List<ActionItem> shopTogetherPlacementDecision1(Economy economy, long decisionTime) {
+
+        List<ActionItem> actionList = new ArrayList<ActionItem>();
+
+        // iterate over all groups of buyers shopping together
+        for (Trader buyer : economy.getBuyersShoppingTogether()) {
+            // TODO (Apostolos): the following is inefficient, but I have ways to optimize it later
+            double [][] quotes = new double [buyer.getBuyers().size()][economy.getTraders().size()];
+            int basketIndex = 0;
+
+            // go over all buyer participations (baskets), calculate and store the quote for each
+            // potential seller
+            for (BuyerParticipation buyerParticipation: buyer.getBuyers()) {
+                Market market = buyerParticipation.getMarket();
+                // calculate the quote for each potential seller
+                Trader currentSupplier = market.getSupplier(buyerParticipation);
+                for (Trader seller: market.getSellers()) {
+                    // if we cannot move to this seller, or it is the current supplier, skip it
+                    int sellerEconomyIndex = economy.getTraders().indexOf(seller);
+                    if (seller != currentSupplier
+                            && decisionTime < decisionState.get(sellerEconomyIndex).getMoveToOnlyAfterThisTime()) {
+                        quotes[basketIndex][sellerEconomyIndex] = Double.MAX_VALUE;
+                    } else {
+                        quotes[basketIndex][sellerEconomyIndex] =
+                                calcQuote(buyerParticipation, market, seller);
+                    }
+                }
+                basketIndex++;
+            }
+
+            // find the minimum total quote of all partitions, i.e., go over all partitions of the
+            // sellers, and calculate the total quote of the partition, by adding the minimum for
+            // every basket in that partition
+            int partitionIndex = 0;
+            int numberOfTraderGroups = buyer.getBuyers().size();
+            double [] cheapestQuotes = new double [numberOfTraderGroups];
+            int [] cheapestSellerMarketIndex = new int [numberOfTraderGroups];
+            int totalPartitionQuote = 0;
+
+            for (Partition partition: buyer.getPartitions()) {
+                basketIndex = 0;
+                int [] cheapestSellerInPartitionMarketIndex = new int [numberOfTraderGroups];
+                double minQuote = Double.MAX_VALUE;
+                double [] cheapestSellerQuote = new double [partition.getTraderGroups().size()];
+
+                // for every basket, find the min quote of the basket
+                for (List<Trader> traderGroup: partition.getTraderGroups()) {
+                    double quote;
+                    for (Trader trader: traderGroup.getTraders()) {
+                        quote = quotes[basketIndex][economy.getTraders().indexOf(trader)];
+                        if (quote < minQuote && quote > 0) {
+                            minQuote = quote;
+                            cheapestSellerInPartitionMarketIndex[basketIndex] =
+                                    economy.getTraders().indexOf(trader);
+                        }
+                    }
+                    totalPartitionQuote += minQuote;
+                    basketIndex++;
+                }
+                // if the partition quote is less than the minQuote so far, replace it, and for
+                // each basket keep the sellers providing it and the quote they provide (the quote
+                // primarily for debugging)
+                if (totalPartitionQuote < minQuote) {
+                    minQuote = totalPartitionQuote;
+                    for (basketIndex = 0; basketIndex < numberOfTraderGroups; basketIndex++) {
+                        cheapestSellerMarketIndex[basketIndex] =
+                                cheapestSellerInPartitionMarketIndex[basketIndex];
+                        cheapestSellerQuote[basketIndex] =
+                                quotes[basketIndex][cheapestSellerInPartitionMarketIndex[basketIndex]];
+
+                    }
+                }
+                partitionIndex++;
+            }
+            // at this point, for each basket we have the cheapesQuote and the cheapestSeller, so
+            // we are done
+        }
+        // TODO (Apostolos): create the actions
+        return actionList;
+    }
+*/
+
+    /**
+     *
+     */
+    // In contrast to the previous solution, here the mediation creates access commodities that
+    // are sold by traders.
+    // Lets call a 'partition' the set of resources (traders) that are accessible from each
+    // other.
+    // The access commodities that a trader sells correspond to partitions. A trader may be
+    // selling one or more of these commodities, depending on how many partitions on belongs to.
+    //
+    // As an example, sao we have two partitions of PMs and storages, the first containing PM1, PM2,
+    // ST1, ST2, and ST3, and the second containing PM3, and ST3 and ST4. Then PM1, PM2, ST1, ST2,
+    // and ST3 will be selling an access commodity with a key P1 (partition 1), while PM3, ST3, and
+    // ST4 will be selling an access commodity with a key P2 (partition 2). Notice that ST3 is
+    // selling two access commodities, one with key P1, the other with key P2.
+    //
+    // Now assume the mediation is creating multiple baskets for each basket of good, each
+    // corresponding to the different partition it may shop from.
+    //
+    // As an example, say a VM is buying mem and cpu from a PM. In the above example, instead of
+    // creating a basket with mem and stAmt, we create two of them, the first is b1 = <mem, cpu,
+    // accessCommP1>, and the second is b2 = <mem, cpu, accessCommP2>.
+    //
+    // Mediation is also associating with the buyer a list of the partitions.
+    // Someone - MAGIC DON'T KNOW HOW TO DO IT - that is not the mediation, but somewhere in the
+    // market realm, creates buyer participations for each of these multiple baskets with different
+    // access commodities, and magically associates them with the particular partition of the buyer.
+    //
+    // For example, if the PM in the above example is shopping for a PM and two storages, then
+    // the PM has partition 1, where we have buyer participation 1 (BP1) buying <mem, cpu, accessCommP1>,
+    // BP2 buying <stAmt, accessCommP1>, and BP3 buying <stAmt, accessCommP1>. Similarly in
+    // partition 2, we have BP4 buying <mem, cpu, accessCommP2>, BP5 buying <stAmt, accessCommP5>,
+    // and BP6 buying <stAmt, accessCommP2>.
+    //
+/*    public List<ActionItem> shopTogetherPlacementDecision2(Economy economy, long decisionTime) {
+
+        List<ActionItem> actionList = new ArrayList<ActionItem>();
+
+        // iterate over all buyers with shop together baskets
+        for (Trader buyer: economy.getBuyersShoppingTogether()) {
+            double [] partitionTotalQuote = new double [buyer.getPartitions().size()];
+            int [][] indexOfCheapestSeller = new int [buyer.getPartitions().size()]
+                    [buyer.getPartitions().get(0).size()];
+            int partitionIndex = 0;
+
+            // find the minimum total quote for all partitions
+            for (Partition partition: buyer.getPartitions()) {
+                double totalQuote = 0;
+                int quoteIndex = 0;
+                // find the combined quote for all buyer participations in the partition
+                for (BuyerParticipation buyerParticipation: partition.getBuyerParticipations()) {
+                    // find the min quote for this buyerParticipation
+                    Market market = buyerParticipation.getMarket();
+                    Trader currentSupplier = market.getSupplier(buyerParticipation);
+                    double quote;
+                    double minQuote = Double.MAX_VALUE;
+                    Trader cheapestSeller;
+                    for (Trader seller: market.getSellers()) {
+                        // if we cannot move to this seller, or it is the current supplier, skip it
+                        int sellerIndex = economy.getTraders().indexOf(seller);
+                        if (seller != currentSupplier
+                                && decisionTime < decisionState.get(sellerIndex).getMoveToOnlyAfterThisTime()) {
+                            quote = Double.MAX_VALUE;
+                        } else {
+                            quote = DecisionsCommon.calcQuote(buyerParticipation, market, seller);
+                        }
+                        if (quote < minQuote) {
+                            minQuote = quote;
+                            cheapestSeller = seller;
+                        }
+                    }
+                    totalQuote += minQuote;
+                    indexOfCheapestSeller[partitionIndex][quoteIndex] =
+                            economy.getTraders().indexOf(cheapestSeller);
+                    quoteIndex++;
+                }
+                partitionTotalQuote[partitionIndex] = totalQuote;
+                partitionIndex++;
+            }
+        }
+
+        //... chose the best one ...
+
+        return actionList;
+    }
+*/
+
