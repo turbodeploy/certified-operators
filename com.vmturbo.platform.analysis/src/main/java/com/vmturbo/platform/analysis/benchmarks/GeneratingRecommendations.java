25007807c774cd0a9f31c27cee905f9930e008df
vaptistis_anogeianakis <vaptistis_anogeianakis@3bb2320e-05c5-11dd-9ba1-b9e69423245e>
2015-12-28 19:35:56 +0000

    Added the benchmark used to create some 'Stage 3' measurements in Confluence.
    This is the first version of the benchmark that was used to compare parallel
    and sequential execution of the placement algorithm, but it can also be used
    for other measurements on economies of growing sizes.
    
    This version creates and measures 'worst case' economies with a single market
    comprising an equal number of buyers and sellers. This is a 'low resolution'
    benchmark raising the number of both buyers and sellers by a factor of 10 each
    time.
    
    Reviewed at https://rbcommons.com/s/VMTurbo/r/7282/
    
    git-svn-id: https://vcs.turbonomic.com/svn/analysis/trunk@116 3bb2320e-05c5-11dd-9ba1-b9e69423245e

---
+++ com.vmturbo.platform.analysis/src/main/java/com/vmturbo/platform/analysis/benchmarks/GeneratingRecommendations.java
@@ -0,0 +1,87 @@
+package com.vmturbo.platform.analysis.benchmarks;
+
+import java.util.List;
+
+import org.checkerframework.checker.javari.qual.ReadOnly;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import com.vmturbo.platform.analysis.economy.Basket;
+import com.vmturbo.platform.analysis.economy.CommodityBought;
+import com.vmturbo.platform.analysis.economy.CommoditySold;
+import com.vmturbo.platform.analysis.economy.CommoditySpecification;
+import com.vmturbo.platform.analysis.economy.Economy;
+import com.vmturbo.platform.analysis.economy.Trader;
+import com.vmturbo.platform.analysis.economy.TraderState;
+import com.vmturbo.platform.analysis.ese.Ede;
+import com.vmturbo.platform.analysis.recommendations.RecommendationItem;
+
+/**
+ * A benchmark to determine the performance of the 'market algorithms' on Economies of varying sizes.
+ */
+public final class GeneratingRecommendations {
+    // Methods
+
+    public static void main(String[] args) {
+        // Parameters
+        final int nOrdersOfMagnitude = 5;
+        final int nCommodities = 10;
+        final int nIterations = 5;
+
+        // Generate a number of large economies
+        System.out.println("Populating Economies:");
+        final CommoditySpecification[] commoditySpecifications = new CommoditySpecification[nCommodities];
+        for (int i = 0 ; i < commoditySpecifications.length ; ++i) {
+            commoditySpecifications[i] = new CommoditySpecification(i);
+        }
+        final @NonNull Basket emptyBasket = new Basket();
+        final @NonNull Basket tradedBasket = new Basket(commoditySpecifications);
+
+        final @NonNull Economy[] economies = new Economy[nOrdersOfMagnitude];
+        for (int i = 0, nBuyers = 1, nSellers = 1 ; i < nOrdersOfMagnitude ; ++i, nBuyers *= 10, nSellers *= 10) {
+            long start = System.nanoTime();
+            economies[i] = new Economy();
+
+            for (int c = 0 ; c < nBuyers ; ++c) {
+                Trader trader = economies[i].addTrader(1, TraderState.ACTIVE, emptyBasket, tradedBasket);
+
+                for (CommodityBought commodityBought : economies[i].getCommoditiesBought(
+                        economies[i].getMarketsAsBuyer(trader).values().iterator().next())) {
+                    commodityBought.setQuantity(10);
+                    commodityBought.setPeakQuantity(15);
+                }
+            }
+
+            for (int c = 0 ; c < nSellers ; ++c) {
+                Trader trader = economies[i].addTrader(0, TraderState.ACTIVE, tradedBasket);
+
+                for (@NonNull @ReadOnly CommoditySold commoditySold : trader.getCommoditiesSold()) {
+                    commoditySold.setCapacity(100);
+                    commoditySold.setQuantity(50);
+                    commoditySold.setPeakQuantity(60);
+                }
+            }
+
+            System.out.println(System.nanoTime()-start);
+        }
+
+        // Warm up
+        final @NonNull Ede ede = new Ede();
+        System.out.println("Warming up:");
+        long start = System.nanoTime();
+        List<RecommendationItem> recommendations = ede.createRecommendations(economies[nOrdersOfMagnitude-1]);
+        System.out.println(System.nanoTime()-start);
+
+
+        // Run placement
+        for (int i = 0 ; i < nOrdersOfMagnitude ; ++i) {
+            for (int j = 0 ; j < nIterations ; ++j) {
+                start = System.nanoTime();
+                recommendations = ede.createRecommendations(economies[i]);
+                System.out.print(System.nanoTime()-start + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println("Done!");
+    }
+
+} // end GeneratingRecommendations benchmark
