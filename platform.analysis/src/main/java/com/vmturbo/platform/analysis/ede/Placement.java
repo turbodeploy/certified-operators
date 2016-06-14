a5dc3cc16b7b2d06fbebe51ab1566284c684ac1f
vaptistis_anogeianakis <vaptistis_anogeianakis@3bb2320e-05c5-11dd-9ba1-b9e69423245e>
2016-06-14 12:29:54 +0000

    Added a first version of shop-together placement without activating it.
    Kept old placement code and everything that used to use it still does.
    Will have to change our drivers and server to use the new version at a later
    time.
    
    Included a large number of hand-written tests for shop-together placement that
    mostly cover simple placements and a few key shop-together cases.
    
    Still need to improve handling of mixes of movable and immovable shopping
    lists, but current version should work as long as there are access commodities
    for VDCs.
    
    Also need to extend tests and provide for a more expressive way to write them.
    
    Reviewed at https://rbcommons.com/s/VMTurbo/r/9771/
    
    git-svn-id: https://vcs.turbonomic.com/svn/analysis/trunk@260 3bb2320e-05c5-11dd-9ba1-b9e69423245e

--- platform.analysis/src/main/java/com/vmturbo/platform/analysis/ede/Placement.java
+++ platform.analysis/src/main/java/com/vmturbo/platform/analysis/ede/Placement.java
@@ -2,10 +2,18 @@ package com.vmturbo.platform.analysis.ede;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Map.Entry;
 
+import org.checkerframework.checker.javari.qual.ReadOnly;
 import org.checkerframework.checker.nullness.qual.NonNull;
+
+import com.google.common.collect.Sets;
 import com.vmturbo.platform.analysis.actions.Action;
+import com.vmturbo.platform.analysis.actions.CompoundMove;
 import com.vmturbo.platform.analysis.actions.Move;
 import com.vmturbo.platform.analysis.actions.Reconfigure;
 import com.vmturbo.platform.analysis.economy.ShoppingList;
@@ -13,14 +21,17 @@ import com.vmturbo.platform.analysis.economy.Economy;
 import com.vmturbo.platform.analysis.economy.Market;
 import com.vmturbo.platform.analysis.economy.Trader;
 
+/**
+ * Contains static methods related to optimizing the placement of {@link Trader}s in an
+ * {@link Economy}.
+ */
 public class Placement {
 
     /**
-     * Return a list of recommendations to optimize the placement of all traders in the economy.
+     * Returns a list of recommendations to optimize the placement of all traders in the economy.
      *
      * <p>
-     *  As a result of invoking this method, both the economy and the state that are passed as
-     *  parameters to it, may be changed.
+     *  As a result of invoking this method, the economy may be changed.
      * </p>
      *
      * @param economy - the economy whose traders' placement we want to optimize
@@ -68,4 +79,55 @@ public class Placement {
         return actions;
     }
 
-}
+    /**
+     * Returns a list of recommendations to optimize the placement of all traders in the economy.
+     *
+     * <p>
+     *  As a result of invoking this method, the economy may be changed.
+     * </p>
+     *
+     * @param economy - the economy whose traders' placement we want to optimize
+     */
+    public static @NonNull List<@NonNull Action> shopTogetherDecisions(@NonNull Economy economy) {
+        @NonNull List<@NonNull Action> output = new ArrayList<>();
+
+        for (@NonNull @ReadOnly Trader buyer : economy.getTraders()) {
+            final @NonNull @ReadOnly Set<Entry<@NonNull ShoppingList, @NonNull Market>> entries =
+                economy.getMarketsAsBuyer(buyer).entrySet();
+
+            // Only optimize active traders that are at least partially movable.
+            if (buyer.getState().isActive() && entries.stream().anyMatch(entry->entry.getKey().isMovable())) {
+                // Find the set of k-partite cliques where the trader can potentially be placed.
+                Set<Integer> commonCliques = entries.stream()
+                    .map(entry->entry.getKey().isMovable() // if shopping list is movable
+                        ? entry.getValue().getCliques().keySet() // use the cliques of the market
+                        : (new TreeSet<>(entry.getKey().getSupplier() != null // else if shopping list is placed
+                                        ? entry.getKey().getSupplier().getCliques() // use clique that contain supplier
+                                        : Arrays.asList())) // else there is no valid placement.
+                    ).reduce(Sets::intersection).get();
+
+                // TODO: properly skip immovable traders when computing total quotes
+
+                // Compute current total quote.
+                final double currentTotalQuote = entries.stream().mapToDouble(entry ->
+                    entry.getKey().getSupplier() == null // if unplaced or incorrectly placed
+                        || !entry.getValue().getBasket().isSatisfiedBy(entry.getKey().getSupplier().getBasketSold())
+                    ? Double.POSITIVE_INFINITY // current total is infinite
+                    : EdeCommon.quote(economy, entry.getKey(), entry.getKey().getSupplier(),
+                                      Double.POSITIVE_INFINITY, false)[0]
+                ).sum(); // TODO: break early...
+
+                // Compute the best total quote.
+                CliqueMinimizer minimizer = commonCliques.stream().collect(
+                    ()->new CliqueMinimizer(economy,entries), CliqueMinimizer::accept, CliqueMinimizer::combine);
+
+                // If buyer can improve its position, output action.
+                if (minimizer.getBestTotalQuote() < currentTotalQuote) {
+                    output.add(new CompoundMove(economy, buyer, minimizer.getBestSellers()).take());
+                }
+            }
+        }
+        return output;
+    }
+
+} // end Placement class
