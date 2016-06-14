b7dba6d0a9e30590075f84c3aef19f51c345ad02
vaptistis_anogeianakis <vaptistis_anogeianakis@3bb2320e-05c5-11dd-9ba1-b9e69423245e>
2016-06-14 12:28:54 +0000

    Created a CompoundMove class to bundle moves that should be performed atomically.
    This is intended to be used for share-nothing moves. E.g. the user should only
    be able to accept or reject the bundle as a whole and attempting to execute
    only a subset of it could fail.
    
    Unfortunately no tests have been added for this class.
    It was indirectly tested locally as part of the shop-together placement tests.
    
    Reviewed at https://rbcommons.com/s/VMTurbo/r/9768/
    
    git-svn-id: https://vcs.turbonomic.com/svn/analysis/trunk@257 3bb2320e-05c5-11dd-9ba1-b9e69423245e

---
+++ platform.analysis/src/main/java/com/vmturbo/platform/analysis/actions/CompoundMove.java
@@ -0,0 +1,201 @@
+package com.vmturbo.platform.analysis.actions;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.checkerframework.checker.javari.qual.ReadOnly;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.checkerframework.dataflow.qual.Pure;
+
+import com.vmturbo.platform.analysis.economy.ShoppingList;
+import com.google.common.collect.Lists;
+import com.vmturbo.platform.analysis.economy.Economy;
+import com.vmturbo.platform.analysis.economy.Trader;
+
+/**
+ * An action to group multiple {@link Move}s that should happen atomically by {@link ShoppingList}s
+ * of the same {@link Trader}.
+ */
+public class CompoundMove implements Action {
+    // Fields
+    private final @NonNull List<@NonNull Move> moves_;
+
+    // Constructors
+
+    /**
+     * Constructs a new compound move action with the specified target, economy and destinations.
+     *
+     * @param economy The economy containing <b>target</b> and <b>destinations</b>.
+     * @param target The trader whose shopping lists should move atomically.
+     * @param destinations The new suppliers for <b>target</b>s shopping lists in the same order as
+     *                     the shopping lists appear in {@link Economy#getMarketsAsBuyer(Trader)}.
+     *                     Must not be empty.
+     */
+    public CompoundMove(@NonNull Economy economy, @NonNull Trader target,
+                        @NonNull Collection<@Nullable Trader> destinations) {
+        this(economy,target,economy.getMarketsAsBuyer(target).keySet().stream()
+            .map(ShoppingList::getSupplier).collect(Collectors.toList()),destinations);
+    }
+
+    /**
+     * Constructs a new compound move action with the specified target, economy, sources and
+     * destinations.
+     *
+     * <p>
+     *  Same as {@link #CompoundMove(Economy, Trader, Collection)}, but let's you explicitly specify
+     *  the sources of the moves instead of getting them from the shopping lists.
+     * </p>
+     *
+     * @param economy Same as for {@link #CompoundMove(Economy, Trader, Collection)}.
+     * @param target Same as for {@link #CompoundMove(Economy, Trader, Collection)}.
+     * @param sources The current suppliers for the shopping lists, at the time the move would be
+     *                executed, in the same order as the latter appear in
+     *                {@link Economy#getMarketsAsBuyer(Trader)}.
+     *                Must be the same size as <b>destinations</b> and not empty.
+     * @param destinations Same as for {@link #CompoundMove(Economy, Trader, Collection)}.
+     */
+    public CompoundMove(@NonNull Economy economy, @NonNull Trader target,
+            @NonNull Collection<@Nullable Trader> sources, @NonNull Collection<@Nullable Trader> destinations) {
+        final Set<@NonNull ShoppingList> shoppingLists = economy.getMarketsAsBuyer(target).keySet();
+        checkArgument(shoppingLists.size() == destinations.size(), "shoppingLists.size() = "
+                    + shoppingLists.size() + ", destinations.size() = " + destinations.size());
+        checkArgument(shoppingLists.size() == sources.size(), "shoppingLists.size() = "
+                    + shoppingLists.size() + ", sources.size() = " + sources.size());
+        @NonNull List<@NonNull Move> moves = new ArrayList<>();
+
+        Iterator<ShoppingList> shoppingListIter = shoppingLists.iterator();
+        Iterator<Trader> sourceIter = sources.iterator();
+        Iterator<Trader> destinationIter = destinations.iterator();
+        while (shoppingListIter.hasNext()) {
+            moves.add(new Move(economy, shoppingListIter.next(), sourceIter.next(), destinationIter.next()));
+        }
+
+        moves_ = Collections.unmodifiableList(moves);
+    }
+
+    // Getters
+
+    /**
+     * Returns an unmodifiable list of the moves that comprise {@code this} compound move.
+     */
+    @Pure
+    public @NonNull List<@NonNull Move> getConstituentMoves(@ReadOnly CompoundMove this) {
+        return moves_;
+    }
+
+    // Methods
+
+    @Override
+    public @NonNull String serialize(@NonNull Function<@NonNull Trader, @NonNull String> oid) {
+        final @NonNull StringBuilder sb = new StringBuilder();
+
+        sb.append("<action type=\"compound move\" >");
+        for (Move move : moves_) {
+            sb.append("    ").append(move.serialize(oid)).append('\n');
+        }
+        sb.append("</action>");
+
+        return sb.toString();
+    }
+
+    @Override
+    public @NonNull CompoundMove take() {
+        moves_.forEach(Move::take);
+        return this;
+    }
+
+    @Override
+    public @NonNull CompoundMove rollback() {
+        moves_.forEach(Move::rollback);
+        return this;
+    }
+
+    @Override
+    public @NonNull Trader getActionTarget() {
+        return moves_.get(0).getActionTarget();
+    }
+
+    @Override
+    @Pure
+    public @NonNull @ReadOnly Object getCombineKey() {
+        return Lists.newArrayList(CompoundMove.class, getActionTarget());
+    }
+
+    @Override
+    @Pure
+    public @Nullable @ReadOnly Action combine(@NonNull Action action) {
+        // Assume the argument is a CompoundMove of the same target, otherwise we are not supposed
+        // to get here. Also assume a consistent sequence of actions, i.e.
+        // this.getDestination() == action.getSource().
+        // TODO: do we need to combine with scalar moves?
+
+        CompoundMove other = (CompoundMove)action;
+        checkArgument(moves_.size() == other.moves_.size());
+
+        if (IntStream.range(0, moves_.size()).allMatch(i->moves_.get(i).getSource() == other.moves_.get(i).getDestination())) {
+            return null;
+        } else {
+            return new CompoundMove(moves_.get(0).getEconomy(), getActionTarget(),
+                                    moves_.stream().map(Move::getSource).collect(Collectors.toList()),
+                              other.moves_.stream().map(Move::getDestination).collect(Collectors.toList()));
+        }
+    }
+
+    /**
+     * Tests whether two {@link CompoundMove}s are equal field by field.
+     */
+    @Override
+    @Pure
+    public boolean equals(@ReadOnly CompoundMove this, @ReadOnly Object other) {
+        if(other == null || !(other instanceof CompoundMove))
+            return false;
+        return moves_.equals(((CompoundMove)other).moves_);
+    }
+
+    @Override
+    @Pure
+    public int hashCode() {
+        return moves_.hashCode();
+    }
+
+    @Override
+    public @NonNull String debugDescription(@NonNull Function<@NonNull Trader, @NonNull String> uuid,
+                                            @NonNull Function<@NonNull Trader, @NonNull String> name,
+                                            @NonNull IntFunction<@NonNull String> commodityType,
+                                            @NonNull IntFunction<@NonNull String> traderType) {
+        final @NonNull StringBuilder sb = new StringBuilder();
+
+        sb.append("Perform the following moves atomically:\n");
+        for (Move move : moves_) {
+            sb.append("    ").append(move.debugDescription(uuid, name, commodityType, traderType)).append('\n');
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public @NonNull String debugReason(@NonNull Function<@NonNull Trader, @NonNull String> uuid,
+                                       @NonNull Function<@NonNull Trader, @NonNull String> name,
+                                       @NonNull IntFunction<@NonNull String> commodityType,
+                                       @NonNull IntFunction<@NonNull String> traderType) {
+        final @NonNull StringBuilder sb = new StringBuilder();
+
+        sb.append("To improve the state of the environment. The reasons for individual moves follow:\n");
+        for (Move move : moves_) {
+            sb.append("    ").append(move.debugReason(uuid, name, commodityType, traderType)).append('\n');
+        }
+
+        return sb.toString();
+    }
+
+} // end CompoundMove class
