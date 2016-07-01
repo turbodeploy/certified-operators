ba82245251a404010da575011ba679d11d83feba
shravan_sriram <shravan_sriram@3bb2320e-05c5-11dd-9ba1-b9e69423245e>
2016-07-01 19:14:18 +0000

    running single placement runs several times till no new actions are generated
    git-svn-id: https://vcs.turbonomic.com/svn/analysis/trunk@303 3bb2320e-05c5-11dd-9ba1-b9e69423245e

--- platform.analysis/src/main/java/com/vmturbo/platform/analysis/ede/Ede.java
+++ platform.analysis/src/main/java/com/vmturbo/platform/analysis/ede/Ede.java
@@ -38,7 +38,12 @@ public final class Ede {
     public @NonNull List<@NonNull Action> generateActions(@NonNull Economy economy) {
         @NonNull List<Action> actions = new ArrayList<>();
         // generate placement actions
-        actions.addAll(Placement.placementDecisions(economy));
+        boolean keepRunning = true;
+        while (keepRunning) {
+            List<Action> placeActions = Placement.placementDecisions(economy);
+            keepRunning = !placeActions.isEmpty();
+            actions.addAll(placeActions);
+        }
         Ledger ledger = new Ledger(economy);
         // generate provision actions
         actions.addAll(Provision.provisionDecisions(economy, ledger));
