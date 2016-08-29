a4ada0bdd29f3a1a303ee76a4b728e444205b978
sheyeh <sheyeh@3bb2320e-05c5-11dd-9ba1-b9e69423245e>
2016-08-29 20:13:46 +0000

    Move collapse actions into Ede RB:11082 [s:jira, t:11199]
    git-svn-id: https://vcs.turbonomic.com/svn/analysis/trunk@381 3bb2320e-05c5-11dd-9ba1-b9e69423245e

--- platform.analysis/src/main/java/com/vmturbo/platform/analysis/ede/Ede.java
+++ platform.analysis/src/main/java/com/vmturbo/platform/analysis/ede/Ede.java
@@ -2,12 +2,14 @@ package com.vmturbo.platform.analysis.ede;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.log4j.Logger;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 import com.vmturbo.platform.analysis.actions.Action;
 import com.vmturbo.platform.analysis.actions.CompoundMove;
+import com.vmturbo.platform.analysis.actions.Move;
 import com.vmturbo.platform.analysis.actions.Reconfigure;
 import com.vmturbo.platform.analysis.economy.Economy;
 import com.vmturbo.platform.analysis.economy.Trader;
@@ -35,6 +37,17 @@ public final class Ede {
     // Methods
 
     /**
+     * @deprecated
+     * Add 'collapse' argument
+     * @see {@link #generateActions(Economy, boolean, boolean, boolean, boolean, boolean)}
+     */
+    public @NonNull List<@NonNull Action> generateActions(@NonNull Economy economy,
+            boolean isShopTogether, boolean isProvision, boolean isSuspension,
+            boolean isResize) {
+        return generateActions(economy, isShopTogether, isProvision, isSuspension, isResize, false);
+    }
+
+    /**
      * Create a new set of actions for a snapshot of the economy.
      *
      * @param economy The snapshot of the economy which we analyze and take decisions.
@@ -42,29 +55,25 @@ public final class Ede {
      * @param isProvision True if we need to trigger provision algorithm and false otherwise
      * @param isSuspension True if we need to trigger suspension algorithm and false otherwise
      * @param isResize True if we need to trigger resize algorithm and false otherwise
+     * @param collapse whether to collapse the returned list of actions.
      * @return A list of actions suggested by the economic decisions engine.
+     * 
+     * @see {@link Action#collapsed(List)}
      */
     public @NonNull List<@NonNull Action> generateActions(@NonNull Economy economy,
                     boolean isShopTogether, boolean isProvision, boolean isSuspension,
-                    boolean isResize) {
-
+                    boolean isResize, boolean collapse) {
         logger.info("Plan Started.");
-        @NonNull List<Action> actions = new ArrayList<>();
-        // provision enough Traders so that plan starts with enough supply for all demand to be
-        // placed and comply to the user-defined utilization upper bound constraints.
-        actions.addAll(BootstrapSupply.bootstrapSupplyDecisions(economy));
-
+        // Start by provisioning enough traders to satisfy all the demand
+        @NonNull List<Action> actions = BootstrapSupply.bootstrapSupplyDecisions(economy);
         // generate placement actions
         boolean keepRunning = true;
         while (keepRunning) {
-            List<Action> placeActions;
-            if (isShopTogether) {
-                placeActions = breakDownCompoundMove(Placement.shopTogetherDecisions(economy));
-            } else {
-                placeActions = Placement.placementDecisions(economy);
-            }
-            keepRunning = !(placeActions.isEmpty() || placeActions.stream().allMatch(a ->
-                                a instanceof Reconfigure));
+            List<Action> placeActions = isShopTogether
+                    ? breakDownCompoundMove(Placement.shopTogetherDecisions(economy))
+                    : Placement.placementDecisions(economy);
+            keepRunning = !(placeActions.isEmpty()
+                    || placeActions.stream().allMatch(a -> a instanceof Reconfigure));
             actions.addAll(placeActions);
         }
         Ledger ledger = new Ledger(economy);
@@ -83,8 +92,25 @@ public final class Ede {
         if (isResize) {
             actions.addAll(Resizer.resizeDecisions(economy, ledger));
         }
+        if (collapse) {
+            // TODO: All of this should be done in the collapse method
+            // Don't collapse Moves that have a 'null' source (no-source moves)
+            List<Action> noSourceMoves = actions.stream()
+                    .filter(m -> m instanceof Move && ((Move)m).getSource() == null)
+                    .collect(Collectors.toList());
+            actions.removeAll(noSourceMoves);
+            // Now collapse all actions except for those no-source moves
+            List<@NonNull Action> collapsed = Action.collapsed(actions);
+            // Reorder actions by type. Include the no-source moves.
+            actions = Action.groupActionsByTypeAndReorderBeforeSending(noSourceMoves, collapsed);
+        }
         logger.info("Plan completed with " + actions.size() + " actions.");
-
+        if (logger.isDebugEnabled()) {
+        	// Log number of actions by type
+        	actions.stream()
+        	    .collect(Collectors.groupingBy(Action::getClass))
+        	    .forEach((k, v) -> logger.debug("    " + k.getSimpleName() + " : " + v.size()));
+        }
         return actions;
     }
 
