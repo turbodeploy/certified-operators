0d230ca11747ee9fe0c36000dd60ba1a539ceabd
alexander_milantev <alexander.milantev@turbonomic.com>
2017-10-30 09:28:13 +0000

    OM-24821: Created data structure for resolving conflicts of targets which can execute the same action

---
+++ com.vmturbo.action.orchestrator/src/main/java/com/vmturbo/action/orchestrator/execution/ActionTargetByProbeCategoryResolver.java
@@ -0,0 +1,157 @@
+package com.vmturbo.action.orchestrator.execution;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import javax.annotation.Nonnull;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+
+import com.vmturbo.common.protobuf.action.ActionDTO;
+import com.vmturbo.common.protobuf.action.ActionDTO.Action;
+import com.vmturbo.common.protobuf.action.ActionDTO.ActionInfo.ActionTypeCase;
+import com.vmturbo.communication.CommunicationException;
+import com.vmturbo.topology.processor.api.ProbeInfo;
+import com.vmturbo.topology.processor.api.TargetInfo;
+import com.vmturbo.topology.processor.api.TopologyProcessor;
+import com.vmturbo.topology.processor.api.TopologyProcessorException;
+
+/**
+ * Class resolves conflicts when multiple targets are able to execute the action.
+ * Resolving is based on priorities of probe categories which are hardcoded inside.
+ */
+public class ActionTargetByProbeCategoryResolver implements ActionTargetResolver {
+
+    private final Logger logger = LogManager.getLogger();
+
+    private final TopologyProcessor topologyProcessor;
+
+    /**
+     * If multiple probes support the same action, this priority is used to determine which target
+     * will execute the action. Position of category in list determines priority. List contains
+     * highest priority categories which should be executed firstly at the begin of it and lowest
+     * priority categories at the end.
+     **/
+    private static final List<String> PROBE_CATEGORY_PRIORITIES =
+            ImmutableList.of(
+                    "CLOUD MANAGEMENT",
+                    "LOAD BALANCER",
+                    "STORAGE",
+                    "FABRIC",
+                    "HYPERVISOR",
+                    "OPERATION MANAGER APPLIANCE",
+                    "APPLICATION SERVER",
+                    "DATABASE",
+                    "FLOW",
+                    "CUSTOM",
+                    "GUEST OS PROCESSES");
+
+    /** Provides Priorities of Probe categories for each action type. **/
+    private static final Map<ActionTypeCase, List<String>> ACTION_TYPES_PROBE_PRIORITIES;
+
+    /** For now we have the same priorities for all action types. **/
+    static {
+        final Builder<ActionTypeCase, List<String>> builder = ImmutableMap.builder();
+        builder.put(ActionTypeCase.ACTIONTYPE_NOT_SET, PROBE_CATEGORY_PRIORITIES);
+        builder.put(ActionTypeCase.MOVE, PROBE_CATEGORY_PRIORITIES);
+        builder.put(ActionTypeCase.RECONFIGURE, PROBE_CATEGORY_PRIORITIES);
+        builder.put(ActionTypeCase.PROVISION, PROBE_CATEGORY_PRIORITIES);
+        builder.put(ActionTypeCase.RESIZE, PROBE_CATEGORY_PRIORITIES);
+        builder.put(ActionTypeCase.ACTIVATE, PROBE_CATEGORY_PRIORITIES);
+        builder.put(ActionTypeCase.DEACTIVATE, PROBE_CATEGORY_PRIORITIES);
+        ACTION_TYPES_PROBE_PRIORITIES = builder.build();
+    }
+
+    public ActionTargetByProbeCategoryResolver(@Nonnull final TopologyProcessor topologyProcessor) {
+        this.topologyProcessor = Objects.requireNonNull(topologyProcessor);
+    }
+
+    /**
+     * Resolves target for action which can be executed by multiple targets.
+     * Resolving is based on probe category prorities which are hardcoded inside.
+     *
+     * @param action action to be executed.
+     * @param targets targets which can execute the action
+     * @return resolved targetId
+     * @throws TargetResolutionException when either provided action was null or set of targets
+     * was null or empty
+     */
+    @Override
+    public long resolveExecutantTarget(@Nonnull ActionDTO.Action action, @Nonnull Set<Long> targets)
+            throws TargetResolutionException {
+        checkForNullActionAndTargets(action, targets);
+        final Map<Long, ProbeInfo> targetIdsToProbeInfos = getProbeInfosOfTargets(targets);
+
+        final List<String> probePriorities =
+                ACTION_TYPES_PROBE_PRIORITIES.get(action.getInfo().getActionTypeCase());
+
+        final Map<Long, Integer> targetIdsToPriorities = targetIdsToProbeInfos.entrySet().stream()
+                .filter(targetIdProbe -> probePriorities.contains(targetIdProbe.getValue().getCategory()))
+                .collect(Collectors.toMap(targetIdProbe -> targetIdProbe.getKey(),
+                        targetIdProbe -> probePriorities.indexOf(targetIdProbe.getValue().getCategory())));
+        return targetIdsToPriorities.entrySet().stream().min(Entry.comparingByValue()).get().getKey();
+    }
+
+    @Nonnull
+    private Map<Long, ProbeInfo> getProbeInfosOfTargets(@Nonnull Set<Long> targets) {
+        final Map<Long, Optional<TargetInfo>> targetInfos = targets.stream()
+                .collect(Collectors.toMap(Function.identity(), targetId -> getProbeOrTarget
+                        (targetId, topologyProcessor::getTarget)));
+
+        final Map<Long, Optional<ProbeInfo>> probeInfosOptionalsOfTargets = targetInfos.entrySet().stream()
+                .filter(targetIdToTargetInfoOpt -> targetIdToTargetInfoOpt.getValue().isPresent())
+                .collect(Collectors.toMap(targetIdToTargetInfoOpt -> targetIdToTargetInfoOpt.getKey(),
+                        targetIdToTargetInfoOpt -> getProbeOrTarget(targetIdToTargetInfoOpt
+                                .getValue().get().getProbeId(), topologyProcessor::getProbe)));
+
+        return probeInfosOptionalsOfTargets
+                .entrySet().stream().filter(entry -> entry.getValue().isPresent())
+                .collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue().get()));
+    }
+
+    private void checkForNullActionAndTargets(@Nonnull Action action, @Nonnull Set<Long> targets)
+            throws TargetResolutionException {
+        if (action == null || CollectionUtils.isEmpty(targets)) {
+            throw new TargetResolutionException("Cannot resolve target for action");
+        }
+    }
+
+    private <T> Optional<T> getProbeOrTarget(long id, GetByIdFunction<T> getTargetOrProbe) {
+        try {
+            return Optional.of(getTargetOrProbe.apply(id));
+        } catch (CommunicationException | TopologyProcessorException e) {
+            logger.warn("Cannot resolve Target or probe for Id:{}", id, e);
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Interface of function which returnes generic type objects by id.
+     *
+     * @param <T> Type of object to be returned
+     */
+    @FunctionalInterface
+    private interface GetByIdFunction<T> {
+        /**
+         * Returnes object by id.
+         *
+         * @param id id of object to be returned
+         * @return object with this id
+         * @throws CommunicationException if cannot send or receive data from TP
+         * @throws TopologyProcessorException if TP cannot resolve the object
+         */
+        T apply(long id) throws CommunicationException, TopologyProcessorException;
+    }
+}
---
+++ com.vmturbo.action.orchestrator/src/test/java/com/vmturbo/action/orchestrator/execution/ActionTargetByProbeCategoryResolverTest.java
@@ -0,0 +1,127 @@
+package com.vmturbo.action.orchestrator.execution;
+
+import javax.annotation.Nonnull;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.google.common.collect.ImmutableSet;
+
+import com.vmturbo.common.protobuf.action.ActionDTO;
+import com.vmturbo.common.protobuf.action.ActionDTO.Action;
+import com.vmturbo.common.protobuf.action.ActionDTO.ActionInfo;
+import com.vmturbo.common.protobuf.action.ActionDTO.Explanation;
+import com.vmturbo.common.protobuf.action.ActionDTO.Reconfigure;
+import com.vmturbo.communication.CommunicationException;
+import com.vmturbo.topology.processor.api.ProbeInfo;
+import com.vmturbo.topology.processor.api.TargetInfo;
+import com.vmturbo.topology.processor.api.TopologyProcessor;
+import com.vmturbo.topology.processor.api.TopologyProcessorException;
+
+/**
+ * Tests for resolving action execution conflicts by probe category priority.
+ */
+public class ActionTargetByProbeCategoryResolverTest {
+
+    private TopologyProcessor topologyProcessor;
+
+    private ActionTargetResolver targetResolver;
+
+    private final long ID_1 = 1l;
+    private final long ID_2 = 2l;
+    private final long ID_3 = 3l;
+    private final long NOT_EXISTTING_TARGET = 999l;
+    private final long UNKNOWN_CATEGORY_PROBE = 4l;
+
+    @Before
+    public void setup() throws CommunicationException, TopologyProcessorException {
+        topologyProcessor = Mockito.mock(TopologyProcessor.class);
+        targetResolver = new ActionTargetByProbeCategoryResolver(topologyProcessor);
+        final TargetInfo target1 = createTarget(ID_1);
+        final TargetInfo target2 = createTarget(ID_2);
+        final TargetInfo target3 = createTarget(ID_3);
+        final TargetInfo unknownCategoryTarget = createTarget(UNKNOWN_CATEGORY_PROBE);
+        // Curently STORAGE has priority 2
+        final ProbeInfo probe1 = createProbe("STORAGE");
+        // It has 1 priority
+        final ProbeInfo probe2 = createProbe("LOAD BALANCER");
+        // It has 8 priority
+        final ProbeInfo probe3 = createProbe("FLOW");
+        final ProbeInfo unknownCategoryProbe = createProbe("UNKNOWN"); // We haven't this category
+        Mockito.when(topologyProcessor.getTarget(ID_1)).thenReturn(target1);
+        Mockito.when(topologyProcessor.getTarget(ID_2)).thenReturn(target2);
+        Mockito.when(topologyProcessor.getTarget(ID_3)).thenReturn(target3);
+        Mockito.when(topologyProcessor.getTarget(NOT_EXISTTING_TARGET))
+                .thenThrow(TopologyProcessorException.class);
+        Mockito.when(topologyProcessor.getTarget(UNKNOWN_CATEGORY_PROBE)).thenReturn(unknownCategoryTarget);
+        Mockito.when(topologyProcessor.getProbe(ID_1)).thenReturn(probe1);
+        Mockito.when(topologyProcessor.getProbe(ID_2)).thenReturn(probe2);
+        Mockito.when(topologyProcessor.getProbe(ID_3)).thenReturn(probe3);
+        Mockito.when(topologyProcessor.getProbe(UNKNOWN_CATEGORY_PROBE)).thenReturn(unknownCategoryProbe);
+    }
+
+    @Nonnull
+    private TargetInfo createTarget(@Nonnull long probeId) {
+        final TargetInfo targetInfo = Mockito.mock(TargetInfo.class);
+        Mockito.when(targetInfo.getProbeId()).thenReturn(probeId);
+        return targetInfo;
+    }
+
+    @Nonnull
+    private ProbeInfo createProbe(@Nonnull String category) {
+        final ProbeInfo probeInfo = Mockito.mock(ProbeInfo.class);
+        Mockito.when(probeInfo.getCategory()).thenReturn(category);
+        return probeInfo;
+    }
+
+    /**
+     * Tests resolving target for the conflict when three different targets can execute it.
+     *
+     * @throws TargetResolutionException when action was null of provided targets set was null or
+     * empty
+     */
+    @Test
+    public void testResolveTarget() throws TargetResolutionException {
+        final ActionDTO.Action action = createAction();
+        final long resolvedTarget = targetResolver.resolveExecutantTarget(action,
+                ImmutableSet.of(ID_1, ID_2, ID_3));
+        Assert.assertEquals(ID_2, resolvedTarget);
+    }
+
+    /**
+     * Tests resolving the target for the conflict when provided not existed targetId.
+     *
+     * @throws TargetResolutionException when action was null of provided targets set was null or
+     * empty
+     */
+    @Test
+    public void testResolveTargetWithNotExistingTarget() throws TargetResolutionException {
+        final ActionDTO.Action action = createAction();
+        final long resolvedTarget = targetResolver.resolveExecutantTarget(action,
+                ImmutableSet.of(ID_1, ID_2, ID_3, NOT_EXISTTING_TARGET));
+        Assert.assertEquals(ID_2, resolvedTarget);
+    }
+
+    /**
+     * Tests resolving the target when provided provided the target with unknown probe category
+     *
+     * @throws TargetResolutionException when action was null of provided targets set was null or
+     * empty
+     */
+    @Test
+    public void testResolveTargetOfUnknownProbe() throws TargetResolutionException {
+        final ActionDTO.Action action = createAction();
+        final long resolvedTarget = targetResolver.resolveExecutantTarget(action,
+                ImmutableSet.of(UNKNOWN_CATEGORY_PROBE, ID_3));
+        Assert.assertEquals(ID_3, resolvedTarget);
+    }
+
+    private Action createAction() {
+        return Action.newBuilder().setInfo(ActionInfo.newBuilder()
+                .setReconfigure(Reconfigure.newBuilder().setTargetId(1).build())
+                .build()).setImportance(1).setExplanation(Explanation.newBuilder().build())
+                .setId(1).build();
+    }
+}
