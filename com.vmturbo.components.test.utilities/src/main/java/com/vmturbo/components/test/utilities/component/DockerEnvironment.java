9ec98c5989b949f5fcc8e4d51e61a2c3a0fbbd4b
david_blinn <david.blinn@turbonomic.com>
2018-02-02 17:51:24 -0500

    OM-30496: Add DB_MEM_PCT_FOR_BUFFER_POOL to DockerEnvironment RB:21426
    * The performance tests were looking for this environment variable and failed to run without it.
    
    TestingDone:
    * Performance tests now run locally

--- com.vmturbo.components.test.utilities/src/main/java/com/vmturbo/components/test/utilities/component/DockerEnvironment.java
+++ com.vmturbo.components.test.utilities/src/main/java/com/vmturbo/components/test/utilities/component/DockerEnvironment.java
@@ -113,6 +113,7 @@ public class DockerEnvironment {
             .put("MEDIATION_COMPELLENT_DEBUG_PORT", "8000")
             // MEMORY LIMITS and XMX Settings
             .put("DB_MEM_LIMIT_MB", "2048")
+            .put("DB_MEM_PCT_FOR_BUFFER_POOL", "80")
             .put("ARANGODB_MEM_LIMIT_MB", "1024")
             .put("AUTH_MEM_LIMIT_MB", "768")
             .put("AUTH_XMX_MB", "512")
