5bf45d46144fe50c5f6834ecb9515d2219c9bf24
patrick_soohoo <patrick.soohoo@turbonomic.com>
2018-02-15 11:09:11 -0500

    OM-31095: Catch kafka consumer start IllegalStateExceptions

--- com.vmturbo.components.api/src/main/java/com/vmturbo/components/api/client/KafkaConsumerStarter.java
+++ com.vmturbo.components.api/src/main/java/com/vmturbo/components/api/client/KafkaConsumerStarter.java
@@ -1,5 +1,7 @@
 package com.vmturbo.components.api.client;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -9,13 +11,28 @@ import org.springframework.context.event.ContextRefreshedEvent;
  * {@link KafkaMessageConsumer} instances after application context is started up.
  */
 public class KafkaConsumerStarter implements ApplicationListener<ContextRefreshedEvent> {
+    private final Logger logger = LogManager.getLogger();
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         final ApplicationContext context = event.getApplicationContext();
         for (KafkaMessageConsumer consumer : context.getBeansOfType(KafkaMessageConsumer.class)
                 .values()) {
-            consumer.start();
+            try {
+                logger.info("Starting Kafka consumer.");
+                consumer.start();
+            } catch (IllegalStateException ise) {
+                // This is a harmless exception in this situation. We expect these from time to time
+                // because multiple ContextRefreshedEvents over the course of a component instance
+                // is a supported case (i.e. configuration changes can trigger these). They are
+                // also the norm in the API component, which has two spring contexts firing events.
+                //
+                // In other applications using our kafka libs, the consumer.start() may not be tied
+                // to spring context lifecycle events, and the redundant start in that kind of case
+                // could be a sign of something more troubling. But here, we'll just log a message
+                // and continue on our merry way.
+                logger.info("Kafka consumer already started.");
+            }
         }
     }
 }
