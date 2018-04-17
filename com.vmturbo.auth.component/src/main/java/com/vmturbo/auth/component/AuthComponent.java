16ebc3cbb6e65d9805f4c0a87e2ed46f967095f2
Pavel Baranchikov <pavel.baranchikov@turbonomic.com>
2018-04-17 18:41:03 +0300

    OM-34251: Disabled SpringBoot in Auth component

--- com.vmturbo.auth.component/src/main/java/com/vmturbo/auth/component/AuthComponent.java
+++ com.vmturbo.auth.component/src/main/java/com/vmturbo/auth/component/AuthComponent.java
@@ -5,36 +5,29 @@ import java.util.Optional;
 import javax.annotation.Nonnull;
 import javax.annotation.PostConstruct;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.ServerInterceptors;
+
+import me.dinowernli.grpc.prometheus.MonitoringServerInterceptor;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
-import io.grpc.ServerInterceptors;
-import me.dinowernli.grpc.prometheus.MonitoringServerInterceptor;
 
 import com.vmturbo.auth.api.SpringSecurityConfig;
 import com.vmturbo.auth.api.authorization.jwt.JwtServerInterceptor;
 import com.vmturbo.auth.component.widgetset.WidgetsetConfig;
 import com.vmturbo.components.common.BaseVmtComponent;
-import com.vmturbo.components.common.BaseVmtComponentConfig;
 import com.vmturbo.components.common.health.sql.MariaDBHealthMonitor;
 
 /**
  * The main auth component.
  */
 @Configuration("theComponent")
-@EnableAutoConfiguration
-@EnableDiscoveryClient
-@ComponentScan({"com.vmturbo.auth.component.services", "com.vmturbo.auth.component.store"})
 @Import({AuthRESTSecurityConfig.class, AuthDBConfig.class, SpringSecurityConfig.class,
         WidgetsetConfig.class})
 public class AuthComponent extends BaseVmtComponent {
@@ -42,12 +35,6 @@ public class AuthComponent extends BaseVmtComponent {
      * The logger.
      */
     private final Logger logger = LogManager.getLogger(AuthComponent.class);
-
-    /**
-     * The component name.
-     */
-    @Value("${spring.application.name}")
-    private String componentName;
 
     @Value("${mariadbHealthCheckIntervalSeconds:60}")
     private int mariaHealthCheckIntervalSeconds;
@@ -81,23 +68,7 @@ public class AuthComponent extends BaseVmtComponent {
      * @param args The mandatory arguments.
      */
     public static void main(String[] args) {
-        // apply the configuration properties for this component prior to Spring instantiation
-        fetchConfigurationProperties();
-        // instantiate and run this component
-        new SpringApplicationBuilder()
-                .sources(AuthComponent.class)
-                .sources(BaseVmtComponentConfig.class)
-                .run(args);
-    }
-
-    /**
-     * Returns the component name.
-     *
-     * @return The component name.
-     */
-    @Override
-    public String getComponentName() {
-        return componentName;
+        startContext(AuthComponent.class);
     }
 
     @Override
