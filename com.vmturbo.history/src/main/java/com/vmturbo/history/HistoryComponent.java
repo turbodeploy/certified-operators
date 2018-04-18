79c495974a47883fbebc60c95c0a58e0d420c7bc
Pavel Baranchikov <pavel.baranchikov@turbonomic.com>
2018-04-18 10:03:17 +0300

    OM-34251: Disabled SpringBoot in History component

--- com.vmturbo.history/src/main/java/com/vmturbo/history/HistoryComponent.java
+++ com.vmturbo.history/src/main/java/com/vmturbo/history/HistoryComponent.java
@@ -5,22 +5,18 @@ import java.util.Optional;
 import javax.annotation.Nonnull;
 import javax.annotation.PostConstruct;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.ServerInterceptors;
+
+import me.dinowernli.grpc.prometheus.MonitoringServerInterceptor;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
-import io.grpc.ServerInterceptors;
-import me.dinowernli.grpc.prometheus.MonitoringServerInterceptor;
 
 import com.vmturbo.components.common.BaseVmtComponent;
 import com.vmturbo.components.common.health.sql.MariaDBHealthMonitor;
@@ -33,16 +29,11 @@ import com.vmturbo.history.stats.StatsConfig;
 import com.vmturbo.history.topology.TopologyListenerConfig;
 
 @Configuration("theComponent")
-@EnableAutoConfiguration
-@EnableDiscoveryClient
 @Import({HistoryDbConfig.class, TopologyListenerConfig.class, MarketListenerConfig.class,
         StatsConfig.class, HistoryApiConfig.class, ApiSecurityConfig.class})
 public class HistoryComponent extends BaseVmtComponent {
 
     private final static Logger log = LogManager.getLogger();
-
-    @Value("${spring.application.name}")
-    private String componentName;
 
     @Autowired
     private HistoryDbConfig historyDbConfig;
@@ -73,17 +64,7 @@ public class HistoryComponent extends BaseVmtComponent {
     }
 
     public static void main(String[] args) {
-        // apply the configuration properties for this component prior to Spring instantiation
-        fetchConfigurationProperties();
-        // instantiate and run this component
-        new SpringApplicationBuilder()
-                .sources(HistoryComponent.class)
-                .run(args);
-    }
-
-    @Override
-    public String getComponentName() {
-        return componentName;
+        startContext(HistoryComponent.class);
     }
 
     @Override
@@ -96,15 +77,5 @@ public class HistoryComponent extends BaseVmtComponent {
         return Optional.of(builder
             .addService(ServerInterceptors.intercept(statsConfig.statsRpcService(), monitoringInterceptor))
             .build());
-    }
-
-    /**
-     * This bean performs registration of all configured websocket endpoints.
-     *
-     * @return bean
-     */
-    @Bean
-    public ServerEndpointExporter endpointExporter() {
-        return new ServerEndpointExporter();
     }
 }
