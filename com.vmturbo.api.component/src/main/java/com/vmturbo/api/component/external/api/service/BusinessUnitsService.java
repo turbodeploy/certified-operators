10e0c277829be53df51513654a53ae23d41525c3
vmtbuild <billy.oconnell@turbonomic.com>
2018-06-05 15:54:39 -0400

    Reverted from xl-develop-ci-build-785 and reopened jira issues ( )

--- com.vmturbo.api.component/src/main/java/com/vmturbo/api/component/external/api/service/BusinessUnitsService.java
+++ com.vmturbo.api.component/src/main/java/com/vmturbo/api/component/external/api/service/BusinessUnitsService.java
@@ -1,27 +1,25 @@
 package com.vmturbo.api.component.external.api.service;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 import org.springframework.validation.Errors;
+
+import com.google.common.collect.ImmutableSet;
 
 import com.vmturbo.api.component.external.api.util.ApiUtils;
 import com.vmturbo.api.dto.businessunit.BusinessUnitApiDTO;
 import com.vmturbo.api.dto.businessunit.BusinessUnitApiInputDTO;
 import com.vmturbo.api.dto.group.GroupApiDTO;
-import com.vmturbo.api.enums.BusinessUnitType;
-import com.vmturbo.api.enums.HierarchicalRelationship;
 import com.vmturbo.api.serviceinterfaces.IBusinessUnitsService;
 
 public class BusinessUnitsService implements IBusinessUnitsService {
 
     @Override
-    public List<BusinessUnitApiDTO> getBusinessUnits(@Nullable BusinessUnitType type,
-                    @Nullable String cloudType, @Nullable Boolean hasParent) throws Exception {
-        // TODO OM-35804 implement required behavior
+    public List<BusinessUnitApiDTO> getBusinessUnits() throws Exception {
         throw ApiUtils.notImplementedInXL();
     }
 
@@ -54,14 +52,6 @@ public class BusinessUnitsService implements IBusinessUnitsService {
 
     @Override
     public void validateInput(final Object o, final Errors errors) {
-        throw ApiUtils.notImplementedInXL();
-    }
-
-    @Nonnull
-    @Override
-    public Collection<BusinessUnitApiDTO> getRelatedBusinessUnits(@Nonnull String uuid,
-                    HierarchicalRelationship relationship) throws Exception {
-        // TODO OM-35804 implement required behavior
         throw ApiUtils.notImplementedInXL();
     }
 
