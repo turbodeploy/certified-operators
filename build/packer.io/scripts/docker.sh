5727822168c38d69a1a67bff9f0ebefd8f8de935
vmtbuild <billy.oconnell@turbonomic.com>
2018-06-20 11:53:49 -0400

    Reverted from xl-develop-ci-build-830 and reopened jira issues (OM-36181 )

--- build/packer.io/scripts/docker.sh
+++ build/packer.io/scripts/docker.sh
@@ -35,8 +35,7 @@ export HOST_IP=$(/sbin/ip route get 1 | awk '{print $NF;exit}')
 #    so that other components will all be running before the user-facing UI comes up
 XL_COMPONENTS="auth topology-processor market repository plan-orchestrator action-orchestrator group \
         history reporting mediation-hyperv mediation-vcenter mediation-netapp mediation-ucs \
-        mediation-vmax mediation-vmm mediation-hpe3par mediation-pure mediation-scaleio \
-        mediation-hds mediation-compellent mediation-xtremio mediation-vplex \
+        mediation-vmax mediation-vmm mediation-hpe3par \
         api"
 # list of "support" containers to start
 BASE_IMAGES="nginx rsyslog consul clustermgr db arangodb"
