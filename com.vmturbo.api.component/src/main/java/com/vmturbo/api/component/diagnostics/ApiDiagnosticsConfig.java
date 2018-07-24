f83612c7f409031c4df724e40bbf6cbacdd857c4
david_blinn <david.blinn@turbonomic.com>
2018-07-24 14:25:56 -0400

    OM-37360: Add telemetry metrics to XL RB:25156
    * For related reviews in the classic platform, see reviews 24770 (version info), 24910 (entity counts), 24964 (api call counts)
    
    ** Intent **
    * Track metrics for version, service entity counts, and API call counts and latency to help us better understand how our product is used.
    * Only track this information if customers opt in to telemetry.
    
    ** Version Information **
    * Capture turbonomic platform version and revision
    
    Example:
    turbonomic_version_and_revision{version_name="Turbonomic Operations Manager 7.4.0",revision="20180730100910000",} 1.0
    
    ** Entity counts **
    * Capture the number of each kind of service entity in the topology.
    * The counts captured should match what the customer sees in the global supply chain in the UI.
    
    Example:
    service_entity_count{service_entity_name="Storage",} 3.0
    service_entity_count{service_entity_name="DataCenter",} 1.0
    service_entity_count{service_entity_name="Application",} 54.0
    service_entity_count{service_entity_name="Network",} 9.0
    service_entity_count{service_entity_name="VirtualMachine",} 54.0
    
    ** API call counts **
    * Classification for number of API calls is done based on
    1. URI e.g. /entities/{uuid}/groups. NOTE : we don't consider the actual uuid while classifying requests. So the request URI /entities/ABCD/groups and /entities/EFGH/groups are same.
    2. The method type. (GET, POST).
    3. If the request is coming from browser or a curl command.
    4. if the processing was successful.
    
    To do this, reuse the TelemetryInterceptor originally created for the classic platform.
    
    Example:
    api_call_counts{method="GET",uri="/supplychains",from_browser="true",failed="false",} 3.0
    api_call_latency_in_seconds_bucket{method="GET",uri="/supplychains",le="0.01",} 0.0
    api_call_latency_in_seconds_bucket{method="GET",uri="/supplychains",le="0.05",} 1.0
    api_call_latency_in_seconds_bucket{method="GET",uri="/supplychains",le="0.2",} 1.0
    api_call_latency_in_seconds_bucket{method="GET",uri="/supplychains",le="1.0",} 3.0
    api_call_latency_in_seconds_bucket{method="GET",uri="/supplychains",le="3.0",} 3.0
    api_call_latency_in_seconds_bucket{method="GET",uri="/supplychains",le="10.0",} 3.0
    api_call_latency_in_seconds_bucket{method="GET",uri="/supplychains",le="60.0",} 3.0
    api_call_latency_in_seconds_bucket{method="GET",uri="/supplychains",le="+Inf",} 3.0
    api_call_latency_in_seconds_count{method="GET",uri="/supplychains",} 3.0
    api_call_latency_in_seconds_sum{method="GET",uri="/supplychains",} 1.4641972

---
+++ com.vmturbo.api.component/src/main/java/com/vmturbo/api/component/diagnostics/ApiDiagnosticsConfig.java
@@ -0,0 +1,46 @@
+package com.vmturbo.api.component.diagnostics;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import com.vmturbo.api.component.communication.CommunicationConfig;
+import com.vmturbo.api.component.external.api.service.ServiceConfig;
+import com.vmturbo.components.common.DiagnosticsWriter;
+
+/**
+ * Configuration for group component diagnostics.
+ */
+@Configuration
+@Import({
+    CommunicationConfig.class,
+    ServiceConfig.class
+})
+public class ApiDiagnosticsConfig {
+
+    @Autowired
+    private CommunicationConfig communicationConfig;
+
+    @Autowired
+    private ServiceConfig serviceConfig;
+
+    @Bean
+    public DiagnosticsWriter diagnosticsWriter() {
+        return new DiagnosticsWriter();
+    }
+
+    @Bean
+    public ApiDiagnosticsHandler diagsHandler() {
+        return new ApiDiagnosticsHandler(communicationConfig.supplyChainFetcher(),
+            serviceConfig.adminService(),
+            communicationConfig.clusterMgr(),
+            diagnosticsWriter(),
+            communicationConfig.getRealtimeTopologyContextId());
+    }
+
+    @Bean
+    public ApiDiagnosticsController diagnosticsController() {
+        return new ApiDiagnosticsController(diagsHandler());
+    }
+}
