6a6db8ab38cfd6f89d1d810d7e60be8a506646b8
zhengbin_xiang <zhengbin.xiang@turbonomic.com>
2018-09-07 16:33:56 -0400

    OM-38092: RB:25845 Add persist part about reserved instance utilization and coverage

---
+++ com.vmturbo.cost.component/src/main/java/com/vmturbo/cost/component/reserved/instance/ReservedInstanceCoverageStore.java
@@ -0,0 +1,85 @@
+package com.vmturbo.cost.component.reserved.instance;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.annotation.Nonnull;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jooq.DSLContext;
+
+import com.google.common.collect.Lists;
+
+import com.vmturbo.cost.component.db.Tables;
+import com.vmturbo.cost.component.db.tables.records.ReservedInstanceCoverageLatestRecord;
+
+/**
+ * This class is used to store entity reserved instance coupons coverage information into database.
+ * And it used the data which comes from real time topology which contains the total coupons for
+ * each entity, and also used the {@link EntityReservedInstanceMappingStore} which has the latest
+ * used coupons for each entity. And it will combine these data and store into database.
+ */
+public class ReservedInstanceCoverageStore {
+
+    private final static Logger logger = LogManager.getLogger();
+
+    //TODO: set this chunk config through consul.
+    private final static int chunkSize = 1000;
+
+    private final DSLContext dsl;
+
+    private final EntityReservedInstanceMappingStore entityReservedInstanceMappingStore;
+
+    public ReservedInstanceCoverageStore(
+            @Nonnull final DSLContext dsl,
+            @Nonnull final EntityReservedInstanceMappingStore entityReservedInstanceMappingStore) {
+        this.dsl = dsl;
+        this.entityReservedInstanceMappingStore = entityReservedInstanceMappingStore;
+    }
+
+    /**
+     * Given a list of {@link ServiceEntityReservedInstanceCoverage} and current entity reserved
+     * instance mapping information, combine them together and store into database.
+     *
+     * @param context {@link DSLContext} transactional context.
+     * @param entityRiCoverages a list {@link ServiceEntityReservedInstanceCoverage}.
+     */
+    public void updateReservedInstanceCoverageStore(
+            @Nonnull final DSLContext context,
+            @Nonnull final List<ServiceEntityReservedInstanceCoverage> entityRiCoverages) {
+        final LocalDateTime currentTime = LocalDateTime.now(ZoneOffset.UTC);
+        final Map<Long, Double> entityRiUsedCouponsMap =
+                entityReservedInstanceMappingStore.getEntityUsedCouponsMap(context);
+        List<ReservedInstanceCoverageLatestRecord> riCoverageRecords = entityRiCoverages.stream()
+                .map(entityRiCoverage -> createReservedInstanceCoverageRecord(context, currentTime,
+                        entityRiCoverage, entityRiUsedCouponsMap))
+                .collect(Collectors.toList());
+        Lists.partition(riCoverageRecords, chunkSize).forEach(entityChunk ->
+                context.batchInsert(entityChunk).execute());
+    }
+
+    /**
+     * Create {@link ReservedInstanceCoverageLatestRecord}.
+     *
+     * @param context {@link DSLContext} transactional context.
+     * @param currentTime the current time.
+     * @param entityRiCoverage {@link ServiceEntityReservedInstanceCoverage}.
+     * @param entityRiUsedCouponsMap a map which key is entity id, value is the used coupons of the entity.
+     * @return
+     */
+    private ReservedInstanceCoverageLatestRecord createReservedInstanceCoverageRecord(
+            @Nonnull final DSLContext context,
+            @Nonnull final LocalDateTime currentTime,
+            @Nonnull final ServiceEntityReservedInstanceCoverage entityRiCoverage,
+            @Nonnull final Map<Long, Double> entityRiUsedCouponsMap) {
+        return context.newRecord(Tables.RESERVED_INSTANCE_COVERAGE_LATEST,
+                new ReservedInstanceCoverageLatestRecord(currentTime, entityRiCoverage.getId(),
+                        entityRiCoverage.getRegionId(), entityRiCoverage.getAvailabilityZoneId(),
+                        entityRiCoverage.getBusinessAccountId(), entityRiCoverage.getTotalCoupons(),
+                        entityRiUsedCouponsMap.getOrDefault(entityRiCoverage.getId(), 0.0)));
+    }
+}
---
+++ com.vmturbo.cost.component/src/test/java/com/vmturbo/cost/component/reserved/instance/ReservedInstanceCoverageStoreTest.java
@@ -0,0 +1,138 @@
+package com.vmturbo.cost.component.reserved.instance;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.flywaydb.core.Flyway;
+import org.jooq.DSLContext;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.vmturbo.cost.component.db.Tables;
+import com.vmturbo.cost.component.db.tables.records.EntityToReservedInstanceMappingRecord;
+import com.vmturbo.cost.component.db.tables.records.ReservedInstanceCoverageLatestRecord;
+import com.vmturbo.cost.component.identity.IdentityProvider;
+import com.vmturbo.sql.utils.TestSQLDatabaseConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+        classes = {TestSQLDatabaseConfig.class}
+)
+@TestPropertySource(properties = {"originalSchemaName=cost"})
+public class ReservedInstanceCoverageStoreTest {
+
+    private final static double DELTA = 0.000001;
+
+    @Autowired
+    protected TestSQLDatabaseConfig dbConfig;
+
+    private Flyway flyway;
+
+    private EntityReservedInstanceMappingStore entityReservedInstanceMappingStore;
+
+    private ReservedInstanceCoverageStore reservedInstanceCoverageStore;
+
+    private ReservedInstanceBoughtStore reservedInstanceBoughtStore;
+
+    private DSLContext dsl;
+
+    private final ServiceEntityReservedInstanceCoverage firstEntity =
+            ServiceEntityReservedInstanceCoverage.newBuilder()
+                    .setId(123)
+                    .setBusinessAccountId(12345L)
+                    .setAvailabilityZoneId(100L)
+                    .setRegionId(101L)
+                    .setTotalCoupons(100)
+                    .build();
+    private final ServiceEntityReservedInstanceCoverage secondEntity =
+            ServiceEntityReservedInstanceCoverage.newBuilder()
+                    .setId(124)
+                    .setBusinessAccountId(12345L)
+                    .setAvailabilityZoneId(100L)
+                    .setRegionId(101L)
+                    .setTotalCoupons(200)
+                    .build();
+    private final ServiceEntityReservedInstanceCoverage thirdEntity =
+            ServiceEntityReservedInstanceCoverage.newBuilder()
+                    .setId(125)
+                    .setBusinessAccountId(12345L)
+                    .setAvailabilityZoneId(100L)
+                    .setRegionId(101L)
+                    .setTotalCoupons(50)
+                    .build();
+
+    @Before
+    public void setup() throws Exception {
+        flyway = dbConfig.flyway();
+        dsl = dbConfig.dsl();
+        flyway.clean();
+        flyway.migrate();
+        reservedInstanceBoughtStore = new ReservedInstanceBoughtStore(dsl,
+                new IdentityProvider(0));
+        entityReservedInstanceMappingStore = new EntityReservedInstanceMappingStore(dsl,
+                reservedInstanceBoughtStore);
+        reservedInstanceCoverageStore = new ReservedInstanceCoverageStore(dsl,
+                entityReservedInstanceMappingStore);
+        insertDefaultEntityReservedInstanceMapping();
+    }
+
+    @Test
+    public void testUpdateReservedInstanceCoverageStore() {
+        reservedInstanceCoverageStore.updateReservedInstanceCoverageStore(dsl,
+                Arrays.asList(firstEntity, secondEntity, thirdEntity));
+        final List<ReservedInstanceCoverageLatestRecord> records =
+                dsl.selectFrom(Tables.RESERVED_INSTANCE_COVERAGE_LATEST).fetch();
+        assertEquals(3, records.size());
+        final Optional<ReservedInstanceCoverageLatestRecord> firstRecord = records.stream()
+                .filter(ri -> ri.getEntityId().equals(123L))
+                .findFirst();
+        final Optional<ReservedInstanceCoverageLatestRecord> secondRecord = records.stream()
+                .filter(ri -> ri.getEntityId().equals(124L))
+                .findFirst();
+        final Optional<ReservedInstanceCoverageLatestRecord> thirdRecord = records.stream()
+                .filter(ri -> ri.getEntityId().equals(125L))
+                .findFirst();
+        assertTrue(firstRecord.isPresent());
+        assertTrue(secondRecord.isPresent());
+        assertTrue(thirdRecord.isPresent());
+        assertEquals(100.0, firstRecord.get().getTotalCoupons(), DELTA);
+        assertEquals(30.0, firstRecord.get().getUsedCoupons(), DELTA);
+        assertEquals(200.0, secondRecord.get().getTotalCoupons(), DELTA);
+        assertEquals(80.0, secondRecord.get().getUsedCoupons(), DELTA);
+        assertEquals(50.0, thirdRecord.get().getTotalCoupons(), DELTA);
+        assertEquals(0.0, thirdRecord.get().getUsedCoupons(), DELTA);
+
+    }
+
+    private void insertDefaultEntityReservedInstanceMapping() {
+        final LocalDateTime curTime = LocalDateTime.now(ZoneOffset.UTC);
+        final EntityToReservedInstanceMappingRecord firstRecord =
+                dsl.newRecord(Tables.ENTITY_TO_RESERVED_INSTANCE_MAPPING,
+                        new EntityToReservedInstanceMappingRecord(curTime, 123L,
+                                11L, 10.0));
+        final EntityToReservedInstanceMappingRecord secondRecord =
+                dsl.newRecord(Tables.ENTITY_TO_RESERVED_INSTANCE_MAPPING,
+                        new EntityToReservedInstanceMappingRecord(curTime, 123L,
+                                12L, 20.0));
+        final EntityToReservedInstanceMappingRecord thirdRecord =
+                dsl.newRecord(Tables.ENTITY_TO_RESERVED_INSTANCE_MAPPING,
+                        new EntityToReservedInstanceMappingRecord(curTime, 124L,
+                                11L, 30.0));
+        final EntityToReservedInstanceMappingRecord fourthRecord =
+                dsl.newRecord(Tables.ENTITY_TO_RESERVED_INSTANCE_MAPPING,
+                        new EntityToReservedInstanceMappingRecord(curTime, 124L,
+                                13L, 50.0));
+        dsl.batchInsert(Arrays.asList(firstRecord, secondRecord, thirdRecord, fourthRecord)).execute();
+    }
+}
