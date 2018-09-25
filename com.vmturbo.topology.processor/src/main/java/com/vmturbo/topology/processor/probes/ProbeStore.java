98935863e8f7bdad342303c0a2eec56bb0380814
karthik_thyagaraja <karthik.thyagaraja@turbonomic.com>
2018-09-25 11:24:33 -0400

    OM-38783 - Upgrade vCenter VM entity identity records stored in TP with new heuristic property. RB: 25979
    The heuristic properties for vCenter VM entities was changed by this commit in
    the SDK: https://rbcommons.com/s/VMTurbo/r/24982/diff/5/#2. In XL, we have to upgrade
    the probeInfo and the entity identifying properties to include these changes.
    
    The upgrade is done in two phases:
    1) During the 1st phase, the probe info is updated to include the new fields and
    the entities are updated to have the new heuristic property with a dummy value.
    As we currently don't store entityType, we can't decide which entity to update.
    So we add a new entity_type column to the the assigned_identity table and then
    populate it by fetching the entityTypes from history component. For those entities
    in the assigned_identity table for which we couldn't find the entityType in
    history component, we delete them.
    2) In the 2nd phase, during the normal discovery of the entities, when the
    heuristic properties match, we replace the dummy value with the actual value.
    
    The migration added a new flyway script and it's version number(V_2_3) is the same
    as the version number for the create_targetspec_table script. Since the
    create_targetspec_table script was added in develop(higher version) branch,
    we are renaming this flyway script to have a higher version number.

--- com.vmturbo.topology.processor/src/main/java/com/vmturbo/topology/processor/probes/ProbeStore.java
+++ com.vmturbo.topology.processor/src/main/java/com/vmturbo/topology/processor/probes/ProbeStore.java
@@ -70,6 +70,16 @@ public interface ProbeStore {
     Optional<Long> getProbeIdForType(@Nonnull final String probeTypeName);
 
     /**
+     * Retrieve probe info by probe type name.
+     *
+     * @param probeTypeName Name of the probe to retrieve.
+     * @return The ID of the probe with the given type, or an empty Optional if no probe
+     *         with the given type is registered.
+     */
+    Optional<ProbeInfo> getProbeInfoForType(@Nonnull final String probeTypeName);
+
+
+    /**
      * Retrieve probe ids for a probe category.
      *
      * @param probeCategory the category of the probe whose ids should be retrieved.
@@ -117,7 +127,7 @@ public interface ProbeStore {
      * Returns a boolean to indicate whether the probe with the given probe ID is connected.
      *
      * @param probeId Probe ID
-     * @return ture if probe is connected, false otherwise.
+     * @return true if probe is connected, false otherwise.
      */
     boolean isProbeConnected(@Nonnull Long probeId);
 
@@ -126,4 +136,11 @@ public interface ProbeStore {
      * @return {@link ProbeOrdering} for this ProbeStore
      */
     ProbeOrdering getProbeOrdering();
+
+    /**
+     * Update the existing probe with the new probe info.
+     * @param updatedProbeInfo New probeInfo for the probe.
+     *
+     */
+    void updateProbeInfo(@Nonnull ProbeInfo updatedProbeInfo) throws ProbeException;
 }
