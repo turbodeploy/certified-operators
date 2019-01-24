269aba047aa2836c10df5a1ab5d4ffd04d44b033
gary_zeng <gary.zeng@turbonomic.com>
2019-01-24 16:36:34 -0500

    OM-42364: Fix user with "Deployer" or "Observer" role can access/see plan icon in the navigation bar (rb28505)
    Problem:
    
    In XL, user with "Deployer" or "Observer" role can access/see plan icon in the navigation bar.
    
    Investigation:
    
    XL already has the permissions properly defined in restricted.view.xl.json file, e.g.:
    
    "componentId": "vmt-plan-nav-menu",
    "users": ["observer", "deployer"],
    "restricted": true
    
    But the permissions were overwritten when the UI trying to restrict views based on license features!
    The problem doesn't happen in classic because endpoint "/licenses/summary" is protected (see restrictedView.service.ts -> _putLicenseItem) for details.
    
    Proposed solution:
    
    Align with classic, remove endpoint "/licenses/summary" from unprotected list.

--- com.vmturbo.api.component/src/main/java/com/vmturbo/api/component/external/api/ApiSecurityConfig.java
+++ com.vmturbo.api.component/src/main/java/com/vmturbo/api/component/external/api/ApiSecurityConfig.java
@@ -214,7 +214,6 @@ public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers(base_uri + "users/saml").permitAll()
                     .antMatchers(base_uri + "admin/versions").permitAll()
                     .antMatchers(base_uri + "license").permitAll()
-                    .antMatchers(base_uri + "licenses/summary").permitAll()
                     .antMatchers(base_uri + "initAdmin").permitAll()
                     .antMatchers(base_uri + "saml/**").permitAll()
                     .antMatchers(base_uri + "**").authenticated();
