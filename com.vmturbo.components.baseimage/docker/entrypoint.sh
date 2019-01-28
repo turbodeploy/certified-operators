536d72ace6a199e48955431762e3534c6dce44fa
michael_borodiansky <michael.borodiansky@turbonomic.com>
2019-01-28 16:33:56 -0500

    OM-43118: Lower memory utilization by containers.
    RB: 28585

--- com.vmturbo.components.baseimage/docker/entrypoint.sh
+++ com.vmturbo.components.baseimage/docker/entrypoint.sh
@@ -4,13 +4,12 @@
 # The Djava.security.egd=file:/dev/./urandom configuration significantly speeds up start-up time
 # for the components using the SecureRandom class (see
 # http://stackoverflow.com/questions/25660899/spring-boot-actuator-application-wont-start-on-ubuntu-vps)
-COMMON_JAVA_OPTS="-XX:+UseG1GC -XX:CompileThreshold=1500 -XX:OnOutOfMemoryError=\"/dump_histogram.sh\" -XX:+ExitOnOutOfMemoryError"
-COMMON_JAVA_OPTS="-verbose:gc -XX:+PrintGCDateStamps $COMMON_JAVA_OPTS"
+# COMMON_JAVA_OPTS="-XX:+UseG1GC -XX:CompileThreshold=1500 -XX:OnOutOfMemoryError=\"/dump_histogram.sh\" -XX:+ExitOnOutOfMemoryError"
+# COMMON_JAVA_OPTS="-verbose:gc -XX:+PrintGCDateStamps $COMMON_JAVA_OPTS"
 COMMON_JAVA_OPTS="-XX:SoftRefLRUPolicyMSPerMB=0 -XX:+PrintConcurrentLocks -XX:+PrintClassHistogram $COMMON_JAVA_OPTS"
-COMMON_JAVA_OPTS="-XX:+PrintCommandLineFlags -XX:+UseStringDeduplication -XX:StringDeduplicationAgeThreshold=1 $COMMON_JAVA_OPTS"
+# COMMON_JAVA_OPTS="-XX:+PrintCommandLineFlags -XX:+UseStringDeduplication -XX:StringDeduplicationAgeThreshold=1 $COMMON_JAVA_OPTS"
 COMMON_JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Djava.net.preferIPv4Stack=true -XX:-OmitStackTraceInFastThrow $COMMON_JAVA_OPTS"
 COMMON_JAVA_OPTS="-Dnetworkaddress.cache.ttl=0 -Dnetworkaddress.cache.negative.ttl=0 $COMMON_JAVA_OPTS"
-COMMON_JAVA_OPTS="-DLog4jContextSelector=${LOG4J_CONTEXT_SELECTOR:-org.apache.logging.log4j.core.async.AsyncLoggerContextSelector} $COMMON_JAVA_OPTS"
 export STARTUP_COMMAND="java $COMMON_JAVA_OPTS $JAVA_OPTS $MORE_JAVA_OPTS -jar $@"
 
 # If LOG_TO_STDOUT is defined in the environment, tee the output so that it is also logged to stdout.
