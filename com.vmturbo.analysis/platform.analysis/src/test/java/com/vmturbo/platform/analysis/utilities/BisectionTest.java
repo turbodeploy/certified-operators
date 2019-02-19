5582e5dd39d82e76e4d9275ac7a1097ca58c28b0
Vaptistis Anogeianakis <vaptistis.anogeianakis@turbonomic.com>
2019-02-20 00:45:21 +0200

    OM-43371: Reimplemented Bisection.solve to fix 2 bugs and improve performance.
    There were two bugs in the old implementation:
    
    1. It didn't check for the possibility that the midpoint of the interval
       would happen to be a root. If you were unlucky enough for that to
       happen, it would skip the solution and converge to some arbitrary
       value.
    2. The closer the root was to 0, the more iterations it took to converge
       to the root. Although this is arguably a performance problem, it
       becomes a bug if you consider the fact that the function used to
       accept a 'maxIterations' argument and gave up after that many
       iterations.
    
       Since the client had no way to know the correct number of iterations
       (Integer.MAX_VALUE wasn't always enough either), the method could
       give up too soon producing no value at all.
    
    The new implementation is loosely based on a Julia post
    (https://www.shapeoperator.com/2014/02/22/bisecting-floats/) and returns
    the closest representable double-precision floating-point value to the
    root in no more than 64 iterations.
    
    The above allowed me to simplify the API removing the epsilon,
    errorFunction and maxIterations parameters as well as the possibility of
    an exception.
    
    Although I didn't write and run proper benchmarks, the fact that the new
    implementation takes the same time to pass 105 tests that the old one
    needed to pass 52 is an indication that performance has been improved.
    
    Another indication is that the new implementation takes less than a
    millisecond to complete in all cases whilst the old one could take up to
    multiple seconds to complete or fail if the root was near zero.
    
    While reimplementing the method I also:
    
    * Improved documentation.
    * Changed parameter names to use domain (mathematics) terminology.
    * Updated clients to make use of the simplified API. That involved
      removing no-longer-used variables and constants and stale comments as
      well as removing a try-catch block and rewrapping lines to take
      advantage of the additional space left from lowering the indentation
      level.
    * Added two utility functions that implement an order-preserving mapping
      from doubles to longs and back with accompanying documentation. Tests
      for them will be added in a subsequent commit.

--- com.vmturbo.analysis/platform.analysis/src/test/java/com/vmturbo/platform/analysis/utilities/BisectionTest.java
+++ com.vmturbo.analysis/platform.analysis/src/test/java/com/vmturbo/platform/analysis/utilities/BisectionTest.java
@@ -2,12 +2,10 @@ package com.vmturbo.platform.analysis.utilities;
 
 import static java.lang.Math.E;
 import static java.lang.Math.PI;
-import static java.lang.Math.abs;
 import static java.lang.Math.exp;
 import static java.lang.Math.sin;
 import static org.junit.Assert.assertEquals;
 
-import java.util.function.DoubleBinaryOperator;
 import java.util.function.DoubleUnaryOperator;
 
 import org.junit.Test;
@@ -29,20 +27,18 @@ import com.vmturbo.platform.analysis.pricefunction.PriceFunction;
 public class BisectionTest {
     // Fields
     private static final double maxError = 1e-10;
-    private static final int maxIterations = Integer.MAX_VALUE;
-    private static final DoubleBinaryOperator errorFunction = (x, y) -> abs(x-y);
 
     // Methods
 
     /**
-     * Feeds {@link Bisection#solve(double, DoubleBinaryOperator, int, DoubleUnaryOperator, double, double)}
+     * Feeds {@link Bisection#solve(DoubleUnaryOperator, double, double)}
      * valid input and tests that it returns the unique root present in the interval.
      */
     @Test
     @Parameters
     @TestCaseName("Test #{index}: Bisection.solve({0},{1},{2}) == {3}")
     public final void testSolve_Positive_OneRoot(DoubleUnaryOperator function, double a, double b, double output) {
-        assertEquals(output, Bisection.solve(maxError, errorFunction, maxIterations, function, a, b), maxError);
+        assertEquals(output, Bisection.solve(function, a, b), maxError);
     }
 
     /**
@@ -60,11 +56,11 @@ public class BisectionTest {
     private static Object[] parametersForTestSolve_Positive_OneRoot() {
         return new Object[][]{
             // sin -- root == midpoint
-//            {(DoubleUnaryOperator)Math::sin,       -PI/2,   PI/2, 0.0},
+            {(DoubleUnaryOperator)Math::sin,       -PI/2,   PI/2, 0.0},
             {(DoubleUnaryOperator)Math::sin,     -3*PI/2,  -PI/2, -PI},
             {(DoubleUnaryOperator)Math::sin,        PI/2, 3*PI/2,  PI},
             // -sin -- root == midpoint
-//            {(DoubleUnaryOperator)x -> - sin(x),   -PI/2,   PI/2, 0.0},
+            {(DoubleUnaryOperator)x -> - sin(x),   -PI/2,   PI/2, 0.0},
             {(DoubleUnaryOperator)x -> - sin(x), -3*PI/2,  -PI/2, -PI},
             {(DoubleUnaryOperator)x -> - sin(x),    PI/2, 3*PI/2,  PI},
             // sin -- arbitrary root
@@ -72,86 +68,88 @@ public class BisectionTest {
             {(DoubleUnaryOperator)Math::sin, -4, -2, -PI},
             {(DoubleUnaryOperator)Math::sin,  3, 4, PI},
             // -sin -- arbitrary root
-//            {(DoubleUnaryOperator)x -> - sin(x), -2.5, 1.5, 0.0},
+            {(DoubleUnaryOperator)x -> - sin(x), -2.5, 1.5, 0.0},
             {(DoubleUnaryOperator)x -> - sin(x), -5.7, -2.1, -PI},
             {(DoubleUnaryOperator)x -> - sin(x), 0.001, 3.2,  PI},
             // exp -- 0/+
-//            {(DoubleUnaryOperator)x -> exp(x) - E, 0, 2, 1},
-//            {(DoubleUnaryOperator)x -> exp(x) - E, 0, Double.MAX_VALUE, 1},
-//            {(DoubleUnaryOperator)x -> exp(x) - E, 0, Double.POSITIVE_INFINITY, 1},
+            {(DoubleUnaryOperator)x -> exp(x) - E, 0, 2, 1},
+            {(DoubleUnaryOperator)x -> exp(x) - E, 0, Double.MAX_VALUE, 1},
+            {(DoubleUnaryOperator)x -> exp(x) - E, 0, Double.POSITIVE_INFINITY, 1},
             // exp -- -/0
             {(DoubleUnaryOperator)x -> exp(x) - 1/E, -100, 0, -1},
             {(DoubleUnaryOperator)x -> exp(x) - 1/E, -Double.MAX_VALUE, 0, -1},
-//            {(DoubleUnaryOperator)x -> exp(x) - 1/E, Double.NEGATIVE_INFINITY, 0, -1},
+            {(DoubleUnaryOperator)x -> exp(x) - 1/E, Double.NEGATIVE_INFINITY, 0, -1},
             // TODO: more concisely express using loops.
             // exp -- -/+ -- root == 1
             {(DoubleUnaryOperator)x -> exp(x) - E, -5, 2, 1},
             {(DoubleUnaryOperator)x -> exp(x) - E, -5, Double.MAX_VALUE, 1},
-//            {(DoubleUnaryOperator)x -> exp(x) - E, -5, Double.POSITIVE_INFINITY, 1},
-//            {(DoubleUnaryOperator)x -> exp(x) - E, -Double.MAX_VALUE, 2, 1},
-//            {(DoubleUnaryOperator)x -> exp(x) - E, -Double.MAX_VALUE, Double.MAX_VALUE, 1},
-//            {(DoubleUnaryOperator)x -> exp(x) - E, -Double.MAX_VALUE, Double.POSITIVE_INFINITY, 1},
-//            {(DoubleUnaryOperator)x -> exp(x) - E, Double.NEGATIVE_INFINITY, 2, 1},
-//            {(DoubleUnaryOperator)x -> exp(x) - E, Double.NEGATIVE_INFINITY, Double.MAX_VALUE, 1},
-//            {(DoubleUnaryOperator)x -> exp(x) - E, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 1},
+            {(DoubleUnaryOperator)x -> exp(x) - E, -5, Double.POSITIVE_INFINITY, 1},
+            {(DoubleUnaryOperator)x -> exp(x) - E, -Double.MAX_VALUE, 2, 1},
+            {(DoubleUnaryOperator)x -> exp(x) - E, -Double.MAX_VALUE, Double.MAX_VALUE, 1},
+            {(DoubleUnaryOperator)x -> exp(x) - E, -Double.MAX_VALUE, Double.POSITIVE_INFINITY, 1},
+            {(DoubleUnaryOperator)x -> exp(x) - E, Double.NEGATIVE_INFINITY, 2, 1},
+            {(DoubleUnaryOperator)x -> exp(x) - E, Double.NEGATIVE_INFINITY, Double.MAX_VALUE, 1},
+            {(DoubleUnaryOperator)x -> exp(x) - E, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 1},
             // exp -- -/+ -- root == -1
             {(DoubleUnaryOperator)x -> exp(x) - 1/E, -5, 2, -1},
             {(DoubleUnaryOperator)x -> exp(x) - 1/E, -5, Double.MAX_VALUE, -1},
-//            {(DoubleUnaryOperator)x -> exp(x) - 1/E, -5, Double.POSITIVE_INFINITY, -1},
+            {(DoubleUnaryOperator)x -> exp(x) - 1/E, -5, Double.POSITIVE_INFINITY, -1},
             {(DoubleUnaryOperator)x -> exp(x) - 1/E, -Double.MAX_VALUE, 2, -1},
-//            {(DoubleUnaryOperator)x -> exp(x) - 1/E, -Double.MAX_VALUE, Double.MAX_VALUE, -1},
-//            {(DoubleUnaryOperator)x -> exp(x) - 1/E, -Double.MAX_VALUE, Double.POSITIVE_INFINITY, -1},
-//            {(DoubleUnaryOperator)x -> exp(x) - 1/E, Double.NEGATIVE_INFINITY, 2, -1},
-//            {(DoubleUnaryOperator)x -> exp(x) - 1/E, Double.NEGATIVE_INFINITY, Double.MAX_VALUE, -1},
-//            {(DoubleUnaryOperator)x -> exp(x) - 1/E, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, -1},
+            {(DoubleUnaryOperator)x -> exp(x) - 1/E, -Double.MAX_VALUE, Double.MAX_VALUE, -1},
+            {(DoubleUnaryOperator)x -> exp(x) - 1/E, -Double.MAX_VALUE, Double.POSITIVE_INFINITY, -1},
+            {(DoubleUnaryOperator)x -> exp(x) - 1/E, Double.NEGATIVE_INFINITY, 2, -1},
+            {(DoubleUnaryOperator)x -> exp(x) - 1/E, Double.NEGATIVE_INFINITY, Double.MAX_VALUE, -1},
+            {(DoubleUnaryOperator)x -> exp(x) - 1/E, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, -1},
             // -exp -- -/+ -- root == 1
             {(DoubleUnaryOperator)x -> -exp(x) + E, -5, 2, 1},
             {(DoubleUnaryOperator)x -> -exp(x) + E, -5, Double.MAX_VALUE, 1},
-//            {(DoubleUnaryOperator)x -> -exp(x) + E, -5, Double.POSITIVE_INFINITY, 1},
-//            {(DoubleUnaryOperator)x -> -exp(x) + E, -Double.MAX_VALUE, 2, 1},
-//            {(DoubleUnaryOperator)x -> -exp(x) + E, -Double.MAX_VALUE, Double.MAX_VALUE, 1},
-//            {(DoubleUnaryOperator)x -> -exp(x) + E, -Double.MAX_VALUE, Double.POSITIVE_INFINITY, 1},
-//            {(DoubleUnaryOperator)x -> -exp(x) + E, Double.NEGATIVE_INFINITY, 2, 1},
-//            {(DoubleUnaryOperator)x -> -exp(x) + E, Double.NEGATIVE_INFINITY, Double.MAX_VALUE, 1},
-//            {(DoubleUnaryOperator)x -> -exp(x) + E, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 1},
+            {(DoubleUnaryOperator)x -> -exp(x) + E, -5, Double.POSITIVE_INFINITY, 1},
+            {(DoubleUnaryOperator)x -> -exp(x) + E, -Double.MAX_VALUE, 2, 1},
+            {(DoubleUnaryOperator)x -> -exp(x) + E, -Double.MAX_VALUE, Double.MAX_VALUE, 1},
+            {(DoubleUnaryOperator)x -> -exp(x) + E, -Double.MAX_VALUE, Double.POSITIVE_INFINITY, 1},
+            {(DoubleUnaryOperator)x -> -exp(x) + E, Double.NEGATIVE_INFINITY, 2, 1},
+            {(DoubleUnaryOperator)x -> -exp(x) + E, Double.NEGATIVE_INFINITY, Double.MAX_VALUE, 1},
+            {(DoubleUnaryOperator)x -> -exp(x) + E, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 1},
             // -exp -- -/+ -- root == -1
             {(DoubleUnaryOperator)x -> -exp(x) + 1/E, -5, 2, -1},
             {(DoubleUnaryOperator)x -> -exp(x) + 1/E, -5, Double.MAX_VALUE, -1},
-//            {(DoubleUnaryOperator)x -> -exp(x) + 1/E, -5, Double.POSITIVE_INFINITY, -1},
+            {(DoubleUnaryOperator)x -> -exp(x) + 1/E, -5, Double.POSITIVE_INFINITY, -1},
             {(DoubleUnaryOperator)x -> -exp(x) + 1/E, -Double.MAX_VALUE, 2, -1},
-//            {(DoubleUnaryOperator)x -> -exp(x) + 1/E, -Double.MAX_VALUE, Double.MAX_VALUE, -1},
-//            {(DoubleUnaryOperator)x -> -exp(x) + 1/E, -Double.MAX_VALUE, Double.POSITIVE_INFINITY, -1},
-//            {(DoubleUnaryOperator)x -> -exp(x) + 1/E, Double.NEGATIVE_INFINITY, 2, -1},
-//            {(DoubleUnaryOperator)x -> -exp(x) + 1/E, Double.NEGATIVE_INFINITY, Double.MAX_VALUE, -1},
-//            {(DoubleUnaryOperator)x -> -exp(x) + 1/E, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, -1},
+            {(DoubleUnaryOperator)x -> -exp(x) + 1/E, -Double.MAX_VALUE, Double.MAX_VALUE, -1},
+            {(DoubleUnaryOperator)x -> -exp(x) + 1/E, -Double.MAX_VALUE, Double.POSITIVE_INFINITY, -1},
+            {(DoubleUnaryOperator)x -> -exp(x) + 1/E, Double.NEGATIVE_INFINITY, 2, -1},
+            {(DoubleUnaryOperator)x -> -exp(x) + 1/E, Double.NEGATIVE_INFINITY, Double.MAX_VALUE, -1},
+            {(DoubleUnaryOperator)x -> -exp(x) + 1/E, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, -1},
             // x - c -- +/+
             {(DoubleUnaryOperator)x -> x - 42, 1, 100, 42},
-//            {(DoubleUnaryOperator)x -> x - 0.9*Double.MAX_VALUE, 1, Double.MAX_VALUE, 0.9*Double.MAX_VALUE},
-//            {(DoubleUnaryOperator)x -> x - 0.9*Double.MAX_VALUE, 0.85*Double.MAX_VALUE, 0.95*Double.MAX_VALUE, 0.9*Double.MAX_VALUE},
-//            {(DoubleUnaryOperator)x -> x - 0.9*Double.MAX_VALUE, 0.85*Double.MAX_VALUE, Double.POSITIVE_INFINITY, 0.9*Double.MAX_VALUE},
+            {(DoubleUnaryOperator)x -> x - 0.9*Double.MAX_VALUE, 1, Double.MAX_VALUE, 0.9*Double.MAX_VALUE},
+            {(DoubleUnaryOperator)x -> x - 0.9*Double.MAX_VALUE, 0.85*Double.MAX_VALUE, 0.95*Double.MAX_VALUE, 0.9*Double.MAX_VALUE},
+            {(DoubleUnaryOperator)x -> x - 0.9*Double.MAX_VALUE, 0.85*Double.MAX_VALUE, Double.POSITIVE_INFINITY, 0.9*Double.MAX_VALUE},
             // c - x -- +/+
             {(DoubleUnaryOperator)x -> -x + 42, 1, 100, 42},
-//            {(DoubleUnaryOperator)x -> -x + 0.9*Double.MAX_VALUE, 1, Double.MAX_VALUE, 0.9*Double.MAX_VALUE},
-//            {(DoubleUnaryOperator)x -> -x + 0.9*Double.MAX_VALUE, 0.85*Double.MAX_VALUE, 0.95*Double.MAX_VALUE, 0.9*Double.MAX_VALUE},
-//            {(DoubleUnaryOperator)x -> -x + 0.9*Double.MAX_VALUE, 0.85*Double.MAX_VALUE, Double.POSITIVE_INFINITY, 0.9*Double.MAX_VALUE},
+            {(DoubleUnaryOperator)x -> -x + 0.9*Double.MAX_VALUE, 1, Double.MAX_VALUE, 0.9*Double.MAX_VALUE},
+            {(DoubleUnaryOperator)x -> -x + 0.9*Double.MAX_VALUE, 0.85*Double.MAX_VALUE, 0.95*Double.MAX_VALUE, 0.9*Double.MAX_VALUE},
+            {(DoubleUnaryOperator)x -> -x + 0.9*Double.MAX_VALUE, 0.85*Double.MAX_VALUE, Double.POSITIVE_INFINITY, 0.9*Double.MAX_VALUE},
             // x - c -- -/-
             {(DoubleUnaryOperator)x -> x + 42, -100, -1, -42},
-//            {(DoubleUnaryOperator)x -> x + 0.9*Double.MAX_VALUE, -Double.MAX_VALUE, -1, -0.9*Double.MAX_VALUE},
-//            {(DoubleUnaryOperator)x -> x + 0.9*Double.MAX_VALUE, -0.95*Double.MAX_VALUE, -0.85*Double.MAX_VALUE, -0.9*Double.MAX_VALUE},
-//            {(DoubleUnaryOperator)x -> x + 0.9*Double.MAX_VALUE, Double.NEGATIVE_INFINITY, -0.85*Double.MAX_VALUE, -0.9*Double.MAX_VALUE},
+            {(DoubleUnaryOperator)x -> x + 0.9*Double.MAX_VALUE, -Double.MAX_VALUE, -1, -0.9*Double.MAX_VALUE},
+            {(DoubleUnaryOperator)x -> x + 0.9*Double.MAX_VALUE, -0.95*Double.MAX_VALUE, -0.85*Double.MAX_VALUE, -0.9*Double.MAX_VALUE},
+            {(DoubleUnaryOperator)x -> x + 0.9*Double.MAX_VALUE, Double.NEGATIVE_INFINITY, -0.85*Double.MAX_VALUE, -0.9*Double.MAX_VALUE},
             // c - x -- -/-
             {(DoubleUnaryOperator)x -> -x - 42, -100, -1, -42},
-//            {(DoubleUnaryOperator)x -> -x - 0.9*Double.MAX_VALUE, -Double.MAX_VALUE, -1, -0.9*Double.MAX_VALUE},
-//            {(DoubleUnaryOperator)x -> -x - 0.9*Double.MAX_VALUE, -0.95*Double.MAX_VALUE, -0.85*Double.MAX_VALUE, -0.9*Double.MAX_VALUE},
-//            {(DoubleUnaryOperator)x -> -x - 0.9*Double.MAX_VALUE, Double.NEGATIVE_INFINITY, -0.85*Double.MAX_VALUE, -0.9*Double.MAX_VALUE},
+            {(DoubleUnaryOperator)x -> -x - 0.9*Double.MAX_VALUE, -Double.MAX_VALUE, -1, -0.9*Double.MAX_VALUE},
+            {(DoubleUnaryOperator)x -> -x - 0.9*Double.MAX_VALUE, -0.95*Double.MAX_VALUE, -0.85*Double.MAX_VALUE, -0.9*Double.MAX_VALUE},
+            {(DoubleUnaryOperator)x -> -x - 0.9*Double.MAX_VALUE, Double.NEGATIVE_INFINITY, -0.85*Double.MAX_VALUE, -0.9*Double.MAX_VALUE},
 
             // miscellaneous -/+
-//            {(DoubleUnaryOperator)x -> x, -PI/2, PI/2, 0.0},
+            {(DoubleUnaryOperator)x -> x, -PI/2, PI/2, 0.0},
             {(DoubleUnaryOperator)x -> x-1, -1e300, 1e300, 1.0},
-//            {(DoubleUnaryOperator)x -> x+1, -Double.MAX_VALUE, Double.MAX_VALUE, -1.0},
-//            {(DoubleUnaryOperator)x -> x, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 0},
-//            {(DoubleUnaryOperator)x -> x*x*x, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 0},
-//            {(DoubleUnaryOperator)Math::log, Double.MIN_VALUE, Double.POSITIVE_INFINITY, 1},
+            {(DoubleUnaryOperator)x -> x+1, -Double.MAX_VALUE, Double.MAX_VALUE, -1.0},
+            {(DoubleUnaryOperator)x -> x, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 0},
+            {(DoubleUnaryOperator)x -> x*x*x, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, 0},
+            {(DoubleUnaryOperator)Math::log, Double.MIN_VALUE, Double.POSITIVE_INFINITY, 1},
+            {(DoubleUnaryOperator)x -> 1/x - 1, 0.0, Double.POSITIVE_INFINITY, 1},
+            {(DoubleUnaryOperator)x -> 1/x + 1, Double.NEGATIVE_INFINITY, -0.0, -1},
             // price function
             {(DoubleUnaryOperator)u -> u * PriceFunction.Cache.createStandardWeightedPriceFunction(1.0)
                 .unitPrice(u, null, null, null, null) - 7.777777777, 0.01, 0.99, 0.7},
@@ -159,7 +157,7 @@ public class BisectionTest {
     }
 
     /**
-     * Feeds {@link Bisection#solve(double, DoubleBinaryOperator, int, DoubleUnaryOperator, double, double)}
+     * Feeds {@link Bisection#solve(DoubleUnaryOperator, double, double)}
      * valid input and tests that its result is a root.
      *
      * When there are multiple roots in the interval, it is implementation-dependent which one the
@@ -170,29 +168,28 @@ public class BisectionTest {
     @Parameters
     @TestCaseName("Test #{index}: {0}(Bisection.solve({0},{1},{2})) == 0.0")
     public final void testSolve_Positive_ManyRoots(DoubleUnaryOperator function, double a, double b) {
-        assertEquals(0.0, function.applyAsDouble(Bisection.solve(
-            maxError, errorFunction, maxIterations, function, a, b)), maxError);
+        assertEquals(0.0, function.applyAsDouble(Bisection.solve(function, a, b)), maxError);
     }
 
     @SuppressWarnings("unused") // it is used reflectively
     private static Object[] parametersForTestSolve_Positive_ManyRoots() {
         return new Object[][]{
-//            {(DoubleUnaryOperator)x ->  (x-2)*x*(x+2),  -3,  3},
-//            {(DoubleUnaryOperator)x -> -(x-2)*x*(x+2), -30, 30},
+            {(DoubleUnaryOperator)x ->  (x-2)*x*(x+2),  -3,  3},
+            {(DoubleUnaryOperator)x -> -(x-2)*x*(x+2), -30, 30},
             {(DoubleUnaryOperator)Math::sin, -33, 33},
             {(DoubleUnaryOperator)Math::cos, -12, 42},
         };
     }
 
     /**
-     * Feeds {@link Bisection#solve(double, DoubleBinaryOperator, int, DoubleUnaryOperator, double, double)}
+     * Feeds {@link Bisection#solve(DoubleUnaryOperator, double, double)}
      * invalid input and tests that the proper exception is thrown.
      */
     @Test(expected = IllegalArgumentException.class)
     @Parameters
     @TestCaseName("Test #{index}: Bisection.solve({0},{1},{2})")
     public final void testSolve_Negative(DoubleUnaryOperator function, double a, double b) {
-        Bisection.solve(maxError, errorFunction, maxIterations, function, a, b);
+        Bisection.solve(function, a, b);
     }
 
     @SuppressWarnings("unused") // it is used reflectively
