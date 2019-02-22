91bbee88a111b8faeb7271a13172fa4050a99258
karthik_thyagaraja <karthik.thyagaraja@turbonomic.com>
2019-02-22 13:18:34 -0500

    OM-43326 : Fix DB migration script. RB:29149
    * If the upgrade is run in a background mode, there is no tty
    and the migration script will fail. Run docker exec in
    non-interactive mode.
    * Exit immediately if a command exits with a non-zero status.

--- build/upgrade_specs/007_012_000/change_mariadb_temp_dir.sh
+++ build/upgrade_specs/007_012_000/change_mariadb_temp_dir.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -e
+
 # Change the temp dir location for mariadb
 MY_CNF="/var/lib/docker/volumes/docker_mysqldata/_data/my.cnf"
 TMPDIR=/var/lib/mysql/tmp
 # We need to bring up the db container so that we can set
 # up the permissions for the tmpdir as mysql user
 docker-compose -f /etc/docker/docker-compose.yml up -d db
-docker exec -it docker_db_1 mkdir -p $TMPDIR
-docker exec -it docker_db_1 chmod 700 $TMPDIR
-docker exec -it docker_db_1 chown -R mysql:mysql $TMPDIR
+docker exec docker_db_1 mkdir -p $TMPDIR
+docker exec docker_db_1 chmod 700 $TMPDIR
+docker exec docker_db_1 chown -R mysql:mysql $TMPDIR
 docker-compose -f /etc/docker/docker-compose.yml stop db
 sed  -i 's/= \/tmp/= \/var\/lib\/mysql\/tmp/' $MY_CNF
