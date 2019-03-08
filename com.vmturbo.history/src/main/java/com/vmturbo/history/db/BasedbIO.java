4bfa4ad2dc6c24e79d12d49704cc150983837682
Professor Git <builder@turbonomic.com>
2019-03-07 19:02:31 -0500

    Reverted from xl-develop-ci-build-1858 and reopened jira issues (OM-43725 ) and reverted commits(7e1fe66bc22220b2c8db490bb97b352229245697 )

--- com.vmturbo.history/src/main/java/com/vmturbo/history/db/BasedbIO.java
+++ com.vmturbo.history/src/main/java/com/vmturbo/history/db/BasedbIO.java
@@ -1288,11 +1288,11 @@ public abstract class BasedbIO {
         // Allow 'vmtplatform' to access the database (= grants.sql):
         final String requestUser = "'vmtplatform'@'" + getRequestHost() + "'";
         logger.info("Initialize permissions for '" + requestUser + "' on '" + dbName + "'...");
-        execute("GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, PROCESS, REFERENCES, INDEX, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER " +
+        execute("GRANT ALL PRIVILEGES " +
                         "ON *.* TO " + requestUser + " " +
                         "IDENTIFIED BY '" + getPassword() + "';",
                 rootConn);
-        execute("GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, PROCESS, REFERENCES, INDEX, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER " +
+        execute("GRANT ALL PRIVILEGES " +
                         "ON *.* TO " + "'vmtplatform'@'localhost'" + " " +
                         "IDENTIFIED BY '" + getPassword() + "';",
                 rootConn);
