53cc4f19f3b4f130d40c8dacef8aee9856e12fbc
gary_zeng <gary.zeng@turbonomic.com>
2019-03-22 14:08:40 -0400

    OM-44204: Add metrics to measure report data generation time (rb29776)

--- com.vmturbo.reports.component/src/main/java/com/vmturbo/reports/component/data/ReportsDataGenerator.java
+++ com.vmturbo.reports.component/src/main/java/com/vmturbo/reports/component/data/ReportsDataGenerator.java
@@ -8,9 +8,9 @@ import javax.annotation.Nonnull;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.google.common.collect.ImmutableMap;
-
-import com.vmturbo.reports.component.data.vm.Daily_vm_rightsizing_advice_grid;
+import com.vmturbo.proactivesupport.DataMetricCounter;
+import com.vmturbo.proactivesupport.DataMetricSummary;
+import com.vmturbo.proactivesupport.DataMetricTimer;
 import com.vmturbo.sql.utils.DbException;
 
 /**
@@ -29,20 +29,44 @@ public class ReportsDataGenerator {
 
     /**
      * Generate data based on report template id.
+     *
      * @param id report template id.
      * @return true if the data generation is successfully.
      */
     public boolean generateByTemplateId(final int id) {
         if (reportMap.containsKey(id)) {
-            try {
-                reportMap.get(id).generateData(context);
+            final ReportTemplate reportTemplate = reportMap.get(id);
+            try (DataMetricTimer timer = Metrics.REPORT_DATA_GENERATION_DURATION_CALCULATION
+                .labels(reportTemplate.getClass().getSimpleName()).startTimer()) {
+                reportTemplate.generateData(context);
             } catch (DbException e) {
                 logger.error("Failed to generate report data for template id: " + id);
+                Metrics.REPORT_DATA_GENERATION_ERROR_COUNTS_SUMMARY
+                    .labels(reportTemplate.getClass().getSimpleName())
+                    .increment();
                 return false;
             }
             logger.info("Generated report data for template with id: " + id);
         }
         logger.info("The template id (" + id + ") is not defined skip data generation.");
         return true;
+    }
+
+    private static class Metrics {
+        private static final DataMetricSummary REPORT_DATA_GENERATION_DURATION_CALCULATION =
+            DataMetricSummary.builder()
+                .withName("report_data_generation_calculation_seconds")
+                .withHelp("Time taken to generate report data in report component.")
+                .withLabelNames("report_template_name")
+                .build()
+                .register();
+
+        private static final DataMetricCounter REPORT_DATA_GENERATION_ERROR_COUNTS_SUMMARY =
+            DataMetricCounter.builder()
+                .withName("report_data_generation_error_count")
+                .withHelp("Number of report data generation errors.")
+                .withLabelNames("report_template_name")
+                .build()
+                .register();
     }
 }
