425b5b14b0c81f381292c6c861b04dca6ba73608
esara <endre.sara@turbonomic.com>
2019-05-17 03:59:59 -0400

    OM-46135: enforce g1gc in 7.17 with java11 and externalize java options, so they can be configured from the operator or helm

--- diags/docker/entrypoint.sh
+++ diags/docker/entrypoint.sh
@@ -4,17 +4,7 @@
 # The Djava.security.egd=file:/dev/./urandom configuration significantly speeds up start-up time
 # for the components using the SecureRandom class (see
 # http://stackoverflow.com/questions/25660899/spring-boot-actuator-application-wont-start-on-ubuntu-vps)
-COMMON_JAVA_OPTS="-XX:CompileThreshold=1500 -XX:+ExitOnOutOfMemoryError -verbose:gc"
-COMMON_JAVA_OPTS="-Xms16m -Xss512k -XX:MaxRAMPercentage=75.0 $COMMON_JAVA_OPTS"
-COMMON_JAVA_OPTS="-XX:SoftRefLRUPolicyMSPerMB=0 -XX:+PrintConcurrentLocks -XX:+PrintClassHistogram $COMMON_JAVA_OPTS"
-COMMON_JAVA_OPTS="-XX:+PrintCommandLineFlags -XX:+UseStringDeduplication -XX:StringDeduplicationAgeThreshold=1 $COMMON_JAVA_OPTS"
-COMMON_JAVA_OPTS="-Djavax.xml.bind.JAXBContextFactory=com.sun.xml.bind.v2.ContextFactory $COMMON_JAVA_OPTS"
-COMMON_JAVA_OPTS="-Djavax.xml.ws.spi.Provider=com.sun.xml.ws.spi.ProviderImpl $COMMON_JAVA_OPTS"
-COMMON_JAVA_OPTS="-Djavax.xml.soap.SAAJMetaFactory=com.sun.xml.messaging.saaj.soap.SAAJMetaFactoryImpl $COMMON_JAVA_OPTS"
-COMMON_JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Djava.net.preferIPv4Stack=true -XX:-OmitStackTraceInFastThrow $COMMON_JAVA_OPTS"
-COMMON_JAVA_OPTS="-Dnetworkaddress.cache.ttl=0 -Dnetworkaddress.cache.negative.ttl=0 $COMMON_JAVA_OPTS"
-COMMON_JAVA_OPTS="-DLog4jContextSelector=${LOG4J_CONTEXT_SELECTOR:-org.apache.logging.log4j.core.async.AsyncLoggerContextSelector} $COMMON_JAVA_OPTS"
-export STARTUP_COMMAND="java $COMMON_JAVA_OPTS $JAVA_OPTS $MORE_JAVA_OPTS -jar $@"
+export STARTUP_COMMAND="java $JAVA_OPTS -jar $@"
 
 # If LOG_TO_STDOUT is defined in the environment, tee the output so that it is also logged to stdout.
 # This is generally desirable in a development setup where you want to see the output on the console when
