b291976ca34f222d80b5549e41d32c3ce9035d3d
akhand_singh <akhand.singh@turbonomic.com>
2019-05-18 11:52:45 -0400

    OM-46021 - RB:31376 - Update post api for plan destination to return PlanDestinationApiDTO

--- com.vmturbo.api.component/src/main/java/com/vmturbo/api/component/external/api/service/PlanDestinationService.java
+++ com.vmturbo.api.component/src/main/java/com/vmturbo/api/component/external/api/service/PlanDestinationService.java
@@ -16,7 +16,7 @@ public class PlanDestinationService implements IPlanDestinationService {
      *
      * @param businessUnitId, id of the business unit to filter list of plan destinations.
      * @return List of PlanDestinationApiDTO.
-     * @throws Exception, thrown if issues in converting PlanDestination to PlanDestinationApiDTO.
+     * @throws Exception thrown if issues in converting PlanDestination to PlanDestinationApiDTO.
      */
     @Override
     public @Nonnull List<PlanDestinationApiDTO> getPlanDestinations(@Nullable String businessUnitId)
@@ -29,7 +29,7 @@ public class PlanDestinationService implements IPlanDestinationService {
      *
      * @param uuid, The uuid of the plan destination.
      * @return PlanDestinationApiDTO.
-     * @throws Exception, thrown if issues in converting PlanDestination to PlanDestinationApiDTO.
+     * @throws Exception thrown if issues in converting PlanDestination to PlanDestinationApiDTO.
      */
     @Override
     public PlanDestinationApiDTO getPlanDestinationById(@Nonnull final String uuid) throws Exception {
@@ -43,10 +43,11 @@ public class PlanDestinationService implements IPlanDestinationService {
      * @param planDestinationId,  Plan destination id,
      *                            this is the plan destination where we would upload plan results to.
      *
-     * @return boolean, true of upload was successful.
+     * @return PlanDestinationApiDTO, with export state and description.
+     * @throws Exception thrown if issues in error in upload or invalid plan.
      */
     @Override
-    public Boolean uploadToPlanDestination(@Nonnull String planMarketId,
+    public PlanDestinationApiDTO uploadToPlanDestination(@Nonnull String planMarketId,
                                            @Nonnull String planDestinationId)
             throws Exception {
         throw ApiUtils.notImplementedInXL();
