8757af67cfb51b56165b05787f395d0625ee417f
patrick_soohoo <patrick.soohoo@turbonomic.com>
2019-06-13 20:02:59 -0400

    OM-46592, RB:32049 Made nginx worker process count and connection pool sizes configurable

--- nginx/docker/entrypoint.sh
+++ nginx/docker/entrypoint.sh
@@ -5,10 +5,24 @@ set -eo pipefail
 # rsyslog
 rm -f /tmp/rsyslog.pid; /usr/sbin/rsyslogd -f /etc/rsyslog.conf -i /tmp/rsyslog.pid
 
+# if WORKER_CONNECTIONS is set, it will be used to set the worker_connections property in the
+# generated nginx.conf file. This controls the number of connections available per worker process.
+# This defaults to 512 (the nginx default value), if WORKER_CONNECTIONS is not set.
+if [ "$WORKER_CONNECTIONS" == "" ]; then
+    export WORKER_CONNECTIONS=512
+fi
+
+# if $WORKER_PROCESSES is set, it will be used to set the worker_processes property in the
+# generated nginx.conf file. This controls the number of worker processes spawned by nginx.
+# This defaults to 1 (the nginx default value), if $WORKER_PROCESSES is not set.
+if [ "$WORKER_PROCESSES" == "" ]; then
+    export WORKER_PROCESSES=1
+fi
+
 if [ "$DNS_RESOLVER" == "" ]; then
     export DNS_RESOLVER=`cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' '`
 fi
-envsubst '${API} ${TOPOLOGY} ${DNS_RESOLVER}' < /etc/nginx/nginx.conf.template > /tmp/nginx.conf
+envsubst '${API} ${TOPOLOGY} ${DNS_RESOLVER} ${WORKER_PROCESSES} ${WORKER_CONNECTIONS}' < /etc/nginx/nginx.conf.template > /tmp/nginx.conf
 
 # If LOG_TO_STDOUT is defined in the environment, tee the output so that it is also logged to stdout.
 # This is generally desirable in a development setup where you want to see the output on the console when
@@ -20,6 +34,8 @@ if [[ -z ${LOG_TO_STDOUT} ]]; then
 else
   export LOGGER_COMMAND="eval tee >(logger --tag nginx-${instance_id} -u /tmp/log.sock)"
 fi
+
+echo "Configuring" $WORKER_PROCESSES "and" $WORKER_CONNECTIONS "connections per process." 2>&1 | ${LOGGER_COMMAND}
 
 mkdir -p /tmp/certs
 
