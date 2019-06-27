ddbfb5af638b467821134b1285e3be76c9cd355c
roman_zimine <roman.zimine@turbonomic.com>
2019-06-27 08:38:37 -0400

    OM-47844: XL API: In plans, requesting entities involved in actions from the projected topology.
    Issue
    =====
    Plan actions not showing up in 7.17.0 and develop.
    
    Background
    ==========
    I added an optimization to request entities from the source topology where possible - because in the realtime context it's much cheaper to get entities from the source topology. However, in plans we don't save the source topology, so we were getting no related entity information!
    
    Fix
    ===
    Modifying the RepositoryApi helper class to internally target the projected topology when used with a non-realtime context.
    
    Future Work
    ===========
    Plan actions still don't show up in develop, because of a separate issue. The action descriptions are now supposed to be calculated in the action orchestrator, but that's only been done for the LiveActionStore. Tariq is working on the plan side under this issue: OM 47108
    
    Testing
    =======
    - New test.
    - Existing tests pass.
    - Tested running a plan.
    
    Reviewed By: psoohoo
    https://rbcommons.com/s/VMTurbo/r/32408/

--- com.vmturbo.api.component/src/main/java/com/vmturbo/api/component/communication/RepositoryApi.java
+++ com.vmturbo.api.component/src/main/java/com/vmturbo/api/component/communication/RepositoryApi.java
@@ -54,7 +54,7 @@ import com.vmturbo.common.protobuf.topology.TopologyDTO.TopologyEntityDTO;
  */
 public class RepositoryApi {
 
-    private final Logger logger = LogManager.getLogger();
+    private static final Logger logger = LogManager.getLogger();
 
     private final SeverityPopulator severityPopulator;
 
@@ -465,10 +465,23 @@ public class RepositoryApi {
                         return Collections.emptyIterator();
                     }
 
+                    final long topoContextId = contextId == null ? realtimeContextId : contextId;
+
+                    final TopologyType topologyType;
+                    if (projectedTopology) {
+                        topologyType = TopologyType.PROJECTED;
+                    } else if (topoContextId != realtimeContextId) {
+                        // In plans we don't store source entities, so requesting them will return
+                        // nothing. Look for everything in the projected topology.
+                        topologyType = TopologyType.PROJECTED;
+                    } else {
+                        topologyType = TopologyType.SOURCE;
+                    }
+
                     final RetrieveTopologyEntitiesRequest request = RetrieveTopologyEntitiesRequest.newBuilder()
                         .addAllEntityOids(targetId)
-                        .setTopologyContextId(contextId == null ? realtimeContextId : contextId)
-                        .setTopologyType(projectedTopology ? TopologyType.PROJECTED : TopologyType.SOURCE)
+                        .setTopologyContextId(topoContextId)
+                        .setTopologyType(topologyType)
                         .addAllEntityType(restrictedTypes)
                         .setReturnType(type)
                         .build();
