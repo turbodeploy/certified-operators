1c49686357584ff994c92fbbafb9e317251298c5
karthik_thyagaraja <karthik.thyagaraja@turbonomic.com>
2019-07-05 11:55:20 -0400

    OM-48176 :  Fixes to db partition script. RB:32631

--- build/kubernetes/bin/mariadb_storage_setup.sh
+++ build/kubernetes/bin/mariadb_storage_setup.sh
@@ -1,4 +1,4 @@
-#!/bin/build
+#!/bin/bash
 
 # Script to prepare a disk for storing mariadb database.
 
@@ -9,31 +9,41 @@ MARIADB_DISK_PART="/dev/sdc1"
 VOLUME_GROUP="vg_turbo_db"
 LVM="lv_turbo_db"
 DB_LVM_PATH="/dev/mapper/$VOLUME_GROUP-$LVM"
-DB_MOUNT_PATH="/var/lib/turbodb"
+DB_MOUNT_PATH="/var/lib/mysql"
 FSTAB="/etc/fstab"
-FSLABEL="XL_MARIADB"
+FS_LABEL="XL_MARIADB"
 DISK_SIZE=500
 
-source libs.sh
+source /opt/local/bin/libs.sh
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
 
 log_msg "Preparing mariadb disk"
 
-if [! -f $MARIADB_DISK];
+if [[ ! -b $MARIADB_DISK ]];
 then
     log_msg "Disk: $MARIADB_DISK not available"
     exit 1
 fi
 
-if [ -f $MARIADB_DISK_PART];
+if [[ -b $MARIADB_DISK_PART ]];
 then
-    log_msg "Partition already exists: $MARIADB_DISK_PART "
-    # Check if it's a valid mariadb partition that we can use
-    ([[ $(blkid $MARIADB_DISK | grep 'LVM2_member') ]] && [[ $(fdisk -l  $MARIADB_DISK 2>/dev/null | grep 'Linux LVM') ]] && \
-		[[ `fdisk -l  $MARIADB_DISK 2>/dev/null | grep 'Linux LVM' | awk '{print $4}' `  == "$DISK_SIZE"G]] )  \
-		|| [[ log_msg "Wrong Partition Type" && exit 1 ]]
+    log_msg "Warning: Partition already exists: $MARIADB_DISK_PART "
+    # Check if it's a valid mariadb partition that we can use.
+    if ! [[ $(blkid $MARIADB_DISK_PART | grep 'LVM2_member') \
+        &&  `parted /dev/sdc print | grep -i '^Partition Table' | awk '{print $3}'` == "gpt"  \
+        && `lsblk $MARIADB_DISK_PART | grep part | awk '{print $4}'`  == "$DISK_SIZE"G ]];
+    then
+            log_msg "Wrong partition type detected. Aborting."
+            exit 1
+    fi
 else
-	log_msg "Creating lvm partition on $MARIADB_DISK"
-    parted -s $MARIADB_DISK set 1 lvm on
+    log_msg "Creating lvm partition on $MARIADB_DISK"
+    /sbin/parted $MARIADB_DISK mklabel gpt --script
+    /sbin/parted $MARIADB_DISK mkpart primary 0% 100% set 1 lvm on --script
 fi
 
 log_msg "Creating Physical Volume $MARIADB_DISK_PART"
@@ -48,12 +58,12 @@ log_msg "Creating Logical Volume $LVM"
 log_msg "Creating XFS filesystem on the LVM"
 [[ $(blkid $DB_LVM_PATH | grep xfs) ]] || mkfs.xfs -L $FS_LABEL $DB_LVM_PATH
 
-grep $DB_MOUNT_PATH /etc/fstab
 if ! grep -q $DB_MOUNT_PATH $FSTAB ; then
-    echo "$DB_LVM_PATH  $DB_MOUNT_PATH xfs     defaults        0 0 " >> $FSTAB
+    echo "$DB_LVM_PATH $DB_MOUNT_PATH xfs defaults 0 0" >> $FSTAB
 fi
 
 log_msg "Mounting fileystem"
-mount -a 
+mkdir -p $DB_MOUNT_PATH
+mount -a
 
 log_msg "Successfully setup disk for mariadb."
