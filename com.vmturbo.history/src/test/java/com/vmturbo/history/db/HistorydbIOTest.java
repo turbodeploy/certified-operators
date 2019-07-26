478b019ea93a80cb5d51bacea953130337dc6059
gary_zeng <gary.zeng@turbonomic.com>
2019-07-26 17:41:22 -0400

    OM-48801: support sorting stats by utilization (rb33119)
    Problems:
    
    Basically the issue is that the per-entity stats API returns stats sorted by “avg_value” (i.e. used).
    What we want is to sort the returned stats by “avg_value / capacity” (i.e. utilization).
    
    Investigation:
    
    Some existing suggestions:
    1.  Create a view, like what we have for hourly (slow at write time)
    2.  Insert a row for utilization into latest tables (slow at write time)
    3.  Use the hourly utilization tables, even when sorting by latest (inaccurate)
    4.  Use an arithmetic expression on the query side (i.e. order by avg_value / capacity) – (slow at read time)
    5.  Cache latest utilization (inaccurate if desired snapshot range is not “current”, memory intensive)
    
    Proposed fixes:
    
    After discussing with Roman and Andy, we decided to choose #4. Because it seems the performance is acceptable in large environment.

--- com.vmturbo.history/src/test/java/com/vmturbo/history/db/HistorydbIOTest.java
+++ com.vmturbo.history/src/test/java/com/vmturbo/history/db/HistorydbIOTest.java
@@ -2,9 +2,12 @@ package com.vmturbo.history.db;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jooq.Field;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +16,9 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.vmturbo.components.common.pagination.EntityStatsPaginationParams;
+import com.vmturbo.components.common.utils.StringConstants;
+import com.vmturbo.history.schema.abstraction.tables.VmStatsLatest;
 import com.vmturbo.history.stats.DbTestConfig;
 
 /**
@@ -64,6 +70,25 @@ public class HistorydbIOTest {
         } finally {
             teardownDatabase();
         }
+    }
+
+    @Test
+    public void testGetValueFieldSortByPriceIndex() {
+        final EntityStatsPaginationParams paginationParam = mock(EntityStatsPaginationParams.class);
+        when(paginationParam.getSortCommodity()).thenReturn(StringConstants.PRICE_INDEX);
+        Field<Double> field = historydbIO.getValueField(paginationParam, VmStatsLatest.VM_STATS_LATEST);
+        assertEquals("avg_value", field.getName());
+    }
+
+    @Test
+    public void testGetValueFieldSortByCUP() {
+        final EntityStatsPaginationParams paginationParam = mock(EntityStatsPaginationParams.class);
+        when(paginationParam.getSortCommodity()).thenReturn(StringConstants.CPU);
+        Field<Double> field = historydbIO.getValueField(paginationParam, VmStatsLatest.VM_STATS_LATEST);
+        // the filed type is org.jooq.impl.Expression which is not exposed. There seems no other way to validate
+        // the output other than filed.toString.
+        assertEquals("(\"vmtdb\".\"vm_stats_latest\".\"avg_value\" / \"vmtdb\".\"vm_stats_latest\".\"capacity\")"
+            , field.toString());
     }
 
     private void setupDatabase() throws VmtDbException {
