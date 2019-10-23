104a0f6aa0599cfa934d8f572bdb6c9af81ea948
cheuk_lam <cheuk.lam@turbonomic.com>
2019-10-23 14:46:33 -0400

    OM-50823 RB:34848 Changing to listen to the Market component since a similar interface from action orchestrator has been deprecated.

--- com.vmturbo.ml.datastore/src/main/java/com/vmturbo/ml/datastore/topology/ActionsListenerConfig.java
+++ com.vmturbo.ml.datastore/src/main/java/com/vmturbo/ml/datastore/topology/ActionsListenerConfig.java
@@ -2,6 +2,11 @@ package com.vmturbo.ml.datastore.topology;
 
 import com.vmturbo.action.orchestrator.api.ActionOrchestrator;
 import com.vmturbo.action.orchestrator.api.impl.ActionOrchestratorClientConfig;
+import com.vmturbo.components.api.client.KafkaMessageConsumer.TopicSettings.StartFrom;
+import com.vmturbo.market.component.api.MarketComponent;
+import com.vmturbo.market.component.api.impl.MarketClientConfig;
+import com.vmturbo.market.component.api.impl.MarketSubscription;
+import com.vmturbo.market.component.api.impl.MarketSubscription.Topic;
 import com.vmturbo.ml.datastore.influx.InfluxConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -15,12 +20,16 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({
     ActionOrchestratorClientConfig.class,
+    MarketClientConfig.class,
     InfluxConfig.class
 })
 public class ActionsListenerConfig {
 
     @Autowired
     private ActionOrchestratorClientConfig aoConfig;
+
+    @Autowired
+    private MarketClientConfig marketClientConfig;
 
     @Autowired
     private InfluxConfig influxConfig;
@@ -31,7 +40,15 @@ public class ActionsListenerConfig {
             new ActionMetricsListener(influxConfig.influxDBConnectionFactory(),
                 influxConfig.metricsStoreWhitelist());
         actionsProcessor().addActionsListener(actionMetricsListener);
+        marketComponent().addActionsListener(actionMetricsListener);
         return actionMetricsListener;
+    }
+
+    @Bean
+    public MarketComponent marketComponent() {
+        final MarketComponent market = marketClientConfig.marketComponent(
+                MarketSubscription.forTopic(Topic.ActionPlans));
+        return market;
     }
 
     @Bean
