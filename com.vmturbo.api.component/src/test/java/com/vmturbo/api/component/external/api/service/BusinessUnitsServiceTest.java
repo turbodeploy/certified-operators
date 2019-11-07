3db147ab8be36b8d3b9461d8e701a38aa9145143
vasile_branici <vasile.branici@turbonomic.com>
2019-11-07 10:00:31 -0500

    OM-51282 BusinessAccount : implement API call POST /businessunits/{businessUnitUuid}/actions/stats

--- com.vmturbo.api.component/src/test/java/com/vmturbo/api/component/external/api/service/BusinessUnitsServiceTest.java
+++ com.vmturbo.api.component/src/test/java/com/vmturbo/api/component/external/api/service/BusinessUnitsServiceTest.java
@@ -1,11 +1,16 @@
 package com.vmturbo.api.component.external.api.service;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -15,20 +20,33 @@ import org.mockito.Mockito;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 
+import com.vmturbo.api.component.ApiTestUtils;
+import com.vmturbo.api.component.communication.RepositoryApi;
+import com.vmturbo.api.component.communication.RepositoryApi.SingleEntityRequest;
 import com.vmturbo.api.component.external.api.mapper.BusinessUnitMapper;
+import com.vmturbo.api.component.external.api.mapper.UuidMapper;
+import com.vmturbo.api.component.external.api.mapper.UuidMapper.ApiId;
 import com.vmturbo.api.component.external.api.service.BusinessUnitsService.MissingDiscountException;
+import com.vmturbo.api.component.external.api.util.SupplyChainFetcherFactory;
+import com.vmturbo.api.dto.action.ActionApiInputDTO;
 import com.vmturbo.api.dto.businessunit.BusinessUnitApiDTO;
 import com.vmturbo.api.dto.businessunit.BusinessUnitApiInputDTO;
 import com.vmturbo.api.dto.businessunit.CloudServicePriceAdjustmentApiDTO;
 import com.vmturbo.api.dto.businessunit.EntityPriceDTO;
 import com.vmturbo.api.dto.businessunit.TemplatePriceAdjustmentDTO;
+import com.vmturbo.api.dto.statistic.StatSnapshotApiDTO;
 import com.vmturbo.api.enums.BusinessUnitType;
+import com.vmturbo.api.enums.EnvironmentType;
 import com.vmturbo.api.enums.ServicePricingModel;
 import com.vmturbo.api.serviceinterfaces.IBusinessUnitsService;
 import com.vmturbo.api.serviceinterfaces.ITargetsService;
+import com.vmturbo.api.utils.DateTimeUtil;
 import com.vmturbo.common.protobuf.cost.CostMoles.CostServiceMole;
 import com.vmturbo.common.protobuf.cost.CostServiceGrpc;
 import com.vmturbo.common.protobuf.cost.CostServiceGrpc.CostServiceBlockingStub;
+import com.vmturbo.common.protobuf.topology.TopologyDTO.PartialEntity.MinimalEntity;
+import com.vmturbo.common.protobuf.topology.TopologyDTO.TopologyEntityDTO;
+import com.vmturbo.common.protobuf.topology.UIEntityType;
 import com.vmturbo.components.api.test.GrpcTestServer;
 
 /**
@@ -38,6 +56,7 @@ public class BusinessUnitsServiceTest {
 
 
     public static final String UUID_STRING = "123";
+    public static final long OID_LONG= 123L;
     public static final String TEST_DISPLAY_NAME = "testDisplayName";
     public static final String CHILD_UNIT_ID1 = "123";
     public static final String SERVICE_DISCOUNT_UUID = "3";
@@ -45,16 +64,28 @@ public class BusinessUnitsServiceTest {
     public static final float TIER_DISCOUNT = 11f;
     public static final float SERVICE_DISCOUNT = 22f;
     private final BusinessUnitMapper mapper = Mockito.mock(BusinessUnitMapper.class);
-    private final ITargetsService targetsService = Mockito.mock(TargetsService.class);
-    private IBusinessUnitsService businessUnitsService;
+
+    private final UuidMapper uuidMapper = Mockito.mock(UuidMapper.class);
 
     private CostServiceMole costServiceMole = spy(new CostServiceMole());
-
 
     @Rule
     public GrpcTestServer grpcTestServer = GrpcTestServer.newServer(costServiceMole);
 
+    private EntitiesService entitiesService = Mockito.mock(EntitiesService.class);
+
+    private SupplyChainFetcherFactory supplyChainFetcherFactory = Mockito.mock(SupplyChainFetcherFactory.class);
+
+    private ITargetsService targetsService = Mockito.mock(TargetsService.class);
+
+    private RepositoryApi repositoryApi = Mockito.mock(RepositoryApi.class);
+
+    private static final long CONTEXT_ID = 777777L;
+
     private CostServiceBlockingStub costService;
+
+    private IBusinessUnitsService businessUnitsService;
+
 
     private static CloudServicePriceAdjustmentApiDTO populateServiceDto() {
         final CloudServicePriceAdjustmentApiDTO cloudServiceDiscountApiDTO = new CloudServicePriceAdjustmentApiDTO();
@@ -80,7 +111,16 @@ public class BusinessUnitsServiceTest {
     @Before
     public void setup() throws Exception {
         costService = CostServiceGrpc.newBlockingStub(grpcTestServer.getChannel());
-        businessUnitsService = new BusinessUnitsService(costService, mapper, targetsService);
+
+        businessUnitsService = new BusinessUnitsService(
+                        costService,
+                        mapper,
+                        targetsService,
+                        CONTEXT_ID,
+                        uuidMapper,
+                        entitiesService,
+                        supplyChainFetcherFactory,
+                        repositoryApi);
     }
 
     @Test
@@ -189,4 +229,48 @@ public class BusinessUnitsServiceTest {
     public void testGetBusinessUnitDiscounts() throws Exception {
         businessUnitsService.getBusinessUnitPriceAdjustments(UUID_STRING);
     }
+
+    /**
+     * Testing get count of actions stats by business unit.
+     * @throws Exception If one of the necessary steps/RPCs failed.
+     */
+    @Test
+    public void testGetActionCountStatsByUuid() throws Exception {
+        final ApiId apiId = ApiTestUtils.mockGroupId(UUID_STRING, uuidMapper);
+        when(uuidMapper.fromUuid(UUID_STRING)).thenReturn(apiId);
+
+        final SingleEntityRequest mockReq = ApiTestUtils.mockSingleEntityRequest(MinimalEntity.newBuilder()
+                        .setOid(OID_LONG)
+                        .setEntityType(UIEntityType.VIRTUAL_MACHINE.typeNumber())
+                        .build());
+        when(repositoryApi.entityRequest(OID_LONG)).thenReturn(mockReq);
+
+        TopologyEntityDTO entityDTO = TopologyEntityDTO.newBuilder()
+                        .setOid(OID_LONG)
+                        .setDisplayName("foo")
+                        .setEntityType(UIEntityType.VIRTUAL_MACHINE.typeNumber())
+                        .build();
+        when(mockReq.getFullEntity()).thenReturn(Optional.of(entityDTO));
+
+        ActionApiInputDTO actionDTO = new ActionApiInputDTO();
+        actionDTO.setEndTime(DateTimeUtil.getNow());
+        actionDTO.setEnvironmentType(EnvironmentType.CLOUD);
+        actionDTO.setRelatedEntityTypes(ImmutableList.of());
+        actionDTO.setGroupBy(ImmutableList.of("riskSubCategory", "actionStates"));
+
+        final List<StatSnapshotApiDTO> listStatDTO = new ArrayList<>();
+        StatSnapshotApiDTO stat = new StatSnapshotApiDTO();
+        stat.setDisplayName("foo");
+        stat.setDate("2000-01-01");
+        listStatDTO.add(stat);
+
+        when(entitiesService.getActionCountStatsByUuid(UUID_STRING, actionDTO)).thenReturn(listStatDTO);
+
+        List<StatSnapshotApiDTO> statsByUuid = businessUnitsService.getActionCountStatsByUuid(UUID_STRING, actionDTO);
+
+        verify(entitiesService).getActionCountStatsByUuid(UUID_STRING, actionDTO);
+
+        assertThat(statsByUuid, is(listStatDTO));
+    }
+
 }
