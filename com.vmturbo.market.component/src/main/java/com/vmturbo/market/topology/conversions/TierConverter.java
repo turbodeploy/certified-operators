390936fc3cd93b961fa6ad46014f6ca8c79f4ddc
Harnoor Powar <harnoor.powar@turbonomic.com>
2019-11-18 16:18:32 -0500

    OM-50211: Fix for creation of multiple cost DTO's develop RB:35444

--- com.vmturbo.market.component/src/main/java/com/vmturbo/market/topology/conversions/TierConverter.java
+++ com.vmturbo.market.component/src/main/java/com/vmturbo/market/topology/conversions/TierConverter.java
@@ -16,11 +16,11 @@ public interface TierConverter {
      * @param tier The tier for which the traderTOs need to be created
      * @param topology The topology
      * @param businessAccounts The set of business accounts.
-     * @param accountPricingDataByBusinessAccount The mapping of ba oid to account pricing data.
+     * @param uniqueAccountPricingData The set of unique pricing data (price table + discount) we have in the topology
      *
      * @return Map of {@link TraderTO.Builder} created to {@link MarketTier}
      */
     Map<TraderTO.Builder, MarketTier> createMarketTierTraderTOs(
             TopologyEntityDTO tier, Map<Long, TopologyEntityDTO> topology,
-            Set<TopologyEntityDTO> businessAccounts, Map<Long, AccountPricingData> accountPricingDataByBusinessAccount);
+            Set<TopologyEntityDTO> businessAccounts, Set<AccountPricingData> uniqueAccountPricingData);
 }
