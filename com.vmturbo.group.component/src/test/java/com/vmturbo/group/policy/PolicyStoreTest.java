5b520571497602dfd341303b3d5447c444b4331f
Pavel Baranchikov <pavel.baranchikov@turbonomic.com>
2019-11-19 12:24:11 +0300

    OM-52456: Change foreign key constraint for policy_store in group component database

--- com.vmturbo.group.component/src/test/java/com/vmturbo/group/policy/PolicyStoreTest.java
+++ com.vmturbo.group.component/src/test/java/com/vmturbo/group/policy/PolicyStoreTest.java
@@ -21,6 +21,7 @@ import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 
 import org.jooq.DSLContext;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -236,17 +237,15 @@ public class PolicyStoreTest {
 
 
         // delete the discovered group
-        policyStore.deletePoliciesForGroupBeingRemoved(dbConfig.getDslContext(), CONSUMER_GROUP_ID);
+        policyStore.deletePoliciesForGroupBeingRemoved(dbConfig.getDslContext(),
+                Collections.singleton(CONSUMER_GROUP_ID));
 
         // the user policy should have been deleted
         assertFalse(policyStore.get(POLICY_ID).isPresent());
         // the discovered policy should still be there.
-        assertTrue(policyStore.get(discoveredPolicyId).isPresent());
-        // Policy-to-group mappings should be cleared for the user policy id, but not the discovered
-        // policy id.
+        assertFalse(policyStore.get(discoveredPolicyId).isPresent());
         Map<Long, Set<Long>> policyToGroupMap = getPolicyToGroupMapping();
-        assertFalse(policyToGroupMap.containsKey(POLICY_ID));
-        assertTrue(policyToGroupMap.containsKey(discoveredPolicyId));
+        Assert.assertEquals(Collections.emptyMap(), policyToGroupMap);
     }
 
     @Test
