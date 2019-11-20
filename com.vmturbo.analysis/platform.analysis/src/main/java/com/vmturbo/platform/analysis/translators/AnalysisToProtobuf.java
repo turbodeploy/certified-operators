c5efe4f1a87e78aa783990989fea9d4a1ee67f8b
Bradford Cervenak <bradford.cervenak@turbonomic.com>
2019-11-19 19:32:26 -0500

    OM-49707 rb:35489 Remove duplicate import

--- com.vmturbo.analysis/platform.analysis/src/main/java/com/vmturbo/platform/analysis/translators/AnalysisToProtobuf.java
+++ com.vmturbo.analysis/platform.analysis/src/main/java/com/vmturbo/platform/analysis/translators/AnalysisToProtobuf.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
