0fd7f2db684cff76de5f2ced5ca80beb5781470c
Alexander Vasin <alexandr.vasin@turbonomic.com>
2019-11-21 14:23:47 +0300

    OM-52656: RB:35496. Changed CommodityConverter to adjust history quantity by scaling factor only in case scale factor is set.

--- com.vmturbo.market.component/src/main/java/com/vmturbo/market/topology/conversions/CommodityConverter.java
+++ com.vmturbo.market.component/src/main/java/com/vmturbo/market/topology/conversions/CommodityConverter.java
@@ -108,18 +108,16 @@ public class CommodityConverter {
                                                   : null);
         // if this commodity has a scaling factor set, then scale up the
         // USED and CAPACITY by scalingFactor for use in the new CommoditySoldTO
-        if (topologyCommSold.hasScalingFactor()) {
-            final float scalingFactor = (float)topologyCommSold.getScalingFactor();
-            if (logger.isDebugEnabled()) {
-                logger.debug("Scaling up comm {}, factor {}, for topology entity {},"
-                             + " prev used {}, new {}, prev capacity {}, new {}",
-                        commodityType.getType(), scalingFactor, dto.getDisplayName(),
-                        used, used * scalingFactor, capacity, capacity * scalingFactor
-                );
-            }
-            capacity *= scalingFactor;
-            used *= scalingFactor;
+        final float scalingFactor = (float)topologyCommSold.getScalingFactor();
+        if (topologyCommSold.hasScalingFactor() && logger.isDebugEnabled()) {
+            logger.debug("Scaling comm {}, factor {}, for topology entity {},"
+                         + " prev used {}, new {}, prev capacity {}, new {}",
+                    commodityType.getType(), scalingFactor, dto.getDisplayName(),
+                    used, used * scalingFactor, capacity, capacity * scalingFactor
+            );
         }
+        capacity *= scalingFactor;
+        used *= scalingFactor;
         final int type = commodityType.getType();
         final CommodityDTO.CommodityType sdkCommType = CommodityDTO.CommodityType.forNumber(type);
         final String comName = sdkCommType == null ? "UNKNOWN" : sdkCommType.name();
--- com.vmturbo.market.component/src/test/java/com/vmturbo/market/topology/conversions/CommodityConverterTest.java
+++ com.vmturbo.market.component/src/test/java/com/vmturbo/market/topology/conversions/CommodityConverterTest.java
@@ -7,16 +7,16 @@ import static org.junit.Assert.assertEquals;
 import java.util.Collection;
 import java.util.Map;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-
 import com.google.common.collect.Table;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import junitparams.naming.TestCaseName;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 
 import com.vmturbo.common.protobuf.topology.TopologyDTO.CommoditySoldDTO;
 import com.vmturbo.common.protobuf.topology.TopologyDTO.CommodityType;
@@ -81,25 +81,24 @@ public class CommodityConverterTest {
         final TopologyEntityDTO originalTopologyEntityDTO = TopologyEntityDTO.newBuilder()
                 .setOid(PM_OID)
                 .setEntityType(EntityType.PHYSICAL_MACHINE_VALUE)
-                .addCommoditySoldList(CommoditySoldDTO.newBuilder()
-                        .setCapacity(RAW_CAPACITY)
-                        .setEffectiveCapacityPercentage(effectiveCapacityPercentage)
-                        .setHistoricalUsed(histUsed)
-                        .setCommodityType(CommodityType.newBuilder()
-                                .setType(CommodityDTO.CommodityType.CPU_VALUE)
-                                .build())
-                        .setScalingFactor(SCALING_FACTOR)
-                        .build())
-                .build();
+                        .addCommoditySoldList(createSoldCommodity(histUsed, SCALING_FACTOR,
+                                        CommodityDTO.CommodityType.CPU, effectiveCapacityPercentage))
+                        .addCommoditySoldList(
+                                        createSoldCommodity(histUsed, null,
+                                                        CommodityDTO.CommodityType.MEM,
+                                                        effectiveCapacityPercentage))
+                        .build();
         // Act
-        Collection<CommoditySoldTO> result = converterToTest.commoditiesSoldList(originalTopologyEntityDTO);
+        final Collection<CommoditySoldTO> result =
+                        converterToTest.commoditiesSoldList(originalTopologyEntityDTO);
         // Assert
-        assertThat(result.size(), equalTo(1));
-        final CommoditySoldTO cpuCommodityTO = result.iterator().next();
-        assertThat(cpuCommodityTO.getCapacity(), equalTo(MARKET_CAPACITY));
-        assertThat(cpuCommodityTO.getQuantity(), equalTo(MARKET_USED));
-        assertThat(cpuCommodityTO.getHistoricalQuantity(),
-                equalTo(PERCENTILE_USED * SCALING_FACTOR * RAW_CAPACITY));
+        assertThat(result.size(), equalTo(2));
+        final CommoditySoldTO cpuCommodityTO =
+                        getCommodityByType(result, CommodityDTO.CommodityType.CPU);
+        checkSoldCommodityTO(cpuCommodityTO,
+                        SCALING_FACTOR, MARKET_CAPACITY, MARKET_USED);
+        checkSoldCommodityTO(getCommodityByType(result, CommodityDTO.CommodityType.MEM), null,
+                        RAW_CAPACITY, RAW_USED);
         assertEquals(cpuCommodityTO.getSettings().getUtilizationUpperBound(),
                 (effectiveCapacityPercentage / 100.0) > 1.0f ? 1.0f :
                         (effectiveCapacityPercentage / 100.0), .001f);
@@ -110,6 +109,39 @@ public class CommodityConverterTest {
                 (effectiveCapacityPercentage / 100.0) < 1.0f ? 1.0f :
                         (effectiveCapacityPercentage / 100.0), 0.001f);
 
+    }
+
+
+
+    private static CommoditySoldTO getCommodityByType(Collection<CommoditySoldTO> commodities,
+                    CommodityDTO.CommodityType desiredType) {
+        return commodities.stream()
+                        .filter(comm -> desiredType.getNumber() == comm.getSpecification()
+                                        .getBaseType()).findAny().orElse(null);
+    }
+
+    private void checkSoldCommodityTO(CommoditySoldTO commodityTO, Float rawScalingFactor,
+                    Float expectedCapacity, Float expectedUsed) {
+        assertThat(commodityTO.getCapacity(), equalTo(expectedCapacity));
+        assertThat(commodityTO.getQuantity(), equalTo(expectedUsed));
+        final float scalingFactor = rawScalingFactor == null ? 1 : rawScalingFactor;
+        assertThat(commodityTO.getHistoricalQuantity(),
+                        equalTo(PERCENTILE_USED * scalingFactor * RAW_CAPACITY));
+    }
+
+    private CommoditySoldDTO createSoldCommodity(HistoricalValues histUsed, Float scalingFactor,
+                    CommodityDTO.CommodityType commodityType, double effectiveCapacityPercentage) {
+        final CommoditySoldDTO.Builder builder =
+                        CommoditySoldDTO.newBuilder().setCapacity(RAW_CAPACITY)
+                                        .setEffectiveCapacityPercentage(effectiveCapacityPercentage)
+                                        .setHistoricalUsed(histUsed).setCommodityType(
+                                        CommodityType.newBuilder()
+                                                        .setType(commodityType.getNumber())
+                                                        .build());
+        if (scalingFactor != null) {
+            builder.setScalingFactor(scalingFactor);
+        }
+        return builder.build();
     }
 
 }
