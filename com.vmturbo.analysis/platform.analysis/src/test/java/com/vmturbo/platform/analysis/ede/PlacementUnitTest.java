3a85654ebc95182e96f46d5cdc96ec4d51178774
Bradford Cervenak <bradford.cervenak@turbonomic.com>
2019-12-25 17:44:13 -0500

    OM-49713 rb:35869 Disable invalid unit test.

--- com.vmturbo.analysis/platform.analysis/src/test/java/com/vmturbo/platform/analysis/ede/PlacementUnitTest.java
+++ com.vmturbo.analysis/platform.analysis/src/test/java/com/vmturbo/platform/analysis/ede/PlacementUnitTest.java
@@ -55,7 +55,6 @@ public class PlacementUnitTest {
         testGroupLeaderAlwaysMoves(false);
     }
 
-    @Test
     public void testGroupLeaderAlwaysMoves_ShopTogether() {
         testGroupLeaderAlwaysMoves(true);
     }
