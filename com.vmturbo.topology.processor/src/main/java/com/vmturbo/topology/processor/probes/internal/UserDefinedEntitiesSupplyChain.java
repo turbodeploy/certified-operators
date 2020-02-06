0f5ae1d158903ad4d125b0ce36da0ff768ad74b4
Dmitry Glushkov <dmitry.glushkov@turbonomic.com>
2020-02-06 11:19:16 +0200

    OM-54850 Updated supply chain definition for the UDE probe.

--- com.vmturbo.topology.processor/src/main/java/com/vmturbo/topology/processor/probes/internal/UserDefinedEntitiesSupplyChain.java
+++ com.vmturbo.topology.processor/src/main/java/com/vmturbo/topology/processor/probes/internal/UserDefinedEntitiesSupplyChain.java
@@ -22,11 +22,7 @@ import static com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.EntityType.DAT
 import static com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.EntityType.SERVICE;
 import static com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.EntityType.VIRTUAL_MACHINE;
 import static com.vmturbo.platform.common.dto.SupplyChain.MergedEntityMetadata.ReturnType.STRING;
-import static com.vmturbo.platform.common.dto.SupplyChain.Provider.ProviderType.HOSTING;
-import static com.vmturbo.platform.common.dto.SupplyChain.Provider.ProviderType.LAYERED_OVER;
-import static com.vmturbo.platform.sdk.common.supplychain.ServerEntityPropDefConstants.VM_IP;
 import static com.vmturbo.platform.sdk.common.supplychain.SupplyChainConstants.ID;
-import static com.vmturbo.platform.sdk.common.supplychain.SupplyChainConstants.IP_ADDRESS;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -40,14 +36,10 @@ import com.google.common.collect.Maps;
 
 import com.vmturbo.platform.common.dto.CommonDTO.CommodityDTO.CommodityType;
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.EntityType;
-import com.vmturbo.platform.common.dto.SupplyChain;
 import com.vmturbo.platform.common.dto.SupplyChain.TemplateDTO;
 import com.vmturbo.platform.common.dto.SupplyChain.TemplateDTO.TemplateType;
-import com.vmturbo.platform.sdk.common.supplychain.ExternalLinkBuilder;
 import com.vmturbo.platform.sdk.common.supplychain.MergedEntityMetadataBuilder;
 import com.vmturbo.platform.sdk.common.supplychain.SupplyChainBuilder;
-import com.vmturbo.platform.sdk.common.supplychain.SupplyChainLink;
-import com.vmturbo.platform.sdk.common.supplychain.SupplyChainLinkBuilder;
 import com.vmturbo.platform.sdk.common.supplychain.SupplyChainNodeBuilder;
 
 /**
@@ -69,8 +61,6 @@ public class UserDefinedEntitiesSupplyChain {
         SELL_COMMODITY_MAP.put(CONTAINER,             ImmutableSet.of(VMEM, VCPU, VSTORAGE));
     }
 
-    private static final String IP_ADDRESS_DESCRIPTION = "IP Address of the VM hosting the discovered node";
-
     /*pkg*/ UserDefinedEntitiesSupplyChain() {
         // default constructor
     }
@@ -83,7 +73,7 @@ public class UserDefinedEntitiesSupplyChain {
     @Nonnull
     public Set<TemplateDTO> getSupplyChainDefinition() {
 
-        final TemplateType templateType = TemplateType.EXTENSION;
+        final TemplateType templateType = TemplateType.BASE;
         final int templatePriority = -1;
         final Collection<CommodityType> appCommodity = ImmutableSet.of(APPLICATION);
 
@@ -102,16 +92,12 @@ public class UserDefinedEntitiesSupplyChain {
         final SupplyChainNodeBuilder containerNode = createSupplyChainNodeBuilder(CONTAINER,
                 templateType, templatePriority, appCommodity);
 
-        SupplyChainBuilder supplyChainBuilder = new SupplyChainBuilder();
-        supplyChainBuilder.top(businessAppNode);
-
-        createBusinessAppLinks(supplyChainBuilder, businessAppNode);
-        createBusinessTransactionLinks(supplyChainBuilder, businessTransactionNode);
-        createServiceLinks(supplyChainBuilder, serviceNode);
-        createAppComponentLinks(supplyChainBuilder, appComponentNode);
-        createDbServerLinks(supplyChainBuilder, databaseServerNode);
-
-        return supplyChainBuilder
+        return new SupplyChainBuilder()
+                .top(businessAppNode)
+                .entity(businessTransactionNode)
+                .entity(serviceNode)
+                .entity(appComponentNode)
+                .entity(databaseServerNode)
                 .entity(virtualMachineNode)
                 .entity(containerNode)
                 .configure();
@@ -123,10 +109,10 @@ public class UserDefinedEntitiesSupplyChain {
         final SupplyChainNodeBuilder builder = new SupplyChainNodeBuilder();
         builder.entity(entityType, templateType, priority);
         for (final CommodityType commodity : SELL_COMMODITY_MAP.get(entityType)) {
-            builder.selling(commodity);
+            builder.sellingOptional(commodity);
         }
         for (final CommodityType commodity : commoditiesWithKey) {
-            builder.sellingWithKey(commodity);
+            builder.sellingOptionalWithKey(commodity);
         }
         MergedEntityMetadataBuilder mergedMetaData = new MergedEntityMetadataBuilder()
                 .internalMatchingType(STRING).internalMatchingField(ID, Collections.emptyList())
@@ -135,134 +121,4 @@ public class UserDefinedEntitiesSupplyChain {
         return builder;
     }
 
-    private void createBusinessAppLinks(
-            SupplyChainBuilder supplyChainBuilder,
-            SupplyChainNodeBuilder businessApp) {
-        final SupplyChainLink businessAppToBusinessTransactionLink = new SupplyChainLinkBuilder()
-                .link(BUSINESS_APPLICATION, BUSINESS_TRANSACTION, LAYERED_OVER)
-                .commodityWithKey(APPLICATION)
-                .build();
-
-        final SupplyChainLink businessAppToServiceLink = new SupplyChainLinkBuilder()
-                .link(BUSINESS_APPLICATION, SERVICE, LAYERED_OVER)
-                .commodityWithKey(APPLICATION)
-                .build();
-
-        final SupplyChainLink businessAppToDbLink = new SupplyChainLinkBuilder()
-                .link(BUSINESS_APPLICATION, DATABASE_SERVER, LAYERED_OVER)
-                .commodityWithKey(APPLICATION)
-                .build();
-
-        supplyChainBuilder.entity(businessApp)
-                .connectsTo(businessAppToDbLink)
-                .connectsTo(businessAppToBusinessTransactionLink)
-                .connectsTo(businessAppToServiceLink);
-    }
-
-    private void createBusinessTransactionLinks(
-            SupplyChainBuilder supplyChainBuilder,
-            SupplyChainNodeBuilder businessTransaction) {
-        final SupplyChainLink businessTransactionToServiceLink = new SupplyChainLinkBuilder()
-                .link(BUSINESS_TRANSACTION, SERVICE, LAYERED_OVER)
-                .commodityWithKey(APPLICATION)
-                .build();
-
-        supplyChainBuilder.entity(businessTransaction)
-                .connectsTo(businessTransactionToServiceLink);
-    }
-
-    private void createServiceLinks(
-            SupplyChainBuilder supplyChainBuilder,
-            SupplyChainNodeBuilder service) {
-        final SupplyChainLink serviceToAppComponentLink = new SupplyChainLinkBuilder()
-                .link(SERVICE, APPLICATION_COMPONENT, LAYERED_OVER)
-                .commodityWithKey(APPLICATION)
-                .build();
-
-        final SupplyChainLink serviceToDbServerLink = new SupplyChainLinkBuilder()
-                .link(SERVICE, DATABASE_SERVER, LAYERED_OVER)
-                .commodityWithKey(APPLICATION)
-                .build();
-
-        supplyChainBuilder.entity(service)
-                .connectsTo(serviceToAppComponentLink)
-                .connectsTo(serviceToDbServerLink);
-    }
-
-    private void createAppComponentLinks(
-            SupplyChainBuilder supplyChainBuilder,
-            SupplyChainNodeBuilder appComponent) {
-        final SupplyChainLink appComponentToVmLink = new SupplyChainLinkBuilder()
-                .link(APPLICATION_COMPONENT, VIRTUAL_MACHINE, HOSTING)
-                .commodity(VMEM)
-                .commodity(VCPU)
-                .commodityWithKey(APPLICATION)
-                .build();
-
-        final SupplyChain.ExternalEntityLink appComponentToVmExternalLink = new ExternalLinkBuilder()
-                .link(APPLICATION_COMPONENT, VIRTUAL_MACHINE, HOSTING)
-                .commodityWithKey(APPLICATION)
-                .probeEntityPropertyDef(IP_ADDRESS, IP_ADDRESS_DESCRIPTION)
-                .externalEntityPropertyDef(VM_IP)
-                .build();
-
-        final SupplyChainLink appComponentToContainerLink = new SupplyChainLinkBuilder()
-                .link(APPLICATION_COMPONENT, CONTAINER, HOSTING)
-                .commodity(VMEM)
-                .commodity(VCPU)
-                .commodityWithKey(APPLICATION)
-                .build();
-
-        final SupplyChain.ExternalEntityLink appComponentToContainerExternalLink = new ExternalLinkBuilder()
-                .link(APPLICATION_COMPONENT, CONTAINER, HOSTING)
-                .commodityWithKey(APPLICATION)
-                .probeEntityPropertyDef(IP_ADDRESS, IP_ADDRESS_DESCRIPTION)
-                .externalEntityPropertyDef(VM_IP)
-                .build();
-
-        supplyChainBuilder.entity(appComponent)
-                .connectsTo(appComponentToVmLink)
-                .connectsTo(appComponentToVmExternalLink)
-                .connectsTo(appComponentToContainerLink)
-                .connectsTo(appComponentToContainerExternalLink);
-    }
-
-    private void createDbServerLinks(
-            SupplyChainBuilder supplyChainBuilder,
-            SupplyChainNodeBuilder dbServer) {
-        final SupplyChainLink dbServerToVmLink = new SupplyChainLinkBuilder()
-                .link(DATABASE_SERVER, VIRTUAL_MACHINE, HOSTING)
-                .commodity(VMEM)
-                .commodity(VCPU)
-                .commodityWithKey(APPLICATION)
-                .build();
-
-        final SupplyChain.ExternalEntityLink dbServerToVmExternalLink = new ExternalLinkBuilder()
-                .link(DATABASE_SERVER, VIRTUAL_MACHINE, HOSTING)
-                .commodityWithKey(APPLICATION)
-                .probeEntityPropertyDef(IP_ADDRESS, IP_ADDRESS_DESCRIPTION)
-                .externalEntityPropertyDef(VM_IP)
-                .build();
-
-        final SupplyChainLink dbServerToContainerLink = new SupplyChainLinkBuilder()
-                .link(DATABASE_SERVER, CONTAINER, HOSTING)
-                .commodity(VMEM)
-                .commodity(VCPU)
-                .commodity(VSTORAGE)
-                .commodityWithKey(APPLICATION)
-                .build();
-
-        final SupplyChain.ExternalEntityLink dbServerToContainerExternalLink = new ExternalLinkBuilder()
-                .link(DATABASE_SERVER, CONTAINER, HOSTING)
-                .commodityWithKey(APPLICATION)
-                .probeEntityPropertyDef(IP_ADDRESS, IP_ADDRESS_DESCRIPTION)
-                .externalEntityPropertyDef(VM_IP)
-                .build();
-
-        supplyChainBuilder.entity(dbServer)
-                .connectsTo(dbServerToVmLink)
-                .connectsTo(dbServerToVmExternalLink)
-                .connectsTo(dbServerToContainerLink)
-                .connectsTo(dbServerToContainerExternalLink);
-    }
 }
