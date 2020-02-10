318c3173532d1255599cf53e5f8a5c5619919c01
Ryan McDonald <ryan.mcdonald@turbonomic.com>
2020-02-10 14:27:43 -0500

    OM-55011 RB-37107 Add support for Reservation status updates to broadcast to UI

--- com.vmturbo.plan.orchestrator/src/main/java/com/vmturbo/plan/orchestrator/reservation/ReservationConfig.java
+++ com.vmturbo.plan.orchestrator/src/main/java/com/vmturbo/plan/orchestrator/reservation/ReservationConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import com.vmturbo.common.protobuf.plan.ReservationDTOREST.ReservationServiceController;
+import com.vmturbo.components.api.server.BaseKafkaProducerConfig;
 import com.vmturbo.plan.orchestrator.PlanOrchestratorDBConfig;
+import com.vmturbo.plan.orchestrator.api.impl.PlanOrchestratorClientImpl;
 import com.vmturbo.plan.orchestrator.plan.PlanConfig;
 import com.vmturbo.plan.orchestrator.repository.RepositoryConfig;
 import com.vmturbo.plan.orchestrator.templates.TemplatesConfig;
@@ -15,7 +17,7 @@ import com.vmturbo.plan.orchestrator.templates.TemplatesConfig;
  * Spring Configuration for Reservation services.
  */
 @Configuration
-@Import({PlanOrchestratorDBConfig.class, RepositoryConfig.class, TemplatesConfig.class})
+@Import({PlanOrchestratorDBConfig.class, RepositoryConfig.class, TemplatesConfig.class, BaseKafkaProducerConfig.class})
 public class ReservationConfig {
     @Autowired
     private PlanConfig planConfig;
@@ -29,11 +31,24 @@ public class ReservationConfig {
     @Autowired
     private RepositoryConfig repositoryConfig;
 
+    @Autowired
+    private BaseKafkaProducerConfig kafkaProducerConfig;
 
     @Bean
     public ReservationRpcService reservationRpcService() {
         return new ReservationRpcService(planConfig.planDao(), templatesConfig.templatesDao(),
                 dbConfig.reservationDao(), planConfig.planService(), reservationManager());
+    }
+
+    /**
+     * Create a {@link ReservationNotificationSender}.
+     *
+     * @return the reservation notification sender.
+     */
+    @Bean
+    public ReservationNotificationSender reservationNotificationSender() {
+        return new ReservationNotificationSender(kafkaProducerConfig.kafkaMessageSender()
+            .messageSender(PlanOrchestratorClientImpl.RESERVATION_NOTIFICATION_TOPIC));
     }
 
     /**
@@ -43,7 +58,7 @@ public class ReservationConfig {
     @Bean
     public ReservationManager reservationManager() {
         ReservationManager reservationManager = new ReservationManager(planConfig.planDao(),
-                dbConfig.reservationDao(), planConfig.planService());
+                dbConfig.reservationDao(), planConfig.planService(), reservationNotificationSender());
         planConfig.planDao().addStatusListener(reservationManager);
         return reservationManager;
     }
--- com.vmturbo.plan.orchestrator/src/main/java/com/vmturbo/plan/orchestrator/reservation/ReservationDeletedListener.java
+++ com.vmturbo.plan.orchestrator/src/main/java/com/vmturbo/plan/orchestrator/reservation/ReservationDeletedListener.java
@@ -7,7 +7,7 @@ import com.vmturbo.common.protobuf.plan.ReservationDTO.Reservation;
 /**
  * An interface to be implemented by classes that wish to receive events related to reservations.
  */
-public interface ReservationStatusListener {
+public interface ReservationDeletedListener {
 
     /**
      * A callback to be called when a reservation has been successfully deleted.
