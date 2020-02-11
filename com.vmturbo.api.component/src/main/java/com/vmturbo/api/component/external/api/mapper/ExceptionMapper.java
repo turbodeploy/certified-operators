ca01062edfcbaacef29368c16c938f412593b12c
jerrin.kallukalam <jerrin.kallukalam@turbonomic.com>
2020-02-10 19:16:45 -0500

    OM-55302, RB-37228 Translate ALREADY_EXISTS error to http code 400 instead of 500

--- com.vmturbo.api.component/src/main/java/com/vmturbo/api/component/external/api/mapper/ExceptionMapper.java
+++ com.vmturbo.api.component/src/main/java/com/vmturbo/api/component/external/api/mapper/ExceptionMapper.java
@@ -42,6 +42,7 @@ public class ExceptionMapper {
             case CANCELLED:
                 return new InterruptedException(statusException.getMessage());
             case INVALID_ARGUMENT:
+            case ALREADY_EXISTS:
                 return new InvalidOperationException(statusException.getMessage());
             default:
                 return new OperationFailedException(statusException.getMessage(), statusException);
