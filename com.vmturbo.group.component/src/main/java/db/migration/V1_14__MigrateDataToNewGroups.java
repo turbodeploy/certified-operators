c5556e33935fa42e095a24d2bc2052baa8f76d97
mahdi_bashari <mahdi.bashari@turbonomic.com>
2020-02-11 13:16:15 -0500

    OM-55359-Fix the issue that resulted in migration failure for BofA toplogy
    Intent
    ======
    -Ignore duplicate members in the group when migrating from 7.1x to 7.2x
    -Ignore the nested groups with groups that does not already exists.
    
    Problem
    =======
    When upgrading an opsMgr running 7.17 with BofA toplogy to 7.21 the migration fails.
    
    Diagnosis
    =========
    - The migration initially failed because some groups listed an entity oid twice in their members. Since there is primary key on entity oids for a group that will result in failure in inserting to group table.
    - After fixing this issue, the migration failed as there was nested group which had a child group which did not existed. As nested group children has a foreign key to groups table. That result in migration failure as well.
    
    Solution
    ========
    The solution is simply ignoring those entries when inserting to new tables. This solution simply don't insert the second entity with duplicate PK to the group members table.

--- com.vmturbo.group.component/src/main/java/db/migration/V1_14__MigrateDataToNewGroups.java
+++ com.vmturbo.group.component/src/main/java/db/migration/V1_14__MigrateDataToNewGroups.java
@@ -275,7 +275,8 @@ public class V1_14__MigrateDataToNewGroups implements JdbcMigration {
             @Nonnull StaticGroupMembers staticMembers) throws SQLException {
         final Collection<Long> members = staticMembers.getStaticMemberOidsList();
         final PreparedStatement membersStmt = connection.prepareStatement(
-                "INSERT INTO group_static_members_entities (group_id, entity_type, entity_id) VALUES (?, ?, ?)");
+                "INSERT IGNORE INTO group_static_members_entities (group_id, entity_type, " +
+                    "entity_id) VALUES (?, ?, ?)");
         for (Long member : members) {
             membersStmt.setLong(1, oid);
             membersStmt.setInt(2, entityType);
@@ -289,7 +290,8 @@ public class V1_14__MigrateDataToNewGroups implements JdbcMigration {
             @Nonnull StaticGroupMembers staticMembers) throws SQLException {
         final Collection<Long> members = staticMembers.getStaticMemberOidsList();
         final PreparedStatement membersStmt = connection.prepareStatement(
-                "INSERT INTO group_static_members_groups (parent_group_id, child_group_id) VALUES (?, ?)");
+                "INSERT IGNORE INTO group_static_members_groups (parent_group_id, child_group_id) " +
+                    "VALUES (?, ?)");
         for (Long member : members) {
             membersStmt.setLong(1, oid);
             membersStmt.setLong(2, member);
