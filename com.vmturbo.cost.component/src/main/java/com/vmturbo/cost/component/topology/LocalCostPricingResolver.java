43e79620d3556b6a7b3fb235688f74025216b498
daniel_burtman <daniel.burtman@turbonomic.com>
2020-02-19 11:52:13 -0500

    OM-55180, RB-37410: Significant leaks in market/cost pods when Cloud targets are added

--- com.vmturbo.cost.component/src/main/java/com/vmturbo/cost/component/topology/LocalCostPricingResolver.java
+++ com.vmturbo.cost.component/src/main/java/com/vmturbo/cost/component/topology/LocalCostPricingResolver.java
@@ -11,6 +11,9 @@ import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.vmturbo.common.protobuf.cost.Cost.Discount;
 import com.vmturbo.common.protobuf.cost.Pricing.PriceTable;
 import com.vmturbo.common.protobuf.topology.TopologyDTO.TopologyEntityDTO;
@@ -34,18 +37,13 @@ import com.vmturbo.sql.utils.DbException;
  * A class for resolving the mapping of business accounts to account pricing data.
  */
 public class LocalCostPricingResolver extends ResolverPricing {
+    private static final Logger logger = LogManager.getLogger();
 
     private final BusinessAccountPriceTableKeyStore businessAccountPriceTableKeyStore;
 
     private final PriceTableStore priceTableStore;
 
-    private final IdentityProvider identityProvider;
-
     private final DiscountStore discountStore;
-
-    private final DiscountApplicatorFactory<TopologyEntityDTO> discountApplicatorFactory;
-
-    private final EntityInfoExtractor<TopologyEntityDTO> topologyEntityInfoExtractor;
 
     /**
      * Constructor for the local cost pricing resolver.
@@ -64,18 +62,21 @@ public class LocalCostPricingResolver extends ResolverPricing {
                                     @Nonnull DiscountApplicatorFactory<TopologyEntityDTO>
                                             discountApplicatorFactory,
                                     @Nonnull TopologyEntityInfoExtractor topologyEntityInfoExtractor) {
+        super(discountApplicatorFactory, topologyEntityInfoExtractor);
         this.businessAccountPriceTableKeyStore = Objects.requireNonNull(businessAccountPriceTableKeyStore);
         this.priceTableStore = priceTableStore;
-        this.identityProvider = identityProvider;
         this.discountStore = discountStore;
-        this.discountApplicatorFactory = discountApplicatorFactory;
-        this.topologyEntityInfoExtractor = topologyEntityInfoExtractor;
     }
 
     @Override
     public Map<Long, AccountPricingData<TopologyEntityDTO>> getAccountPricingDataByBusinessAccount(
             @Nonnull final CloudTopology<TopologyEntityDTO> cloudTopo)
             throws CloudCostDataRetrievalException {
+        // A map of the PricingDataIdentifier to the Account Pricing data.
+        // This map helps reusing AccountPricingData which often is identical for many BAs.
+        Map<PricingDataIdentifier, AccountPricingData<TopologyEntityDTO>>
+                accountPricingDataMapByPricingDataIdentifier = new HashMap<>();
+
         Map<Long, AccountPricingData<TopologyEntityDTO>> accountPricingDataByBusinessAccountOid =
                 new HashMap<>();
         Set<Long> baOids = cloudTopo.getAllEntitiesOfType(EntityType.BUSINESS_ACCOUNT_VALUE).stream()
@@ -83,13 +84,14 @@ public class LocalCostPricingResolver extends ResolverPricing {
 
         final Map<Long, Long> priceTableKeyOidByBusinessAccountOid = businessAccountPriceTableKeyStore
                 .fetchPriceTableKeyOidsByBusinessAccount(baOids);
+        logger.debug("Cost:priceTableKeyOidByBusinessAccountOid = {}", priceTableKeyOidByBusinessAccountOid);
 
         final Map<Long, PriceTable> priceTableByPriceTableKeyOid = priceTableStore
                 .getPriceTables(priceTableKeyOidByBusinessAccountOid.values());
 
-        final Map<Long, Discount> discountsByAccountId;
+        final Map<Long, Discount> discountsByAccount;
         try {
-            discountsByAccountId = discountStore.getAllDiscount().stream()
+            discountsByAccount = discountStore.getAllDiscount().stream()
                     .collect(Collectors.toMap(Discount::getAssociatedAccountId, Function.identity()));
         } catch (DbException e) {
             throw new CloudCostDataRetrievalException(e);
@@ -98,28 +100,14 @@ public class LocalCostPricingResolver extends ResolverPricing {
         Map<Long, PriceTable> priceTableByBusinessAccountOid = getPriceTableByBusinessAccountOid(priceTableKeyOidByBusinessAccountOid,
                 priceTableByPriceTableKeyOid);
         for (Map.Entry<Long, PriceTable> entry : priceTableByBusinessAccountOid.entrySet()) {
-            Long businessAccountOid = entry.getKey();
+            Long baOid = entry.getKey();
             PriceTable priceTable = entry.getValue();
-            Optional<Discount> discount = Optional.ofNullable(discountsByAccountId.get(businessAccountOid));
-            PricingDataIdentifier pricingDataIdentifier = new PricingDataIdentifier(discount, priceTable);
-            if (accountPricingDataMapByPricingDataIdentifier.get(pricingDataIdentifier) != null) {
-                accountPricingDataByBusinessAccountOid.put(businessAccountOid,
-                        accountPricingDataMapByPricingDataIdentifier
-                        .get(pricingDataIdentifier));
-            } else {
-                final AtomicLong accountProviderOid = new AtomicLong(identityProvider.next());
-                final DiscountApplicator<TopologyEntityDTO> discountApplicator =
-                        discountApplicatorFactory.accountDiscountApplicator(businessAccountOid,
-                        cloudTopo, topologyEntityInfoExtractor, discount);
-                AccountPricingData<TopologyEntityDTO> accountPricingData =
-                        new AccountPricingData<>(discountApplicator, priceTable,
-                                accountProviderOid.longValue());
-                PricingDataIdentifier newPricingDataIdentifier = new PricingDataIdentifier(discount, priceTable);
-                accountPricingDataMapByPricingDataIdentifier.putIfAbsent(newPricingDataIdentifier,
-                        accountPricingData);
-                accountPricingDataByBusinessAccountOid.put(businessAccountOid, accountPricingData);
-            }
+            Long priceTableOid = priceTableKeyOidByBusinessAccountOid.get(baOid);
+
+            buildPricingDataByBizAccount(cloudTopo, accountPricingDataMapByPricingDataIdentifier, discountsByAccount,
+                    accountPricingDataByBusinessAccountOid, baOid, priceTable, priceTableOid);
         }
+
         return accountPricingDataByBusinessAccountOid;
     }
 }
