b2fcf3ca7e784f659db09a039a6d40377a5b6d66
gary_zeng <gary.zeng@turbonomic.com>
2020-03-10 09:57:51 -0400

    OM-53209: fetch the provided DB credentials and feed to each component (rb37771)
    Background:
    
    This is for Cisco interisght integration.
    Db credentials will be provided by Hashicorp init container. We want to fetch and feed them to each component.
    Note: credentials can be rotated (updated) with sidecar, we could potentially support it in future item.
    
    Design:
    
    It extends the existing properties fetching framework and add secrets.
    
    Updates:
    
    Added mapping from generic credential to component type specific credential.
    E.g.: In Auth component,
    username -> authDbUsername.
    password -> authDbPassword.
    These mappings are defined in Spring config file.
    
    Note: ArangoDB mapping will be added when the exact format is confirmed by the Cisco Intersight team.
    
    Testing Done
    Added new unit tests.
    Done integration tests.

---
+++ com.vmturbo.components.common/src/test/java/com/vmturbo/components/common/config/SecretPropertiesReaderTest.java
@@ -0,0 +1,102 @@
+package com.vmturbo.components.common.config;
+
+import static com.vmturbo.components.common.config.SecretPropertiesReader.PASSWORD;
+import static com.vmturbo.components.common.config.SecretPropertiesReader.USERNAME;
+import static com.vmturbo.components.common.config.SecretPropertiesReader.componentCredentialKeyMap;
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Properties;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import com.vmturbo.components.common.config.SecretPropertiesReader.MissingSecretEntry;
+import com.vmturbo.components.common.config.SecretPropertiesReader.UnknowComponentException;
+
+/**
+ * Verify {@link SecretPropertiesReader}.
+ */
+@RunWith(Parameterized.class)
+public class SecretPropertiesReaderTest {
+
+    private final String componentType;
+
+    /**
+     * Constructor.
+     *
+     * @param componentType component type.
+     */
+    public SecretPropertiesReaderTest(String componentType) {
+        this.componentType = componentType;
+    }
+
+    /**
+     * Parameters setup.
+     *
+     * @return list of parameters.
+     */
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"action-orchestrator"}, {"auth"}, {"cost"}, {"group"}, {"history"}, {"plan-orchestrator"}, {"topology-processor"}
+        });
+    }
+
+    /**
+     * Positive case.
+     *
+     * @throws IOException when IO exception thrown
+     */
+    @Test
+    public void testReadSecretFile() throws IOException {
+        // act
+        Properties result = SecretPropertiesReader.readSecretFile(componentType,
+                "secretMap/sample_secrets");
+        // assert
+        assertEquals("A1a-9Y9tLPAX2NXOJYb9", result.get(
+                componentCredentialKeyMap.get(componentType).get(PASSWORD)));
+        assertEquals("v-kubernetes-coke-plan--3AvWqRZs", result.get(
+                componentCredentialKeyMap.get(componentType).get(USERNAME)));
+    }
+
+    /**
+     * Negative case, missing required key (password).
+     *
+     * @throws IOException when IO exception thrown
+     */
+    @Test(expected = MissingSecretEntry.class)
+    public void testReadIncompleteSecretFile() throws IOException {
+        // act
+        Properties result =
+                SecretPropertiesReader.readSecretFile(componentType,
+                        "secretMap/sample_secrets_missing_password");
+    }
+
+    /**
+     * Negative case, component type is not define in {@link SecretPropertiesReader#componentCredentialKeyMap}.
+     *
+     * @throws IOException when IO exception thrown
+     */
+    @Test(expected = UnknowComponentException.class)
+    public void testReadSecretFileInUnknownComponent() throws IOException {
+        // act
+        Properties result =
+                SecretPropertiesReader.readSecretFile("unknown",
+                        "secretMap/sample_secrets_missing_password");
+    }
+
+    /**
+     * Negative case, missing the secret file.
+     *
+     * @throws IOException when IO exception thrown
+     */
+    @Test(expected = FileNotFoundException.class)
+    public void testReadMissingSecretFile() throws IOException {
+        SecretPropertiesReader.readSecretFile(componentType, "secretMap/notExistedSample_secrets");
+    }
+}
