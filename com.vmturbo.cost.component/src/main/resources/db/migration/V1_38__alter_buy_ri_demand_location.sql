d41649e34b71440943ff57975aeadab937cb48da
Harnoor Powar <harnoor.powar@turbonomic.com>
2020-03-18 11:51:55 -0400

    OM-56146: Make Cost db migration Scripts Idempotent to prevent migration failures between different versions. RB:37901

--- com.vmturbo.cost.component/src/main/resources/db/migration/V1_38__alter_buy_ri_demand_location.sql
+++ com.vmturbo.cost.component/src/main/resources/db/migration/V1_38__alter_buy_ri_demand_location.sql
@@ -1,4 +1,19 @@
 -- Alter the location column in the buy RI demand table from availability zone to region_or_zone to
 -- reflect storage of Azure demand.
+DROP PROCEDURE IF EXISTS `CHANGE_COLUMN_V__38`;
+-- add new columns to tables
+DELIMITER //
+    CREATE PROCEDURE CHANGE_COLUMN_V__38()
+    BEGIN
+        DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+        BEGIN
+            SELECT CONCAT('Failed to ', @operation, ' on table ', @table);
+        END;
+        SET @table='compute_tier_type_hourly_by_week';
+        SET @operation='change `availability_zone to region_or_zone` column';
+        ALTER TABLE compute_tier_type_hourly_by_week CHANGE COLUMN availability_zone region_or_zone_id BIGINT NOT NULL;
+    END//
+DELIMITER ;
 
-ALTER TABLE compute_tier_type_hourly_by_week CHANGE COLUMN availability_zone region_or_zone_id BIGINT NOT NULL;
+call CHANGE_COLUMN_V__38();
+DROP PROCEDURE IF EXISTS `CHANGE_COLUMN_V__38`;
