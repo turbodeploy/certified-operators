a368cd0c0a0b94c549fdef59993d717cca58282d
bonnie.wong <bonnie.wong@turbonomic.com>
2020-03-20 16:50:09 -0400

    OM-56519, RB-38002: Azure VHD Files attached to VM should always be AttachmentState.ATTACHED

--- com.vmturbo.mediation.conversion/src/test/java/com/vmturbo/mediation/conversion/cloud/CloudDiscoveryConverterTest.java
+++ com.vmturbo.mediation.conversion/src/test/java/com/vmturbo/mediation/conversion/cloud/CloudDiscoveryConverterTest.java
@@ -11,18 +11,27 @@ import static org.mockito.Mockito.when;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-import com.google.common.collect.ImmutableSet;
+import javax.annotation.Nonnull;
 
 import org.junit.Test;
+
+import com.google.common.collect.ImmutableSet;
 
 import com.vmturbo.mediation.conversion.util.CloudService;
 import com.vmturbo.platform.common.builders.CommodityBuilders;
 import com.vmturbo.platform.common.builders.EntityBuilders;
+import com.vmturbo.platform.common.dto.CommonDTO.CommodityDTO;
+import com.vmturbo.platform.common.dto.CommonDTO.CommodityDTO.CommodityType;
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO;
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.Builder;
+import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.CommodityBought;
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.EntityProperty;
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.EntityType;
+import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.SubDivisionData;
+import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.VirtualMachineData;
+import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.VirtualMachineData.VirtualMachineFileDescriptor;
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.VirtualMachineFileType;
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.VirtualVolumeData;
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.VirtualVolumeData.AttachmentState;
@@ -146,4 +155,121 @@ public class CloudDiscoveryConverterTest {
         final String consistsOfId = serviceProvider.get().getConsistsOf(0);
         assertEquals(CloudService.AWS_EBS.getId(), consistsOfId);
     }
+
+    /**
+     * Test for VHD file attached to a VM, the VV entityDTO should have a attached status.
+     */
+    @Test
+    public void testAzureVHDFileAttachedToVMShouldHasAttachedStatus() {
+        // Given
+        final String vmId = "azure::VM::8c009224-74d3-4da1-8423-b49b23c39f47";
+        final String vmDisplayName = "discoelastic1";
+        final String stProviderId = "azure::ST::eastus2-unmanaged_standard";
+        final String vhdFilePath = "https://6ajihufzztissstorage.blob.core.windows.net/vhds/os-discoelastic1.vhd";
+        final String subDivisionId = "https:::::6ajihufzztissstorage.blob.core.windows.net::vhds::os-discoelastic1.vhd";
+        final String vvDisplayName = "os-discoelastic1.vhd";
+
+        final EntityDTO vmDto = createVMEntityDTOWithFile(vmId,
+                                                          vmDisplayName,
+                                                          stProviderId,
+                                                          vhdFilePath,
+                                                          subDivisionId,
+                                                          vvDisplayName);
+
+        final DiscoveryResponse response = DiscoveryResponse.newBuilder().addEntityDTO(vmDto).build();
+        final CloudProviderConversionContext context = mock(CloudProviderConversionContext.class);
+
+        // When
+        final CloudDiscoveryConverter converter = new CloudDiscoveryConverter(response, context);
+        final DiscoveryResponse convertedResponse = converter.convert();
+
+        // Verify
+        assertEquals(2, convertedResponse.getEntityDTOCount());
+
+        final List<EntityDTO> vvDtos = convertedResponse.getEntityDTOList().stream()
+            .filter(entityDTO -> entityDTO.getEntityType() == EntityType.VIRTUAL_VOLUME)
+            .collect(Collectors.toList());
+
+        assertEquals(1, vvDtos.size());
+        final EntityDTO vvDto = vvDtos.get(0);
+        assertEquals(EntityType.VIRTUAL_VOLUME, vvDto.getEntityType());
+        assertTrue(vvDto.hasVirtualVolumeData());
+        assertEquals(AttachmentState.ATTACHED, vvDto.getVirtualVolumeData().getAttachmentState());
+    }
+
+    /**
+     * Create VM EntityDTO with VirtualMachineData has a file.
+     *
+     * @param vmId VM id
+     * @param vmDisplayName VM displayName
+     * @param stProviderId Storage Tier Id
+     * @param vhdFilePath full vhd file path
+     * @param subDivisionId vhd file path in subDivision form
+     * @param vvDisplayName VV DisplayName
+     * @return {@link EntityDTO} VM EntityDTO created
+     */
+    @Nonnull
+    private EntityDTO createVMEntityDTOWithFile(@Nonnull final String vmId,
+                                                @Nonnull final String vmDisplayName,
+                                                @Nonnull final String stProviderId,
+                                                @Nonnull final String vhdFilePath,
+                                                @Nonnull final String subDivisionId,
+                                                @Nonnull final String vvDisplayName) {
+        return EntityDTO.newBuilder()
+                .setEntityType(EntityType.VIRTUAL_MACHINE)
+                .setId(vmId)
+                .setDisplayName(vmDisplayName)
+                .addCommoditiesBought(CommodityBought.newBuilder()
+                    .setProviderId(stProviderId)
+                    .addBought(CommodityDTO.newBuilder()
+                        .setCommodityType(CommodityType.STORAGE_ACCESS)
+                    )
+                    .addBought(CommodityDTO.newBuilder()
+                        .setCommodityType(CommodityType.STORAGE_LATENCY)
+                    )
+                    .addBought(CommodityDTO.newBuilder()
+                        .setCommodityType(CommodityType.STORAGE_LATENCY)
+                    )
+                    .addBought(CommodityDTO.newBuilder()
+                        .setCommodityType(CommodityType.STORAGE_PROVISIONED)
+                        .setUsed(30720d)
+                        .setActive(false)
+                    )
+                    .addBought(CommodityDTO.newBuilder()
+                        .setCommodityType(CommodityType.STORAGE_AMOUNT)
+                        .setUsed(30720d)
+                    )
+                    .addBought(CommodityDTO.newBuilder()
+                        .setCommodityType(CommodityType.STORAGE_CLUSTER)
+                        .setKey("Group::UNMANAGED_STANDARD")
+                        .setUsed(1d)
+                    )
+                    .addBought(CommodityDTO.newBuilder()
+                        .setCommodityType(CommodityType.DSPM_ACCESS)
+                        .setKey("PhysicalMachine::azure::eastus2::PM::eastus2")
+                        .setUsed(1d)
+                    )
+                    .setProviderType(EntityType.STORAGE)
+                    .setSubDivision(SubDivisionData.newBuilder()
+                        .setSubDivisionId(subDivisionId)
+                        .setRedundancyType("LRS")
+                        .setStorageAccessCapacity(500f)
+                        .setStorageAmountCapacity(30720f)
+                        .setDisplayName(vvDisplayName)
+                        .build())
+                )
+                .setVirtualMachineData(VirtualMachineData.newBuilder()
+                    .addIpAddress("10.0.0.4")
+                    .addFile(VirtualMachineFileDescriptor.newBuilder()
+                        .setStorageId(stProviderId)
+                        .setPath(vhdFilePath)
+                        .setSizeKb(31457280)
+                        .setType(VirtualMachineFileType.DISK)
+                        .build()
+                    )
+                    .setNumElasticIps(0)
+                )
+                .build();
+    }
+
 }
