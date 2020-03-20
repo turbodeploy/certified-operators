4c54d58a2ce3ae6c2f72afbf3f798e4a0c377df7
andrei_astra <andrei.astrakharchik@turbonomic.com>
2020-03-20 09:34:07 +0300

    OM-55494 RB:37974 vSAN: Fix UnknownProfile

--- com.vmturbo.common.protobuf/src/test/java/com/vmturbo/common/protobuf/search/SearchProtoUtilTest.java
+++ com.vmturbo.common.protobuf/src/test/java/com/vmturbo/common/protobuf/search/SearchProtoUtilTest.java
@@ -38,7 +38,7 @@ public class SearchProtoUtilTest {
                 "Network", "LogicalPool", "Database", "DatabaseServer", "LoadBalancer",
                 "BusinessAccount", "CloudService", "ComputeTier", "StorageTier", "DatabaseTier",
                 "DatabaseServerTier", "AvailabilityZone", "Region", "VirtualVolume", "ProcessorPool",
-                "ViewPod", "DesktopPool", "BusinessUser", "ServiceProvider");
+                "ViewPod", "DesktopPool", "BusinessUser", "ServiceProvider", "HCIPhysicalMachine");
         assertThat(SearchProtoUtil.SEARCH_ALL_TYPES, containsInAnyOrder(expectedTypes.toArray()));
     }
 
