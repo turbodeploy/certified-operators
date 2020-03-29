8f333dfa1989dbd98b052a177140fb3963fa8c32
Igor Golikov <igor.golikov@turbonomic.com>
2020-03-29 23:34:53 +0300

    OM-56109: RB-38086 Rework existing RI Buy unit tests

--- com.vmturbo.cost.component/src/test/java/com/vmturbo/cost/component/reserved/instance/recommendationalgorithm/ReservedInstanceAnalyzerTest.java
+++ com.vmturbo.cost.component/src/test/java/com/vmturbo/cost/component/reserved/instance/recommendationalgorithm/ReservedInstanceAnalyzerTest.java
@@ -1,10 +1,13 @@
 package com.vmturbo.cost.component.reserved.instance.recommendationalgorithm;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,11 +15,11 @@ import java.util.Map;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
 import com.vmturbo.common.protobuf.common.EnvironmentTypeEnum.EnvironmentType;
+import com.vmturbo.common.protobuf.cost.Cost;
 import com.vmturbo.common.protobuf.cost.Cost.RIPurchaseProfile;
 import com.vmturbo.common.protobuf.setting.SettingProto.EnumSettingValue;
 import com.vmturbo.common.protobuf.setting.SettingProto.GetMultipleGlobalSettingsRequest;
@@ -37,19 +40,24 @@ import com.vmturbo.cost.component.pricing.PriceTableStore;
 import com.vmturbo.cost.component.reserved.instance.ActionContextRIBuyStore;
 import com.vmturbo.cost.component.reserved.instance.BuyReservedInstanceStore;
 import com.vmturbo.cost.component.reserved.instance.action.ReservedInstanceActionsSender;
+import com.vmturbo.cost.component.reserved.instance.recommendationalgorithm.demand.AccountGroupingIdentifier;
+import com.vmturbo.cost.component.reserved.instance.recommendationalgorithm.demand.ImmutableAccountGroupingIdentifier;
+import com.vmturbo.cost.component.reserved.instance.recommendationalgorithm.demand.ImmutableRIBuyRegionalContext;
 import com.vmturbo.cost.component.reserved.instance.recommendationalgorithm.demand.RIBuyAnalysisContextProvider;
+import com.vmturbo.cost.component.reserved.instance.recommendationalgorithm.demand.RIBuyRegionalContext;
+import com.vmturbo.cost.component.reserved.instance.recommendationalgorithm.demand.calculator.RIBuyDemandCalculator;
 import com.vmturbo.cost.component.reserved.instance.recommendationalgorithm.demand.calculator.RIBuyDemandCalculatorFactory;
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.EntityType;
+import com.vmturbo.platform.sdk.common.CloudCostDTO.OSType;
 import com.vmturbo.platform.sdk.common.CloudCostDTO.ReservedInstanceType;
 import com.vmturbo.platform.sdk.common.CloudCostDTO.ReservedInstanceType.OfferingClass;
 import com.vmturbo.platform.sdk.common.CloudCostDTO.ReservedInstanceType.PaymentOption;
+import com.vmturbo.platform.sdk.common.CloudCostDTO.Tenancy;
 
 /**
  * This class tests methods in the ReservedInstanceAnalyzer class.
  */
 public class ReservedInstanceAnalyzerTest {
-
-    static final long MASTER_ID = 11111;
 
     static final long ZONE_ID = 33333;
 
@@ -68,26 +76,54 @@ public class ReservedInstanceAnalyzerTest {
      /**
      * This method tests ReservedInstanceAnalyzer::getHourlyOnDemandCost method.
      **/
-    @Ignore
-    public void testGetHourlyOnDemandCost() {
-        //TODO use new classes
-        /*TopologyEntityDTO buyComputeTier = buildComputeTierDTO();
-        TopologyEntityDTO region = buildRegionDTO();
-        Map<TopologyEntityDTO, float[]> templateTypeHourlyDemand = new HashMap<>();
-        float[] demand = new float[ReservedInstanceDataProcessor.WEEKLY_DEMAND_DATA_SIZE];
-        Arrays.fill(demand, 4);
-        templateTypeHourlyDemand.put(buyComputeTier, demand);
-        ReservedInstanceRegionalContext regionalContext = new ReservedInstanceRegionalContext(MASTER_ID,
-                OSType.LINUX, Tenancy.DEFAULT, buyComputeTier, region);
-        ReservedInstanceAnalyzerRateAndRIs priceAndRIProvider = Mockito
-                .mock(ReservedInstanceAnalyzerRateAndRIs.class);
-        Mockito.when(priceAndRIProvider.lookupOnDemandRate(any(), any())).thenReturn(1f);
-        ReservedInstanceAnalyzer analyzer = new ReservedInstanceAnalyzer();
-        final float hourlyOnDemandCost = analyzer.getHourlyOnDemandCost(templateTypeHourlyDemand,
-                        regionalContext, priceAndRIProvider, "RILT0000");
-        assertEquals(1f, hourlyOnDemandCost, 0.0);
-        */
-    }
+     @Test
+     public void testGetHourlyOnDemandCost() {
+
+         TopologyEntityDTO buyComputeTier = buildComputeTierDTO();
+         TopologyEntityDTO region = buildRegionDTO();
+         Map<TopologyEntityDTO, float[]> templateTypeHourlyDemand = new HashMap<>();
+         float[] demand = new float[RIBuyDemandCalculator.WEEKLY_DEMAND_DATA_SIZE];
+         Arrays.fill(demand, 4);
+         templateTypeHourlyDemand.put(buyComputeTier, demand);
+
+         final AccountGroupingIdentifier accountGrouping =
+                 ImmutableAccountGroupingIdentifier.builder()
+                         .groupingType(AccountGroupingIdentifier.AccountGroupingType.BILLING_FAMILY)
+                         .id(1)
+                         .tag("AccountGrouping")
+                         .build();
+
+         final ReservedInstanceType riType =
+                 ReservedInstanceType.newBuilder().setTermYears(1).build();
+         final Cost.ReservedInstanceSpecInfo riSpecInfo = Cost.ReservedInstanceSpecInfo.newBuilder()
+                 .setOs(OSType.RHEL)
+                 .setTenancy(Tenancy.DEFAULT)
+                 .setType(riType)
+                 .setPlatformFlexible(true)
+                 .build();
+
+         final Cost.ReservedInstanceSpec riToPurchase = Cost.ReservedInstanceSpec.newBuilder()
+                 .setReservedInstanceSpecInfo(riSpecInfo)
+                 .build();
+
+         final RIBuyRegionalContext regionalContext = ImmutableRIBuyRegionalContext.builder()
+                 .region(region)
+                 .riSpecToPurchase(riToPurchase)
+                 .computeTier(ReservedInstanceAnalyzerConstantsTest.COMPUTE_TIER_T2_NANO)
+                 .accountGroupingId(accountGrouping)
+                 .contextTag("Context")
+                 .analysisTag("Analysis")
+                 .build();
+
+         RIBuyRateProvider riBuyRateProvider = mock(RIBuyRateProvider.class);
+         when(riBuyRateProvider.lookupOnDemandRate(anyLong(), any(), any())).thenReturn(1f);
+
+         ReservedInstanceAnalyzer analyzer = new ReservedInstanceAnalyzer();
+         final float hourlyOnDemandCost =
+                 analyzer.getHourlyOnDemandCost(1L, regionalContext, templateTypeHourlyDemand,
+                         riBuyRateProvider);
+         Assert.assertEquals(1f, hourlyOnDemandCost, 0.0);
+     }
 
     /**
      * Tests the loading of the global purchase setting constraints.
