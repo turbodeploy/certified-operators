95e137b85cbbb1d974380b43b73d577f2ea00d5b
Professor Git <builder@turbonomic.com>
2020-04-16 17:23:37 -0400

    Reverted from xl-7.21.5-ci-build-186 and reopened jira issues (OM-57551 ) and reverted commits(58cfa3e5fac2a4fc2332ced99d45c33981eb92cd 32f30e61d040f8c192a67c852f797e9762b0878c )

--- com.vmturbo.group.component/src/main/resources/db/migration/V1_27_change_expected_group_type_mapping.sql
+++
@@ -1,6 +0,0 @@
--- This migration is aimed to change GroupType mapped by ordinal to getNumer()
--- The previous migration overlooked the group_type in the group_expected_members_groups table.
---
--- At the time of this migration, the difference between GroupType.ordinal() and GroupType.getNumber()
--- is that 1 is deprecated and unused in GroupType, and all non-Regular groups start at 2.
-UPDATE group_expected_members_groups set group_type = group_type + 1 where group_type > 0;
