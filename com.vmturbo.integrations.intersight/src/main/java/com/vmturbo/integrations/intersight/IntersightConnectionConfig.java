99ea656e92dbf64c58f83f60e624116d28f763d4
Professor Git <builder@turbonomic.com>
2020-05-28 11:31:32 -0400

    Reverted from xl-staging-ci-build-261 and reopened jira issues (OM-57390 ) and reverted commits(39ff05c1e30dec1dbb876a222dbf70d531b08c43 578132fb214fbc07a28ec1955a18002d9c0c022f )

--- com.vmturbo.integrations.intersight/src/main/java/com/vmturbo/integrations/intersight/IntersightConnectionConfig.java
+++ com.vmturbo.integrations.intersight/src/main/java/com/vmturbo/integrations/intersight/IntersightConnectionConfig.java
@@ -12,16 +12,16 @@ import com.vmturbo.mediation.connector.intersight.IntersightConnection;
 @Configuration
 public class IntersightConnectionConfig {
 
-    @Value("${intersightHost:bordeaux.default.svc.cluster.local}")
+    @Value("${intersightHost:bordeaux.default}")
     private String intersightHost;
 
     @Value("${intersightPort:8443}")
     private int intersightPort;
 
-    @Value("${intersightClientId:890ba3015647ba92fa922b62d668375972da829970eb0004f1854dbae9f17089}")
+    @Value("${intersightClientId}")
     private String intersightClientId;
 
-    @Value("${intersightClientSecret:cf21a100e648b5120d98061fc306a297}")
+    @Value("${intersightClientSecret}")
     private String intersightClientSecret;
 
     /**
