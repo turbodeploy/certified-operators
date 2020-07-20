7e46574767d281eebe48efb09dc2ab0f5871979e
Vadim Barkov <vadim.barkov@turbonomic.com>
2020-07-20 15:35:12 +0300

    [OM-60197] RB-40150 Move the StorageBrowsingEnabled flag to the main VC probe

--- com.vmturbo.mediation.conversion/src/main/java/com/vmturbo/mediation/conversion/util/ConverterUtils.java
+++ com.vmturbo.mediation.conversion/src/main/java/com/vmturbo/mediation/conversion/util/ConverterUtils.java
@@ -1,44 +1,19 @@
 package com.vmturbo.mediation.conversion.util;
 
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
 
 import com.google.common.collect.Sets;
 
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.EntityType;
-import com.vmturbo.platform.common.dto.Discovery.DerivedTargetSpecificationDTO;
-import com.vmturbo.platform.common.dto.Discovery.DiscoveryResponse;
 import com.vmturbo.platform.common.dto.SupplyChain.TemplateDTO;
 import com.vmturbo.platform.sdk.common.supplychain.SupplyChainNodeBuilder;
-import com.vmturbo.platform.sdk.common.util.SDKProbeType;
 
 /**
  * Helper class containing constants, util functions, etc, which are used by different converters.
  */
 public class ConverterUtils {
-
-    /**
-     * Take a {@link DiscoveryResponse} and remove any {@link DerivedTargetSpecificationDTO} that
-     * match the given {@link SDKProbeType}.
-     *
-     * @param rawDiscoveryResponse the DiscoveryResponse to filter
-     * @param derivedProbeType the type of probe whose derived targets will be removed
-     * @return {@link DiscoveryResponse} without any derived targets of the given probe type
-     */
-    public static DiscoveryResponse removeDerivedTargets(@Nonnull DiscoveryResponse rawDiscoveryResponse,
-                                                         @Nonnull SDKProbeType derivedProbeType) {
-        List<DerivedTargetSpecificationDTO> targetsToKeep =
-            rawDiscoveryResponse.getDerivedTargetList().stream()
-                .filter(derivedTargetSpec -> derivedTargetSpec.hasProbeType() &&
-                    !derivedTargetSpec.getProbeType()
-                        .equals(derivedProbeType.getProbeType()))
-                .collect(Collectors.toList());
-        return rawDiscoveryResponse.toBuilder().clearDerivedTarget()
-            .addAllDerivedTarget(targetsToKeep).build();
-    }
 
     /**
      * Add a basic TemplateDTO to the existing set if it doesn't exist. This is used to complete
