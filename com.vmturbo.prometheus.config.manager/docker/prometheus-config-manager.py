3113c243919747b7efcc7a6652f6f06ea25c7051
Vaptistis Anogeianakis <vaptistis.anogeianakis@turbonomic.com>
2020-08-14 01:19:25 +0300

    [OM-61086] Minor changes. Mostly to address warnings in IDE.
    - Added a newline at the end of Dockerfile.
    - Added a #! so the script can be executed without explicitly calling Python.
    - Removed and added some whitespace.
    - Removed trailing ‘;’ as they aren’t used in Python.
    - Removed an unused import.
    - Removed a TODO since I’m starting work to do it.
    - Removed some '*' from README.md as these 3 lines were intended to be a
      single bullet.

--- com.vmturbo.prometheus.config.manager/docker/prometheus-config-manager.py
+++ com.vmturbo.prometheus.config.manager/docker/prometheus-config-manager.py
@@ -1,21 +1,21 @@
-## TODO: OM-61086 python script to modify configuration
+#! /usr/bin/python3
+
 import requests
-import time
 
 # defining endpoint to get telemetryEnabled setting from group component
-SETTINGS_ENDPOINT = "http://group:8080/SettingService/getMultipleGlobalSettings";
+SETTINGS_ENDPOINT = "http://group:8080/SettingService/getMultipleGlobalSettings"
 # data of POST request
-jsonTelemetryData = {'settingSpecName': ['telemetryEnabled']};
+jsonTelemetryData = {'settingSpecName': ['telemetryEnabled']}
 # header of content type
-headers = {'Content-type' : 'application/json;charset=UTF-8'};
+headers = {'Content-type': 'application/json;charset=UTF-8'}
 
 try:
-    settingResponse = requests.post(url=SETTINGS_ENDPOINT, headers=headers, json=jsonTelemetryData);
-    print("Response {}", str(settingResponse.json()));
+    settingResponse = requests.post(url=SETTINGS_ENDPOINT, headers=headers, json=jsonTelemetryData)
+    print("Response {}", str(settingResponse.json()))
 
-    #TODO: Call Licenses API to get license information to extract domain name
+    # TODO: Call Licenses API to get license information to extract domain name
 
-    #TODO: change prometheus config based on the setting received
+    # TODO: change prometheus config based on the setting received
 
 except requests.exceptions.ConnectionError:
-    print("Caught connection error. TODO : print stacktrace");
+    print("Caught connection error. TODO : print stacktrace")
