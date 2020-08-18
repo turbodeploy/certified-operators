294a5857edd9cbf1b12bc789a3c0e9aa88e41c07
Vaptistis Anogeianakis <vaptistis.anogeianakis@turbonomic.com>
2020-08-18 20:43:58 +0300

    [OM-61086] Added support to enable/disable telemetry based on user input
    Now the cron job can use the response it gets from group component
    regarding whether the user has enabled or not telemetry from the UI and
    update Prometheus configuration as needed to start/stop sending data to
    DataCloud. Metrics will still be scraped by Prometheus: they will just
    not be sent to the DataCloud gateway container and by extension, not to
    DataCloud.
    
    The 'safe' variants of `load` and `dump` were used for manipulating YAML
    to avoid potential security risks.
    
    Built and deployed prometheus-config-maganger to an XL VM in our lab and
    verified that:
    
    - When I enable telemetry from the UI, the DataCloud remote writer
      configuration is present in the Prometheus configuration file.
      When I disable it, it's not.
    - When I enable telemetry from the UI, I see log lines from DataCloud
      gateway container about sending data. When I disable it, I don't.
    - The cron jobs complete successfully and without errors in the logs.
    
    Also:
    
    - Added comments and TODOs to the Python script.
    - Removed code intended solely for sanity checks during initial
      development.
    - Added some rudimentary logging.
    
    The script still needs error handling code and a number of strings like
    paths and URLs need to be converted to script/container arguments and
    Helm values.

--- com.vmturbo.prometheus.config.manager/docker/prometheus-config-manager.py
+++ com.vmturbo.prometheus.config.manager/docker/prometheus-config-manager.py
@@ -1,37 +1,39 @@
 #! /usr/bin/python3
 
-# Needs to take less than 1 minute to run!
+# Needs to take less than 1 minute to run otherwise it will be terminated externally!
 
-import shutil
+# TODO: input and output paths as well as Prometheus's reload URL should become arguments to this
+# script.
+import datetime
 import requests
+import yaml
 
-print("Hello prometheus config manager!")
+# Read Prometheus configuration coming from Kubernetes through a config map.
+prometheus_config = yaml.safe_load(open("/etc/config/prometheus.yml", "r"))
 
-print(os.getcwd())
-print(open("/prometheus-config-manager.py", "r").read())
-print(open("/etc/config/prometheus.yml", "r").read())
-shutil.copy2('/etc/config/prometheus.yml', '/etc/merged-config/prometheus.yml')
-
-# defining endpoint to get telemetryEnabled setting from group component
+# Ask group component whether a user has enabled telemetry.
 SETTINGS_ENDPOINT = "http://group:8080/SettingService/getMultipleGlobalSettings"
-# data of POST request
-jsonTelemetryData = {'settingSpecName': ['telemetryEnabled']}
-# header of content type
 headers = {'Content-type': 'application/json;charset=UTF-8'}
+jsonData = {'settingSpecName': ['telemetryEnabled']}
+response = requests.post(url=SETTINGS_ENDPOINT, headers=headers, json=jsonData)
 
-try:
-    settingResponse = requests.post(url=SETTINGS_ENDPOINT, headers=headers, json=jsonTelemetryData)
-    print("Response {}", str(settingResponse.json()))
+# Modify configuration based on the answer from group.
+if not response.json()['response'][0]['booleanSettingValue']['value']:  # if not telemetry enabled
+    prometheus_config['remote_write'] = []  # disable remote writer sending data to DataCloud.
 
-    # TODO: Call Licenses API to get license information to extract domain name
+# Get the customer domain and SalesForce ID from the license.
+# To be implemented...
 
-    # TODO: change prometheus config based on the setting received
+# Modify configuration to include the proper customer domain and ID.
+prometheus_config['global']['external_labels']['customer_domain'] = 'tbd_customer_domain'
+prometheus_config['global']['external_labels']['customer_id'] = 'tbd_customer_id'
 
-except requests.exceptions.ConnectionError:
-    print("Caught connection error. TODO : print stacktrace")
+# Write the modified configuration where Prometheus can pick it up.
+yaml.safe_dump(prometheus_config, open("/etc/merged-config/prometheus.yml", "w"))
+
 # Instruct Prometheus server to reload its configuration.
 # The URL must match the one configured in Helm. And currently the '{{ .Values.server.prefixURL }}'
 # part (which is currently empty) isn't taken into account. A better way would be to pass the
 # URL as an argument to this script and container.
-print("Reload: ", requests.post("http://prometheus-server:9090/-/reload",
-    headers=headers, json=jsonData, auth=('', '')))
+requests.post("http://prometheus-server:9090/-/reload", headers=headers)
+print(datetime.datetime.now(), "INFO: Prometheus configuration updated successfully!")
