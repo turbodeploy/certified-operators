1d2ca1e37b5096d49d0717a8946d14cb908aabe4
Vaptistis Anogeianakis <vaptistis.anogeianakis@turbonomic.com>
2020-08-19 04:14:50 +0300

    [OM-61086] Added error handling to the prometheus-config-manager script.
    This is to ensure it does something reasonable in the presence of errors.
    E.g. it would be unacceptable if a network error or group component
    outage caused data to be sent to DataCloud even though the user never
    opted-in to telemetry.
    
    The error handling strategy tries its best to ensure:
    
    - No data is sent without the user consent.
    - We don't loose opportunities to send data while we have the user
      consent.
    - An error in an xl component or in the script that updates Prometheus
      configuration itself won't prevent Prometheus from carrying out its
      other tasks that aren't related to telemetry.
    
    Built and deployed prometheus-config-maganger to an XL VM in our lab and
    verified that:
    
    - Prometheus "spreads out" scrapes through the scrape interval. e.g. if
      Prometheus has to scrape 5 targets every 5 minutes, it will scrape 1
      target every minute more-or-less. It won't try to scrape 5 targets at
      once and then remain idle for 5 minutes.
    - Time continues to count towards the `scrape_interval` whether
      telemetry is on or off and regardless of how many times the
      configuration is updated in between. i.e. if `scrape_interval` is 5
      minutes and cron job runs every minute, the first points will start
      appearing in datacloud-gateway logs about 1 minutes after telemetry is
      enabled. Not 5 minutes. This is the intended behaviour and ensures
      turning telemetry off for a few minutes won't cost us a whole week of
      data.
    - If I introduce a missing key error in last run's YAML, the script will
      log a warning, but still succeed in updating the configuration.
    - If I introduce a group component outage artificially by scaling
      operator and group component to 0 replicas, the cron job will log a
      warning and use old state (tested both enabled and disabled) until
      group comes back up again.
    - If I delete the `prometheus-merged-config` persistent volume to
      simulate the first run of the script on a new installation, a warning
      will be printed stating that the merged configuration from last run is
      missing but otherwise the script will succeed in creating this file.
      Note: This can be used to turn telemetry off while group is down, if
      we need to for some reason. Deleting is safe, but I found I have to
      delete the persistent volume claim instead of the volume for things to
      be recreated and updated automatically and had to use the
      `kubectl patch pvc pvc_name -p '{"metadata":{"finalizers":null}}'`
      command from https://github.com/kubernetes/kubernetes/issues/77258.
    
    Also moved some string literals for paths and URLs to constants near
    the beginning of the script in preparation to making them arguments.

--- com.vmturbo.prometheus.config.manager/docker/prometheus-config-manager.py
+++ com.vmturbo.prometheus.config.manager/docker/prometheus-config-manager.py
@@ -2,38 +2,81 @@
 
 # Needs to take less than 1 minute to run otherwise it will be terminated externally!
 
-# TODO: input and output paths as well as Prometheus's reload URL should become arguments to this
+# This script will try hard to create some configuration for Prometheus to read, as Prometheus won't
+# startup without configuration. If group or auth component isn't accessible, the last known values
+# will be used or some conservative defaults if there are none.
+
+# TODO: Input and output paths as well as Prometheus's reload URL should become arguments to this
 # script.
+# TODO: May need to use a proper logger and/or print stack traces.
 import datetime
 import requests
 import yaml
 
-# Read Prometheus configuration coming from Kubernetes through a config map.
-prometheus_config = yaml.safe_load(open("/etc/config/prometheus.yml", "r"))
-
-# Ask group component whether a user has enabled telemetry.
-SETTINGS_ENDPOINT = "http://group:8080/SettingService/getMultipleGlobalSettings"
-headers = {'Content-type': 'application/json;charset=UTF-8'}
-jsonData = {'settingSpecName': ['telemetryEnabled']}
-response = requests.post(url=SETTINGS_ENDPOINT, headers=headers, json=jsonData)
-
-# Modify configuration based on the answer from group.
-if not response.json()['response'][0]['booleanSettingValue']['value']:  # if not telemetry enabled
-    prometheus_config['remote_write'] = []  # disable remote writer sending data to DataCloud.
-
-# Get the customer domain and SalesForce ID from the license.
-# To be implemented...
-
-# Modify configuration to include the proper customer domain and ID.
-prometheus_config['global']['external_labels']['customer_domain'] = 'tbd_customer_domain'
-prometheus_config['global']['external_labels']['customer_id'] = 'tbd_customer_id'
-
-# Write the modified configuration where Prometheus can pick it up.
-yaml.safe_dump(prometheus_config, open("/etc/merged-config/prometheus.yml", "w"))
-
-# Instruct Prometheus server to reload its configuration.
+input_config_path = "/etc/config/prometheus.yml"
+output_config_path = "/etc/merged-config/prometheus.yml"
+telemetry_enabled_url = "http://group:8080/SettingService/getMultipleGlobalSettings"
+telemetry_labels_url = ""
 # The URL must match the one configured in Helm. And currently the '{{ .Values.server.prefixURL }}'
 # part (which is currently empty) isn't taken into account. A better way would be to pass the
 # URL as an argument to this script and container.
-requests.post("http://prometheus-server:9090/-/reload", headers=headers)
-print(datetime.datetime.now(), "INFO: Prometheus configuration updated successfully!")
+prometheus_reload_url = "http://prometheus-server:9090/-/reload"
+
+# Read Prometheus configuration coming from Kubernetes through a config map.
+# If it's not there or it's malformed, there is no point in copying it to destination and an
+# unhandled exception is fine.
+prometheus_new_config = yaml.safe_load(open(input_config_path, "r"))
+
+# Attempt to read merged Prometheus configuration from a previews run. If it doesn't exist use
+# default values.
+try:
+    prometheus_old_config = yaml.safe_load(open(output_config_path, "r"))
+    old_remote_writers = prometheus_old_config['remote_write']
+    customer_domain = prometheus_old_config['global']['external_labels']['customer_domain']
+    customer_id = prometheus_old_config['global']['external_labels']['customer_id']
+except (yaml.YAMLError, OSError, KeyError) as error:
+    print(datetime.datetime.now(), "WARNING: Can't get old values. Using defaults. Cause:", error)
+    old_remote_writers = []
+    customer_domain = "unlicensed"
+    customer_id = "000000"
+
+try:
+    try:
+        # Ask group component whether a user has enabled telemetry.
+        response = requests.post(url=telemetry_enabled_url,
+                                 headers={'Content-type': 'application/json;charset=UTF-8'},
+                                 json={'settingSpecName': ['telemetryEnabled']})
+
+        # We want to handle unsuccessful status codes the same way as JSON parse errors and
+        # connection errors.
+        response.raise_for_status()
+
+        # Modify configuration based on the response from group. If telemetry isn't enabled, disable
+        # remote writer sending data to DataCloud.
+        if not response.json()['response'][0]['booleanSettingValue']['value']:
+            prometheus_new_config['remote_write'] = []
+    except (requests.RequestException, OSError, ValueError) as error:
+        print(datetime.datetime.now(),
+              "WARNING: Can't get setting from group. Using old value. Cause:", error)
+        prometheus_new_config['remote_write'] = old_remote_writers
+
+    try:
+        # Get the customer domain and SalesForce ID from the license.
+        # To be implemented...
+
+        # Modify configuration to include the proper customer domain and ID.
+        customer_domain = 'tbd_customer_domain'
+        customer_id = 'tbd_customer_id'
+    except (requests.RequestException, OSError, ValueError) as error:
+        print(datetime.datetime.now(),
+              "WARNING: Can't get licence information from auth. Using old values. Cause:", error)
+    finally:
+        prometheus_new_config['global']['external_labels']['customer_domain'] = customer_domain
+        prometheus_new_config['global']['external_labels']['customer_id'] = customer_id
+finally:
+    # Write the modified configuration where Prometheus can pick it up.
+    yaml.safe_dump(prometheus_new_config, open(output_config_path, "w"))
+
+    # Instruct Prometheus server to reload its configuration.
+    requests.post(prometheus_reload_url, headers={'Content-type': 'application/json;charset=UTF-8'})
+    print(datetime.datetime.now(), "INFO: Prometheus configuration updated successfully!")
