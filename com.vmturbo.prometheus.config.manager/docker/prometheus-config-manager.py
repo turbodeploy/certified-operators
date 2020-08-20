ca964a177d46bd5dd561d3b3ae5ba316fbaae95d
Vaptistis Anogeianakis <vaptistis.anogeianakis@turbonomic.com>
2020-08-20 23:51:09 +0300

    [OM-61086] Added support for sending customer domain as telemetry label.
    Modified prometheus configuration manager script to get the customer
    domain from the license returned by the auth component and update
    Prometheus configuration to include it in a label added to data sent to
    DataCloud.
    
    Also:
    
    - Did a selective import to shorten some calls.
    - Fixed a grammar mistake
    
    Deployed image to an XL instance in our lab that had one valid and one
    expired license and verified the string "turbonomic.com" appeared as the
    value of the "customer_domain" label in DataCloud telemetry test project.

--- com.vmturbo.prometheus.config.manager/docker/prometheus-config-manager.py
+++ com.vmturbo.prometheus.config.manager/docker/prometheus-config-manager.py
@@ -9,14 +9,15 @@
 # TODO: Input and output paths as well as Prometheus's reload URL should become arguments to this
 # script.
 # TODO: May need to use a proper logger and/or print stack traces.
-import datetime
+from datetime import datetime
+import re
 import requests
 import yaml
 
 input_config_path = "/etc/config/prometheus.yml"
 output_config_path = "/etc/merged-config/prometheus.yml"
 telemetry_enabled_url = "http://group:8080/SettingService/getMultipleGlobalSettings"
-telemetry_labels_url = ""
+telemetry_labels_url = "http://auth:8080/LicenseManagerService/getLicenses"
 # The URL must match the one configured in Helm. And currently the '{{ .Values.server.prefixURL }}'
 # part (which is currently empty) isn't taken into account. A better way would be to pass the
 # URL as an argument to this script and container.
@@ -27,7 +28,7 @@ prometheus_reload_url = "http://prometheus-server:9090/-/reload"
 # unhandled exception is fine.
 prometheus_new_config = yaml.safe_load(open(input_config_path, "r"))
 
-# Attempt to read merged Prometheus configuration from a previews run. If it doesn't exist use
+# Attempt to read merged Prometheus configuration from a previous run. If it doesn't exist use
 # default values.
 try:
     prometheus_old_config = yaml.safe_load(open(output_config_path, "r"))
@@ -35,7 +36,7 @@ try:
     customer_domain = prometheus_old_config['global']['external_labels']['customer_domain']
     customer_id = prometheus_old_config['global']['external_labels']['customer_id']
 except (yaml.YAMLError, OSError, KeyError) as error:
-    print(datetime.datetime.now(), "WARNING: Can't get old values. Using defaults. Cause:", error)
+    print(datetime.now(), "WARNING: Can't get old values. Using defaults. Cause:", error)
     old_remote_writers = []
     customer_domain = "unlicensed"
     customer_id = "000000"
@@ -56,19 +57,36 @@ try:
         if not response.json()['response'][0]['booleanSettingValue']['value']:
             prometheus_new_config['remote_write'] = []
     except (requests.RequestException, OSError, ValueError) as error:
-        print(datetime.datetime.now(),
+        print(datetime.now(),
               "WARNING: Can't get setting from group. Using old value. Cause:", error)
         prometheus_new_config['remote_write'] = old_remote_writers
 
     try:
-        # Get the customer domain and SalesForce ID from the license.
-        # To be implemented...
+        # Get all the licenses from the auth component. We'll extract customer domain and
+        # SalesForce ID from them.
+        response = requests.post(url=telemetry_labels_url,
+                                 headers={'Content-type': 'application/json;charset=UTF-8',
+                                          'Accept': 'application/json;charset=UTF-8'},
+                                 json={})
+
+        # We want to handle unsuccessful status codes the same way as JSON parse errors and
+        # connection errors.
+        response.raise_for_status()
 
         # Modify configuration to include the proper customer domain and ID.
-        customer_domain = 'tbd_customer_domain'
-        customer_id = 'tbd_customer_id'
+        # The code to get customer domain tries to mimic code to do the same thing in
+        # com.vmturbo.clustermgr.DiagEnvironmentSummary.computeLicenseDomain
+        customer_domain = '_'.join(sorted(
+            {
+                # Remove '@' and anything before it if it exists from e-mail address to get domain.
+                re.sub(r'^[^@]*@', '', license_dto['email'].strip())  # also trim whitespace.
+                for license_dto in response.json()['response']['licenseDTO']
+                # If the licence hasn't expired and has an e-mail
+                if datetime.strptime(license_dto['expirationDate'], '%Y-%m-%d') < datetime.today()
+                and 'email' in license_dto
+            }))
     except (requests.RequestException, OSError, ValueError) as error:
-        print(datetime.datetime.now(),
+        print(datetime.now(),
               "WARNING: Can't get licence information from auth. Using old values. Cause:", error)
     finally:
         prometheus_new_config['global']['external_labels']['customer_domain'] = customer_domain
@@ -79,4 +97,4 @@ finally:
 
     # Instruct Prometheus server to reload its configuration.
     requests.post(prometheus_reload_url, headers={'Content-type': 'application/json;charset=UTF-8'})
-    print(datetime.datetime.now(), "INFO: Prometheus configuration updated successfully!")
+    print(datetime.now(), "INFO: Prometheus configuration updated successfully!")
