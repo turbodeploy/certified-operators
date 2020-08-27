593d85f5bda7a6a798cb3eb94b37c17fb9a79064
gary_zeng <gary.zeng@turbonomic.com>
2020-08-27 11:21:20 -0400

    [OM-62148]: Rename new "Read Only" role to "Operational Observer" (rb41316)
    PM and ID decided to rename new "Read Only" role to "Operational Observer"
    Testing Done
    build passed.
    integration test passed

--- com.vmturbo.auth.api/src/test/java/com/vmturbo/auth/api/authorization/jwt/SecurityConstantTest.java
+++ com.vmturbo.auth.api/src/test/java/com/vmturbo/auth/api/authorization/jwt/SecurityConstantTest.java
@@ -5,8 +5,8 @@ import static com.vmturbo.auth.api.authorization.jwt.SecurityConstant.ADVISOR;
 import static com.vmturbo.auth.api.authorization.jwt.SecurityConstant.AUTOMATOR;
 import static com.vmturbo.auth.api.authorization.jwt.SecurityConstant.DEPLOYER;
 import static com.vmturbo.auth.api.authorization.jwt.SecurityConstant.OBSERVER;
+import static com.vmturbo.auth.api.authorization.jwt.SecurityConstant.OPERATIONAL_OBSERVER;
 import static com.vmturbo.auth.api.authorization.jwt.SecurityConstant.PREDEFINED_ROLE_SET;
-import static com.vmturbo.auth.api.authorization.jwt.SecurityConstant.READ_ONLY;
 import static com.vmturbo.auth.api.authorization.jwt.SecurityConstant.SHARED_ADVISOR;
 import static com.vmturbo.auth.api.authorization.jwt.SecurityConstant.SHARED_OBSERVER;
 import static com.vmturbo.auth.api.authorization.jwt.SecurityConstant.SITE_ADMIN;
@@ -31,7 +31,7 @@ public class SecurityConstantTest {
         assertTrue(PREDEFINED_ROLE_SET.contains("DEPLOYER"));
         assertTrue(PREDEFINED_ROLE_SET.contains("ADVISOR"));
         assertTrue(PREDEFINED_ROLE_SET.contains("OBSERVER"));
-        assertTrue(PREDEFINED_ROLE_SET.contains("READ_ONLY"));
+        assertTrue(PREDEFINED_ROLE_SET.contains("OPERATIONAL_OBSERVER"));
         assertTrue(PREDEFINED_ROLE_SET.contains("SHARED_ADVISOR"));
         assertTrue(PREDEFINED_ROLE_SET.contains("SHARED_OBSERVER"));
         assertEquals(9, PREDEFINED_ROLE_SET.size());
@@ -48,7 +48,7 @@ public class SecurityConstantTest {
         assertEquals("DEPLOYER", DEPLOYER);
         assertEquals("ADVISOR", ADVISOR);
         assertEquals("OBSERVER", OBSERVER);
-        assertEquals("READ_ONLY", READ_ONLY);
+        assertEquals("OPERATIONAL_OBSERVER", OPERATIONAL_OBSERVER);
         assertEquals("SHARED_ADVISOR", SHARED_ADVISOR);
         assertEquals("SHARED_OBSERVER", SHARED_OBSERVER);
     }
