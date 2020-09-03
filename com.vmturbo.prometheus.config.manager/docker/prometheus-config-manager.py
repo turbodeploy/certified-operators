aa8c7f6a41c01eb76fbf0478083a46769df99202
Vaptistis Anogeianakis <vaptistis.anogeianakis@turbonomic.com>
2020-09-03 21:07:07 +0300

    [OM-61086] Converted Prometheus config manager script into a sidecar.
    It used to be a cron job, which was a better fit for some action that
    just needs to happen periodically, but existing instances don't have the
    necessary permissions given to `t8c-operator` to create cron jobs.
    (Interestingly enough, it can create jobs. Something that is rarely done
    directly)
    
    The reason I didn't catch this earlier was that I had run a script from
    product trust during the initial setup of the instance to turn on
    Prometheus and Grafana. It turns out although it was claimed the ability
    to enable Prometheus was there, the necessary permissions were
    not and the PT script was giving permission to do anything to operator
    in an attempt to work around that.
    
    - Removed cron job and persistent volume claim templates.
    - Added a new sidecar container configuration to Prometheus server pod.
    - Changed `prometheus-merged-config` volume from persistent volume claim
      to `emptyDir`. No persistence is needed since this file is recreated
      every minute from scratch. The old contents are used if they exist but
      aren't expected to be there.
    - Made the period between runs an argument to the script and wrapped the
      contents into an infinite loop. If the script fails with an error,
      Kubernetes should be able to restart the container.
    - Removed a comment line that was no longer true.
    
    Currently the latest version of the Docker image packaging the script is
    specified. That's because I haven't figured out how to get access to the
    XL version specified in cr.yaml from this `values.yaml` file yet.
    
    Built and deployed operator and prometheus-config-manager on a recent
    instance setup by Guy and verified that:
    - There are no errors in operator logs about creating a cronjob. Before
      this change, I was getting such errors on this instance.
      I did see some errors about using `charts.helm.k8s.io/v1` instead of
      `charts.helm.k8s.io/v1alpha1` in `xl-release`, but these seemed more
      likely to be caused by the version used in
      `build/kubernetes/operator/deploy/crds/charts_v1alpha1_xl_cr-64gb-openjdk.yaml`
      and
      `build/kubernetes/operator/deploy/crds/charts_v1alpha1_xl_cr-turbo-with-turbo.yaml`
      than by my changes. They went away once I applied `charts_v1alpha1_xl_cr.yaml`.
    - The persistent volume claim for configuration manager isn't created
      any more.
    - I could turn telemetry on and off from the UI twice and the DataCloud
      gateway would start and stop sending data as expected.
    - When an unhandled exception terminates the script, Kubernetes restarts
      the container as expected.
    - The `configuration-manager` container isn't restarted by Kubernetes,
      nor does it exit spontaneously when no errors occur, or when it
      encounters a situation it can mitigate (e.g. When group and/or auth
      components are down. Warnings were printed to the logs in these cases).
    - The timestamps in the messages in the logs about the script finishing
      its work are about 60s apart as desired. Sometimes they were a few
      more seconds apart but exact timing isn't required.
    - When enough arguments aren't supplied to the script it prints a
      message and terminates with an error code. Kubernetes restarts it and
      the message is the desired one.

--- com.vmturbo.prometheus.config.manager/docker/prometheus-config-manager.py
+++ com.vmturbo.prometheus.config.manager/docker/prometheus-config-manager.py
@@ -1,7 +1,5 @@
 #! /usr/bin/python3
 
-# Needs to take less than 1 minute to run otherwise it will be terminated externally!
-
 # This script will try hard to create some configuration for Prometheus to read, as Prometheus won't
 # startup without configuration. If group or auth component isn't accessible, the last known values
 # will be used or some conservative defaults if there are none.
@@ -12,7 +10,15 @@
 from datetime import datetime
 import re
 import requests
+import sys
+import time
 import yaml
+
+if len(sys.argv) < 2:
+    print(datetime.now(), "ERROR: Insufficient arguments! Usage: ", sys.argv[0],
+          " <period-in-seconds>")
+    exit(1)
+period_between_runs = float(sys.argv[1])
 
 input_config_path = "/etc/config/prometheus.yml"
 output_config_path = "/etc/merged-config/prometheus.yml"
@@ -23,78 +29,84 @@ telemetry_labels_url = "http://auth:8080/LicenseManagerService/getLicenses"
 # URL as an argument to this script and container.
 prometheus_reload_url = "http://prometheus-server:9090/-/reload"
 
-# Read Prometheus configuration coming from Kubernetes through a config map.
-# If it's not there or it's malformed, there is no point in copying it to destination and an
-# unhandled exception is fine.
-prometheus_new_config = yaml.safe_load(open(input_config_path, "r"))
+while True:
+    # Read Prometheus configuration coming from Kubernetes through a config map.
+    # If it's not there or it's malformed, there is no point in copying it to destination and an
+    # unhandled exception is fine.
+    prometheus_new_config = yaml.safe_load(open(input_config_path, "r"))
 
-# Attempt to read merged Prometheus configuration from a previous run. If it doesn't exist use
-# default values.
-try:
-    prometheus_old_config = yaml.safe_load(open(output_config_path, "r"))
-    old_remote_writers = prometheus_old_config['remote_write']
-    customer_domain = prometheus_old_config['global']['external_labels']['customer_domain']
-    customer_id = prometheus_old_config['global']['external_labels']['customer_id']
-except (yaml.YAMLError, OSError, KeyError) as error:
-    print(datetime.now(), "WARNING: Can't get old values. Using defaults. Cause:", error)
-    old_remote_writers = []
-    customer_domain = "unlicensed"
-    customer_id = "000000"
-
-try:
+    # Attempt to read merged Prometheus configuration from a previous run. If it doesn't exist use
+    # default values.
     try:
-        # Ask group component whether a user has enabled telemetry.
-        response = requests.post(url=telemetry_enabled_url,
-                                 headers={'Content-type': 'application/json;charset=UTF-8'},
-                                 json={'settingSpecName': ['telemetryEnabled']})
-
-        # We want to handle unsuccessful status codes the same way as JSON parse errors and
-        # connection errors.
-        response.raise_for_status()
-
-        # Modify configuration based on the response from group. If telemetry isn't enabled, disable
-        # remote writer sending data to DataCloud.
-        if not response.json()['response'][0]['booleanSettingValue']['value']:
-            prometheus_new_config['remote_write'] = []
-    except (requests.RequestException, OSError, ValueError) as error:
-        print(datetime.now(),
-              "WARNING: Can't get setting from group. Using old value. Cause:", error)
-        prometheus_new_config['remote_write'] = old_remote_writers
+        prometheus_old_config = yaml.safe_load(open(output_config_path, "r"))
+        old_remote_writers = prometheus_old_config['remote_write']
+        customer_domain = prometheus_old_config['global']['external_labels']['customer_domain']
+        customer_id = prometheus_old_config['global']['external_labels']['customer_id']
+    except (yaml.YAMLError, OSError, KeyError) as error:
+        print(datetime.now(), "WARNING: Can't get old values. Using defaults. Cause:", error)
+        old_remote_writers = []
+        customer_domain = "unlicensed"
+        customer_id = "000000"
 
     try:
-        # Get all the licenses from the auth component. We'll extract customer domain and
-        # SalesForce ID from them.
-        response = requests.post(url=telemetry_labels_url,
-                                 headers={'Content-type': 'application/json;charset=UTF-8',
-                                          'Accept': 'application/json;charset=UTF-8'},
-                                 json={})
+        try:
+            # Ask group component whether a user has enabled telemetry.
+            response = requests.post(url=telemetry_enabled_url,
+                                     headers={'Content-type': 'application/json;charset=UTF-8'},
+                                     json={'settingSpecName': ['telemetryEnabled']})
 
-        # We want to handle unsuccessful status codes the same way as JSON parse errors and
-        # connection errors.
-        response.raise_for_status()
+            # We want to handle unsuccessful status codes the same way as JSON parse errors and
+            # connection errors.
+            response.raise_for_status()
 
-        # Modify configuration to include the proper customer domain and ID.
-        # The code to get customer domain tries to mimic code to do the same thing in
-        # com.vmturbo.clustermgr.DiagEnvironmentSummary.computeLicenseDomain
-        customer_domain = '_'.join(sorted(
-            {
-                # Remove '@' and anything before it if it exists from e-mail address to get domain.
-                re.sub(r'^[^@]*@', '', license_dto['email'].strip())  # also trim whitespace.
-                for license_dto in response.json()['response']['licenseDTO']
-                # If the licence hasn't expired and has an e-mail
-                if datetime.strptime(license_dto['expirationDate'], '%Y-%m-%d') < datetime.today()
-                and 'email' in license_dto
-            }))
-    except (requests.RequestException, OSError, ValueError) as error:
-        print(datetime.now(),
-              "WARNING: Can't get licence information from auth. Using old values. Cause:", error)
+            # Modify configuration based on the response from group. If telemetry isn't enabled,
+            # disable remote writer sending data to DataCloud.
+            if not response.json()['response'][0]['booleanSettingValue']['value']:
+                prometheus_new_config['remote_write'] = []
+        except (requests.RequestException, OSError, ValueError) as error:
+            print(datetime.now(),
+                  "WARNING: Can't get setting from group. Using old value. Cause:", error)
+            prometheus_new_config['remote_write'] = old_remote_writers
+
+        try:
+            # Get all the licenses from the auth component. We'll extract customer domain and
+            # SalesForce ID from them.
+            response = requests.post(url=telemetry_labels_url,
+                                     headers={'Content-type': 'application/json;charset=UTF-8',
+                                              'Accept': 'application/json;charset=UTF-8'},
+                                     json={})
+
+            # We want to handle unsuccessful status codes the same way as JSON parse errors and
+            # connection errors.
+            response.raise_for_status()
+
+            # Modify configuration to include the proper customer domain and ID.
+            # The code to get customer domain tries to mimic code to do the same thing in
+            # com.vmturbo.clustermgr.DiagEnvironmentSummary.computeLicenseDomain
+            customer_domain = '_'.join(sorted(
+                {
+                    # Remove '@' and anything before it if it exists from e-mail address to get
+                    # domain.
+                    re.sub(r'^[^@]*@', '', license_dto['email'].strip())  # also trim whitespace.
+                    for license_dto in response.json()['response']['licenseDTO']
+                    # If the licence hasn't expired and has an e-mail
+                    if datetime.strptime(license_dto['expirationDate'], '%Y-%m-%d') < datetime.today()
+                    and 'email' in license_dto
+                }))
+        except (requests.RequestException, OSError, ValueError) as error:
+            print(datetime.now(),
+                  "WARNING: Can't get licence information from auth. Using old values. Cause:",
+                  error)
+        finally:
+            prometheus_new_config['global']['external_labels']['customer_domain'] = customer_domain
+            prometheus_new_config['global']['external_labels']['customer_id'] = customer_id
     finally:
-        prometheus_new_config['global']['external_labels']['customer_domain'] = customer_domain
-        prometheus_new_config['global']['external_labels']['customer_id'] = customer_id
-finally:
-    # Write the modified configuration where Prometheus can pick it up.
-    yaml.safe_dump(prometheus_new_config, open(output_config_path, "w"))
+        # Write the modified configuration where Prometheus can pick it up.
+        yaml.safe_dump(prometheus_new_config, open(output_config_path, "w"))
 
-    # Instruct Prometheus server to reload its configuration.
-    requests.post(prometheus_reload_url, headers={'Content-type': 'application/json;charset=UTF-8'})
-    print(datetime.now(), "INFO: Prometheus configuration updated successfully!")
+        # Instruct Prometheus server to reload its configuration.
+        requests.post(prometheus_reload_url,
+                      headers={'Content-type': 'application/json;charset=UTF-8'})
+        print(datetime.now(), "INFO: Prometheus configuration updated successfully!")
+
+    time.sleep(period_between_runs)
