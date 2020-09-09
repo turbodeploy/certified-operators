88638b78588dc8b20ddda59b1a902d2d92329ea4
marco.berlot@turbonomic.com <marco.berlot@turbonomic.com>
2020-09-09 17:23:06 -0400

    [OM-62476] Make index migration idempotent

--- com.vmturbo.history.schema/src/main/resources/db/migration/V1.48.7__fix_cpods_indexes.sql
+++ com.vmturbo.history.schema/src/main/resources/db/migration/V1.48.7__fix_cpods_indexes.sql
@@ -1,11 +1,42 @@
-ALTER TABLE cpod_stats_by_hour ADD INDEX snapshot_time_idx (snapshot_time);
-ALTER TABLE cpod_stats_by_hour ADD INDEX property_type_idx (property_type);
-ALTER TABLE cpod_stats_by_hour ADD INDEX uuid_idx (uuid);
+DELIMITER $$
 
-ALTER TABLE cpod_stats_by_day ADD INDEX snapshot_time_idx (snapshot_time);
-ALTER TABLE cpod_stats_by_day ADD INDEX property_type_idx (property_type);
-ALTER TABLE cpod_stats_by_day ADD INDEX uuid_idx (uuid);
+DROP PROCEDURE IF EXISTS `CreateIndex` $$
+CREATE PROCEDURE Createindex
+(
+    given_table    VARCHAR(64) CHARACTER SET utf8mb4,
+    given_index    VARCHAR(64) CHARACTER SET utf8mb4,
+    given_columns  VARCHAR(64) CHARACTER SET utf8mb4
+)
+BEGIN
 
-ALTER TABLE cpod_stats_by_month ADD INDEX snapshot_time_idx (snapshot_time);
-ALTER TABLE cpod_stats_by_month ADD INDEX property_type_idx (property_type);
-ALTER TABLE cpod_stats_by_month ADD INDEX uuid_idx (uuid);
+    DECLARE IndexIsThere INTEGER;
+
+    SELECT COUNT(1) INTO IndexIsThere
+    FROM INFORMATION_SCHEMA.STATISTICS
+    WHERE table_name   = given_table
+    AND   index_name   = given_index;
+
+    IF IndexIsThere = 0 THEN
+        SET @sqlstmt = CONCAT('CREATE INDEX ',given_index,' ON ',given_table,' (',given_columns,')');
+        PREPARE st FROM @sqlstmt;
+        EXECUTE st;
+DEALLOCATE PREPARE st;
+ELSE
+SELECT CONCAT('Index ',given_index,' already exists on Table ',given_table) CreateindexErrorMessage;
+END IF;
+
+END $$
+
+DELIMITER ;
+
+call createindex('cpod_stats_by_hour','snapshot_time_idx','snapshot_time');
+call createindex('cpod_stats_by_hour','property_type_idx','property_type');
+call createindex('cpod_stats_by_hour','uuid_idx','uuid');
+
+call createindex('cpod_stats_by_day','snapshot_time_idx','snapshot_time');
+call createindex('cpod_stats_by_day','property_type_idx','property_type');
+call createindex('cpod_stats_by_day','uuid_idx','uuid');
+
+call createindex('cpod_stats_by_month','snapshot_time_idx','snapshot_time');
+call createindex('cpod_stats_by_month','property_type_idx','property_type');
+call createindex('cpod_stats_by_month','uuid_idx','uuid');
