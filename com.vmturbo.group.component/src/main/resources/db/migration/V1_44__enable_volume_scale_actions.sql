bd3f30bae9b4f2ce6888284d20c3a7edb29b5b11
Professor Git <builder@turbonomic.com>
2020-11-24 07:48:22 -0500

    Reverted from xl-staging-ci-build-1519 and reopened jira issues (OM-64036 ) and reverted commits(97acf705f3802ac696a340207f47cb8bf003f7bd c22943ed2fd8496276aef8aa1781d580bb9a1399 )

--- com.vmturbo.group.component/src/main/resources/db/migration/V1_44__enable_volume_scale_actions.sql
+++
@@ -1,7 +0,0 @@
--- Volume Scale actions were disabled by default for private preview.
--- This migration changes the default setting value to "true" to enable them.
-UPDATE setting_policy_setting
-SET setting_value = 'true'
-WHERE setting_name = 'enableScaleActions' AND policy_id = (
-    SELECT id FROM setting_policy
-    WHERE entity_type = 60 AND policy_type = 'default' );
