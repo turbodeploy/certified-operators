3670716a9695822f8dd65b85baab6c61a6b61792
mahdi_bashari <mahdi.bashari@turbonomic.com>
2020-12-09 12:31:18 -0500

    [OM-64807]: (RB 42776) Update the migration script for adding action hash to handle duplicate rows
    Intent
    ======
    Rewrite the migration script which replaces all action details with the hash of them to handle duplicate rows gracefully
    
    Problem
    =======
    In https://rbcommons.com/s/VMTurbo/r/42594/, I introduced a migration script which replaces all the fields that keeps properties of action that makes it unique with a single field keeping the hash of it. The fields that I replaced with hashed version of them had a unique index. Therefore, I was expecting none of them have duplicate data. However, one of those fields accepted null and it seems Mariadb allows rows with duplicate data if one of the fields is NULL. Therefore, we faced migration failure on an instance that had that type of actions with null data.
    
    Solution
    ========
    - Since this code is not shipped to customers, I am going to update the scripts so it can handle duplicate rows and send an email with instructions how to fix failed migrations to Developers and PT.
    
    - The solution should be removing the duplicate entries as the duplicate entries should not be there in the first place. This should not impact anyone as the oids for start actions (which were in question here) were already changing over time. There was two alternatives:
    -- removing duplicates from source table, I tried different approaches this way and it seems to be inefficient.
    -- create a new table insert entries into it ignore duplicates and replacing the original table with this one. This one seems to be very efficient.

--- com.vmturbo.action.orchestrator/src/main/resources/db/migration/V1_19__add_action_hash.sql
+++ com.vmturbo.action.orchestrator/src/main/resources/db/migration/V1_19__add_action_hash.sql
@@ -1,50 +1,20 @@
+-- drop migrated table
+DROP TABLE IF EXISTS recommendation_identity_hash;
 
--- Remove the procedure if it already exists in the case the migration did not finish successfully last time
-DROP PROCEDURE IF EXISTS ADD_HASH_COLUMN_AND_REMOVE_DETAILS_IDX_RECOMMENDATION_ID;
+-- create the migrated table
+CREATE TABLE recommendation_identity_hash (
+  id BIGINT(20) NOT NULL,
+  action_hash BINARY(20) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX action_hash_uniq_key (action_hash));
 
--- Define the idemponent procedure for adding the hash column and removing the index on action details
-DELIMITER //
-CREATE PROCEDURE ADD_HASH_COLUMN_AND_REMOVE_DETAILS_IDX_RECOMMENDATION_ID()
-  BEGIN
-      DECLARE CONTINUE HANDLER FOR SQLEXCEPTION BEGIN
-          SELECT "Hash column is already in place or index already dropped in recommendation identity table " AS '';
-      END;
-
-      ALTER TABLE recommendation_identity
-      DROP INDEX action_type,
-      ADD COLUMN action_hash BINARY(20) NULL,
-      ADD UNIQUE INDEX action_hash_uniq_key (action_hash);
-
-  END//
-
-DELIMITER ;
-
--- call the procedure to to add the the hash column
-CALL ADD_HASH_COLUMN_AND_REMOVE_DETAILS_IDX_RECOMMENDATION_ID();
-
--- remove the procedure as we no longer need it
-DROP PROCEDURE IF EXISTS ADD_HASH_COLUMN_AND_REMOVE_DETAILS_IDX_RECOMMENDATION_ID;
-
--- populate the the new field with the hash of action
-UPDATE recommendation_identity
-        LEFT JOIN
-    (SELECT
-        recommendation_id,
-            GROUP_CONCAT(detail
-                ORDER BY detail ASC
-                SEPARATOR ' ') AS details
-    FROM
-        recommendation_identity_details
-    GROUP BY recommendation_id) AS concatentated_details ON recommendation_identity.id = concatentated_details.recommendation_id
-SET
-    action_hash = UNHEX(SHA1(CONCAT(action_type,
-                            ' ',
-                            target_id,
-                            ' ',
-                            IFNULL(action_details, ''),
-                            ' ',
-                            IFNULL(concatentated_details.details, ''))));
-
-
--- change the column so it cannot be null
-ALTER TABLE recommendation_identity CHANGE COLUMN action_hash action_hash BINARY(20) NOT NULL ;
+-- populate the the new table and drop the duplicates
+INSERT IGNORE INTO recommendation_identity_hash
+SELECT id, UNHEX(SHA1(CONCAT(action_type, ' ',
+                   target_id, ' ',
+                   IFNULL(action_details, ''), ' ',
+	               IFNULL(concatentated_details.details, ''))))
+  FROM recommendation_identity left join
+    (SELECT recommendation_id, GROUP_CONCAT(detail ORDER BY detail SEPARATOR ' ') AS details
+      FROM recommendation_identity_details GROUP BY recommendation_id) AS concatentated_details
+	ON recommendation_identity.id = concatentated_details.recommendation_id;
