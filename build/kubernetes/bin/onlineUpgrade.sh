d71a7da209a8b73d28f96d159bea73ef551254a8
Billy O'Connell <billy.oconnell@turbonomic.com>
2021-01-02 15:00:16 -0500

    [OM-63419]: Update online update script to provide the updated version as a parameter

--- build/kubernetes/bin/onlineUpgrade.sh
+++ build/kubernetes/bin/onlineUpgrade.sh
@@ -8,14 +8,30 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
 
+turboVersion=${1}
+
+usage()
+{
+  echo
+  echo "======"
+  echo "USAGE:"
+  echo "======"
+  echo "`basename $0` <turboVersion>"
+  echo
+  exit -1
+}
+
+# Test variables are not empty.
+[ -z "${turboVersion}" ] && usage
 
 # Upgrade non-xl application code
 if [ ! -d /mnt/iso ]
 then
   sudo mkdir /mnt/iso
+else
+  sudo rm -rf /mnt/iso/*
 fi
 pushd /mnt/iso/
-export turboVersion=$(cat version)
 sudo curl -o /mnt/iso/online-packages.tar http://download.vmturbo.com/appliance/download/updates/${turboVersion}/online-packages.tar
 sudo tar -xvf online-packages.tar && sudo mv online-packages/* .
 /mnt/iso/turboupgrade.sh
