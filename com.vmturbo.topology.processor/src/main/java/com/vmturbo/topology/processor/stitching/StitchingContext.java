74996fa147c4f24d36799f6b3cc59a123b3d56ed
efrat.hazani <efrat.hazani@turbonomic.com>
2021-01-21 09:47:25 +0200

    [OM-64520]: RB-42996: Data Attribution: add entity details support

--- com.vmturbo.topology.processor/src/main/java/com/vmturbo/topology/processor/stitching/StitchingContext.java
+++ com.vmturbo.topology.processor/src/main/java/com/vmturbo/topology/processor/stitching/StitchingContext.java
@@ -375,7 +375,8 @@ public class StitchingContext {
                         stitchingEntity -> {
                             return SdkToTopologyEntityConverter
                                             .newTopologyEntityDTO(stitchingEntity,
-                                                            resoldCommodityCache)
+                                                    resoldCommodityCache,
+                                                    entityDetailsEnabled)
                                             .setOrigin(Origin.newBuilder().setDiscoveryOrigin(
                                                             stitchingEntity.buildDiscoveryOrigin()));
                         };
