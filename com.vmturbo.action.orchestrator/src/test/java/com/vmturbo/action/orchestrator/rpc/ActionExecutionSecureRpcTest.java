5522c8d93cb308a4489f3e539681c8684e982760
alex_petrean <alex.petrean@turbonomic.com>
2021-02-05 13:45:17 -0500

    [OM-66536] : Make sure the correct action ID is used when updating the action execution states.

--- com.vmturbo.action.orchestrator/src/test/java/com/vmturbo/action/orchestrator/rpc/ActionExecutionSecureRpcTest.java
+++ com.vmturbo.action.orchestrator/src/test/java/com/vmturbo/action/orchestrator/rpc/ActionExecutionSecureRpcTest.java
@@ -151,7 +151,7 @@ public class ActionExecutionSecureRpcTest {
     private final ActionTargetSelector actionTargetSelector = mock(ActionTargetSelector.class);
     private final ProbeCapabilityCache probeCapabilityCache = mock(ProbeCapabilityCache.class);
     private final ActionStorehouse actionStorehouse = new ActionStorehouse(actionStoreFactory,
-            actionStoreLoader, Mockito.mock(ActionAutomationManager.class));
+            actionStoreLoader, Mockito.mock(ActionAutomationManager.class), false);
     private final ActionExecutionListener actionExecutionListener =
         Mockito.mock(ActionExecutionListener.class);
     private final ActionPaginatorFactory paginatorFactory = mock(ActionPaginatorFactory.class);
