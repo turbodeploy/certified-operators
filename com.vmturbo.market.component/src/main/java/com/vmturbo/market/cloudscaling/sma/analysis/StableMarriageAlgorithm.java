514127f1fae07be5d765d2760c4f37cf8188d30a
sankar_veeramoni <sankar.veeramoni@turbonomic.com>
2021-02-09 10:29:09 -0500

    SMA postprocessing: VM should not be over discounted while scaling down

--- com.vmturbo.market.component/src/main/java/com/vmturbo/market/cloudscaling/sma/analysis/StableMarriageAlgorithm.java
+++ com.vmturbo.market.component/src/main/java/com/vmturbo/market/cloudscaling/sma/analysis/StableMarriageAlgorithm.java
@@ -123,8 +123,12 @@ public class StableMarriageAlgorithm {
             Long riKeyId = riKeyIdMatchPair.getKey();
             List<SMAMatch> outgoingCouponMatches = riKeyIdMatchPair.getValue();
             for (SMAMatch outgoingCouponMatch : outgoingCouponMatches) {
-                float coupons_required = outgoingCouponMatch.getVirtualMachine()
-                        .getCurrentRICoverage()
+                // If the VM scales down then coupons required should be based on the
+                // coupons required for the scaled down template. It is legit to lose coupons
+                // if the vm scales down and is 100% covered. For eg a vm in t2.large with 10/16
+                // coverage can go to t2.medium and have 8/8 coverage.
+                float coupons_required = Math.min(outgoingCouponMatch.getVirtualMachine()
+                        .getCurrentRICoverage(), outgoingCouponMatch.getTemplate().getCoupons())
                         - outgoingCouponMatch.getDiscountedCoupons();
                 for (SMAMatch incomingCouponMatch : matchesWithIncomingCoupons.get(riKeyId)) {
                     float coupons_grabbed;
@@ -154,8 +158,11 @@ public class StableMarriageAlgorithm {
                         break;
                     }
                 }
-                outgoingCouponMatch.setDiscountedCoupons(outgoingCouponMatch
-                        .getVirtualMachine().getCurrentRICoverage() - coupons_required);
+                // adjust the outgoingCouponMatch based on the coupons_required. Ideally coupons_required
+                // should be 0 and the VM should retain all the coupons it already had. If not it will
+                // retain coupons to be 100% covered if it had scaled down.
+                outgoingCouponMatch.setDiscountedCoupons(Math.min(outgoingCouponMatch.getVirtualMachine()
+                        .getCurrentRICoverage(), outgoingCouponMatch.getTemplate().getCoupons()) - coupons_required);
                 outgoingCouponMatch.setReservedInstance(outgoingCouponMatch.getVirtualMachine()
                         .getCurrentRI());
             }
