007c1f4117c781a7776cbcc07f43089cdae82ae3
Professor Git <builder@turbonomic.com>
2021-02-19 13:23:22 -0500

    Reverted from xl-staging-opsmgr-ci-build-1329 and reopened jira issues (OM-63929 ) and reverted commits(3b393dd76c91a36a783e79a8835ce40eaf07c6c5 f789cd019f7eefba23bf854313ae81e84fd37a0d 5091ea2483bd622af442753de19e70f6c0efab9b 269d99c6a3a06a53e7e121652e21a7e7645986cc e6713f4c400e0e72872fd8fb08ac6e886d46352b 3b6c1df3c48cb5e1ed8ca2bbda9a5d380ab6cd58 )

--- com.vmturbo.analysis/platform.analysis/src/main/java/com/vmturbo/platform/analysis/utilities/CostFunctionFactory.java
+++ com.vmturbo.analysis/platform.analysis/src/main/java/com/vmturbo/platform/analysis/utilities/CostFunctionFactory.java
@@ -14,6 +14,7 @@ import javax.annotation.Nullable;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableSet;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.checkerframework.checker.javari.qual.ReadOnly;
@@ -553,9 +554,9 @@ public class CostFunctionFactory {
                 balanceAccount.getPriceId() : balanceAccount.getId();
 
         if (!costTable.hasAccountId(accountId)) {
-            logger.debug("Business account id {} is not found on seller: {}, in regionId: {}, for shopping list: {},"
-                            + " return infinity compute quote", accountId,
-                    seller.getDebugInfoNeverUseInCode(), regionIdBought, sl.getDebugInfoNeverUseInCode());
+            logger.warn("Business account id {} is not found on seller: {}, for shopping list: {}, "
+                            + "return infinity compute quote", accountId,
+                    seller.getDebugInfoNeverUseInCode(), sl.getDebugInfoNeverUseInCode());
             return new CostUnavailableQuote(seller, regionIdBought, balanceAccount.getId(),
                     balanceAccount.getPriceId());
         }
