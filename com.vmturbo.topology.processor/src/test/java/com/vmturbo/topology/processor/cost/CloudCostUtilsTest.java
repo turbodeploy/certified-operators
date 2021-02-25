5bd3c4e3f352f0f4c3d90a6ea5c2436bcfc6a420
Roop Ghosh <roop.ghosh@turbonomic.com>
2021-02-25 15:41:00 -0500

    [OM-67443] update aws DBS entities prefix from DBProfile to DBSProfile

--- com.vmturbo.topology.processor/src/test/java/com/vmturbo/topology/processor/cost/CloudCostUtilsTest.java
+++ com.vmturbo.topology.processor/src/test/java/com/vmturbo/topology/processor/cost/CloudCostUtilsTest.java
@@ -24,7 +24,7 @@ public class CloudCostUtilsTest {
     public void testDatabaseTierLocalNameToId() {
         Assert.assertEquals("No change", CloudCostUtils.getEntityIdFromDBProfile(
                 generateTopologyStitchingEntity("No change"), null));
-        Assert.assertEquals("aws::DBPROFILE::id", CloudCostUtils.getEntityIdFromDBProfile(
+        Assert.assertEquals("aws::DBSPROFILE::id", CloudCostUtils.getEntityIdFromDBProfile(
                 generateTopologyStitchingEntity("id"), SDKProbeType.AWS_COST));
     }
 
@@ -43,7 +43,7 @@ public class CloudCostUtilsTest {
      */
     @Test
     public void testAwsDatabaseServerTierLocalNameToId() {
-        Assert.assertEquals("aws::DBPROFILE::db.m2.medium::gp2", CloudCostUtils
+        Assert.assertEquals("aws::DBSPROFILE::db.m2.medium::gp2", CloudCostUtils
                 .getEntityIdFromDBProfile(generateTopologyStitchingEntity("db.m2.medium::gp2"), SDKProbeType.AWS));
     }
 
@@ -58,7 +58,7 @@ public class CloudCostUtilsTest {
                 .setDatabaseServerTierData(DatabaseServerTierData
                         .newBuilder().setTemplateIdentifier("db.t2.24xlarge::io1").build());
         TopologyStitchingEntity topologyStitchingEntity = new TopologyStitchingEntity(entityDTO, 1L, 01L, 1L);
-        Assert.assertEquals("aws::DBPROFILE::db.t2.24xlarge::io1", CloudCostUtils
+        Assert.assertEquals("aws::DBSPROFILE::db.t2.24xlarge::io1", CloudCostUtils
                 .getEntityIdFromDBProfile(topologyStitchingEntity, SDKProbeType.AWS));
     }
 
