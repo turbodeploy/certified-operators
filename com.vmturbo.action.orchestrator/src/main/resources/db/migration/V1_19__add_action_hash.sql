9bad61355f6ae189151043422c49b556c455e6ce
Andy Lowry <andy.lowry@turbonomic.com>
2021-03-12 08:29:30 -0500

    Remediate migration V1.19 so to improve its performance and to spread its overall work over several individual operations in order to reduce risk of exceeding idle-connection timeouts related to mediation through kube-proxy

--- com.vmturbo.action.orchestrator/src/main/resources/db/migration/V1_19__add_action_hash.sql
+++ com.vmturbo.action.orchestrator/src/main/resources/db/migration/V1_19__add_action_hash.sql
@@ -1,20 +1,47 @@
--- drop migrated table
-DROP TABLE IF EXISTS recommendation_identity_hash;
+-- this migratin consolidates the existing recommendation_identity and
+-- recommendation_identity_details tables into a single new recommendation_identity_hash that uses
+-- a hash computed from former action data to manage actoin identities
 
--- create the migrated table
+-- create the new table
+DROP TABLE IF EXISTS recommendation_identity_hash;
 CREATE TABLE recommendation_identity_hash (
   id BIGINT(20) NOT NULL,
   action_hash BINARY(20) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE INDEX action_hash_uniq_key (action_hash));
+  PRIMARY KEY (id));
 
--- populate the the new table and drop the duplicates
-INSERT IGNORE INTO recommendation_identity_hash
-SELECT id, UNHEX(SHA1(CONCAT(action_type, ' ',
-                   target_id, ' ',
-                   IFNULL(action_details, ''), ' ',
-	               IFNULL(concatentated_details.details, ''))))
-  FROM recommendation_identity left join
-    (SELECT recommendation_id, GROUP_CONCAT(detail ORDER BY detail SEPARATOR ' ') AS details
-      FROM recommendation_identity_details GROUP BY recommendation_id) AS concatentated_details
-	ON recommendation_identity.id = concatentated_details.recommendation_id;
+-- now compute the new data one "shard" at a time, by including a WHERE clause to restrict
+-- the action ids to be processed. This is to prevent a what was formerly a single operation
+-- from running so long that the connection exceeds kube-proxy's 15-minute idle conneciton
+-- timeout.
+DROP PROCEDURE IF EXISTS _run_shard;
+DELIMITER $$
+CREATE PROCEDURE _run_shard(shard int, shard_count int, mult int)
+BEGIN
+  INSERT IGNORE INTO recommendation_identity_hash
+    SELECT id, UNHEX(SHA1(CONCAT(
+      action_type, ' ',
+      target_id, ' ',
+      IFNULL(action_details, ''), ' ',
+      IFNULL(GROUP_CONCAT(detail ORDER BY detail SEPARATOR ' '), ''))))
+    FROM  recommendation_identity
+    LEFT JOIN recommendation_identity_details det ON id  = det.recommendation_id
+    WHERE id*mult % shard_count = shard
+    GROUP BY id ;
+END; $$
+DELIMITER ;
+
+CALL _run_shard(0, 10, 23);
+CALL _run_shard(1, 10, 23);
+CALL _run_shard(2, 10, 23);
+CALL _run_shard(3, 10, 23);
+CALL _run_shard(4, 10, 23);
+CALL _run_shard(5, 10, 23);
+CALL _run_shard(6, 10, 23);
+CALL _run_shard(7, 10, 23);
+CALL _run_shard(8, 10, 23);
+CALL _run_shard(9, 10, 23);
+
+DROP procedure _run_shard;
+
+-- finally create an index on the new hash table
+CREATE UNIQUE INDEX action_hash_uniq_key ON recommendation_identity_hash(action_hash);
