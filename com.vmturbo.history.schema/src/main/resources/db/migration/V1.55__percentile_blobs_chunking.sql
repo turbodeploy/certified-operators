5deda4c0f6009f765e0e658758ecc7881a4e6209
Alexander Vasin <alexandr.vasin@turbonomic.com>
2021-03-18 13:14:10 +0300

    [OM-68438]: Fixed tests after 1.55 database migration.

--- com.vmturbo.history.schema/src/main/resources/db/migration/V1.55__percentile_blobs_chunking.sql
+++ com.vmturbo.history.schema/src/main/resources/db/migration/V1.55__percentile_blobs_chunking.sql
@@ -4,7 +4,7 @@ DROP PROCEDURE IF EXISTS _percentile_blobs_add_chunk_index;
 DELIMITER //
 CREATE PROCEDURE _percentile_blobs_add_chunk_index()
 BEGIN
-  IF (SELECT NOT EXISTS(SELECT * FROM information_schema.columns WHERE table_schema = 'vmtdb' AND table_name = 'percentile_blobs' AND column_name = 'chunk_index'))
+  IF (SELECT NOT EXISTS(SELECT * FROM information_schema.columns WHERE table_schema = (SELECT DATABASE()) AND table_name = 'percentile_blobs' AND column_name = 'chunk_index'))
   THEN
     ALTER TABLE `percentile_blobs` ADD `chunk_index` int not null default '0';
   END IF;
