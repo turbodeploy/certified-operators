3fc97ea3cf148f65fc95db91c55f096cb069b5fc
Alexander Vasin <alexandr.vasin@turbonomic.com>
2021-04-02 13:28:04 +0300

    [OM-68840]: Fixed network filter. Removed clearing of aggregated by connection relationships in ConnectedNetworkPreStitchingOperation.

--- com.vmturbo.topology.processor/src/test/java/com/vmturbo/topology/processor/stitching/prestitching/ConnectedNetworkPreStitchingOperationTest.java
+++ com.vmturbo.topology.processor/src/test/java/com/vmturbo/topology/processor/stitching/prestitching/ConnectedNetworkPreStitchingOperationTest.java
@@ -8,9 +8,12 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import javax.annotation.Nonnull;
+
 import com.google.common.collect.ImmutableSet;
 
 import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -38,6 +41,22 @@ import com.vmturbo.topology.processor.targets.TargetStore;
  */
 public class ConnectedNetworkPreStitchingOperationTest {
     private static final String STORAGE_ID = "storageId";
+    private static final String NETWORK1_ID = "network1Id";
+    private static final String NETWORK2_ID = "network2Id";
+    private static final String NETWORK1_DISPLAY_NAME = "network1DisplayName";
+    private static final String VM1_ID = "vm1Id";
+    private static final String NETWORK2_DISPLAY_NAME = "network2DisplayName";
+    private static final String NETWORK3_DISPLAY_NAME = "network3DisplayName";
+    private static final int TARGET1_ID = 1;
+    private static final int TARGET2_ID = 2;
+    private static final int VM1_OID = 1;
+    private static final int VM2_OID = 2;
+    private static final int VM3_OID = 5;
+    private static final int VM4_OID = 6;
+    private static final int NETWORK1_OID = 3;
+    private static final int NETWORK2_OID = 4;
+    private static final int NETWORK3_OID = 7;
+    private static final String VM2_ID = "vm2Id";
 
     /**
      * Checks that connected relationships created from layered over relationships between VM and
@@ -54,25 +73,45 @@ public class ConnectedNetworkPreStitchingOperationTest {
         final IdentityProvider identityProvider = Mockito.mock(IdentityProvider.class);
         stitchingContextBuilder.setIdentityProvider(identityProvider);
         // Prepare entities
-        final EntityDTO.Builder vmBuilder =
-                        EntityDTO.newBuilder().setEntityType(EntityType.VIRTUAL_MACHINE)
-                                        .setId("vmId");
-        final EntityDTO.Builder networkBuilder =
-                        EntityDTO.newBuilder().setEntityType(EntityType.NETWORK).setId("networkId");
-        vmBuilder.addLayeredOver(networkBuilder.getId());
-        vmBuilder.addLayeredOver(STORAGE_ID);
-        final TopologyStitchingEntity vmStitchingEntity = new TopologyStitchingEntity(
-                        StitchingEntityData.newBuilder(vmBuilder).build());
-        final TopologyStitchingEntity networkStitchingEntity = new TopologyStitchingEntity(
-                        StitchingEntityData.newBuilder(networkBuilder).build());
-        networkStitchingEntity.addConnectedFrom(ConnectionType.AGGREGATED_BY_CONNECTION,
-                        vmStitchingEntity);
-        vmStitchingEntity.addConnectedTo(ConnectionType.AGGREGATED_BY_CONNECTION,
-                        networkStitchingEntity);
+        final EntityDTO.Builder vm1Builder = createBuilder(EntityType.VIRTUAL_MACHINE, VM1_ID);
+        final EntityDTO.Builder vm2Builder = createBuilder(EntityType.VIRTUAL_MACHINE, VM2_ID);
+        final EntityDTO.Builder vm3Builder = createBuilder(EntityType.VIRTUAL_MACHINE, VM1_ID);
+        final EntityDTO.Builder vm4Builder = createBuilder(EntityType.VIRTUAL_MACHINE, VM2_ID);
+        final EntityDTO.Builder network1Builder = createBuilder(EntityType.NETWORK, NETWORK1_ID)
+                        .setDisplayName(NETWORK1_DISPLAY_NAME);
+        final EntityDTO.Builder network2Builder = createBuilder(EntityType.NETWORK, NETWORK2_ID)
+                        .setDisplayName(NETWORK2_DISPLAY_NAME);
+        final EntityDTO.Builder network3Builder = createBuilder(EntityType.NETWORK, NETWORK1_ID)
+                        .setDisplayName(NETWORK3_DISPLAY_NAME);
+        vm1Builder.addLayeredOver(network1Builder.getId());
+        vm1Builder.addLayeredOver(network2Builder.getId());
+        vm1Builder.addLayeredOver(STORAGE_ID);
+        vm2Builder.addLayeredOver(network1Builder.getId());
+        vm3Builder.addLayeredOver(network3Builder.getId());
+        vm4Builder.addLayeredOver(network3Builder.getId());
+        final TopologyStitchingEntity vm1Se =
+                        createStitchingEntity(vm1Builder, VM1_OID, TARGET1_ID);
+        final TopologyStitchingEntity vm2Se =
+                        createStitchingEntity(vm2Builder, VM2_OID, TARGET1_ID);
+        final TopologyStitchingEntity vm3Se =
+                        createStitchingEntity(vm3Builder, VM3_OID, TARGET2_ID);
+        final TopologyStitchingEntity vm4Se =
+                        createStitchingEntity(vm4Builder, VM4_OID, TARGET2_ID);
+        final TopologyStitchingEntity network1Se =
+                        createStitchingEntity(network1Builder, NETWORK1_OID, TARGET1_ID);
+        final TopologyStitchingEntity network2Se =
+                        createStitchingEntity(network2Builder, NETWORK2_OID, TARGET1_ID);
+        final TopologyStitchingEntity network3Se =
+                        createStitchingEntity(network3Builder, NETWORK3_OID, TARGET2_ID);
         final Collection<StitchingEntity> stitchingEntities =
-                        ImmutableSet.of(vmStitchingEntity, networkStitchingEntity);
+                        ImmutableSet.of(vm1Se, vm2Se, vm3Se, vm4Se, network1Se, network2Se,
+                                        network3Se);
         final StitchingResultBuilder resultBuilder =
                         new StitchingResultBuilder(stitchingContextBuilder.build());
+        network1Se.addConnectedFrom(ConnectionType.AGGREGATED_BY_CONNECTION, vm1Se);
+        network1Se.addConnectedFrom(ConnectionType.AGGREGATED_BY_CONNECTION, vm2Se);
+        vm1Se.addConnectedTo(ConnectionType.AGGREGATED_BY_CONNECTION, network1Se);
+        vm1Se.addConnectedTo(ConnectionType.AGGREGATED_BY_CONNECTION, network2Se);
         // Execute operation
         operation.performOperation(stitchingEntities.stream(), resultBuilder);
         final TopologicalChangelog<StitchingEntity> changelog = resultBuilder.build();
@@ -80,16 +119,37 @@ public class ConnectedNetworkPreStitchingOperationTest {
         final IStitchingJournal<StitchingEntity> stitchingJournal = new StitchingJournal<>();
         changes.forEach(change -> change.applyChange(stitchingJournal));
         // Check results
-        Assert.assertThat(vmStitchingEntity.getEntityBuilder().getLayeredOverList(),
+        Assert.assertThat(vm1Se.getEntityBuilder().getLayeredOverList(),
                         CoreMatchers.is(Collections.singletonList(STORAGE_ID)));
-        Assert.assertThat(vmStitchingEntity.getConnectedToByType()
+        Assert.assertThat(
+                        vm1Se.getEntityBuilder().getVirtualMachineData().getConnectedNetworkList(),
+                        Matchers.containsInAnyOrder(NETWORK1_DISPLAY_NAME, NETWORK2_DISPLAY_NAME));
+        // ConnectionType.AGGREGATED_BY_CONNECTION relationships left untouched
+        Assert.assertThat(vm1Se.getConnectedToByType()
                                         .getOrDefault(ConnectionType.AGGREGATED_BY_CONNECTION,
                                                         Collections.emptySet()),
-                        CoreMatchers.is(Collections.emptySet()));
-        Assert.assertThat(networkStitchingEntity.getConnectedFromByType()
+                        Matchers.containsInAnyOrder(network1Se, network2Se));
+        Assert.assertThat(network1Se.getConnectedFromByType()
                                         .getOrDefault(ConnectionType.AGGREGATED_BY_CONNECTION,
                                                         Collections.emptySet()),
-                        CoreMatchers.is(Collections.emptySet()));
+                        Matchers.containsInAnyOrder(vm2Se, vm1Se));
+        // No intersections between different targets
+        Assert.assertThat(
+                        vm3Se.getEntityBuilder().getVirtualMachineData().getConnectedNetworkList(),
+                        Matchers.containsInAnyOrder(NETWORK3_DISPLAY_NAME));
+    }
+
+    @Nonnull
+    private static TopologyStitchingEntity createStitchingEntity(EntityDTO.Builder builder, int oid,
+                    int targetId) {
+        return new TopologyStitchingEntity(
+                        StitchingEntityData.newBuilder(builder).oid(oid).targetId(targetId)
+                                        .build());
+    }
+
+    @Nonnull
+    private static EntityDTO.Builder createBuilder(EntityType type, String id) {
+        return EntityDTO.newBuilder().setEntityType(type).setId(id);
     }
 
 }
