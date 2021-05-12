9481a0d2cc0c7a78820c2e1e6d61e406ab46e30e
Professor Git <builder@turbonomic.com>
2021-05-12 16:08:31 -0400

    Reverted from xl-staging-ci-build-4 and reopened jira issues (OM-64189 OM-66869 OM-69523 OM-70325 ) and reverted commits(6fdcb48424e74bef9382ab6346e68d2c1c518cfe a8a3a1c3eaef5b4eb8b2ac592e4444540adbe990 7c78df297f6b1aa005a4924b4c1c0a77d4758106 20b11f63bad7d0abe0200bad44616624dfd2e6c2 9998f2d14c55c4b801c24e73447b5d5c4e2590b9 fc77fab720e972ebfc14e85cec360fcaf21b517f b5e8ca441320a24a853d58419e813b69d6cb8169 28921af6c43bee08f406e617e2769661a529f249 )

--- com.vmturbo.market.component/src/main/java/com/vmturbo/market/topology/conversions/SMAConverter.java
+++ com.vmturbo.market.component/src/main/java/com/vmturbo/market/topology/conversions/SMAConverter.java
@@ -229,13 +229,6 @@ public class SMAConverter {
                 ShoppingListTO sl =
                         projectedVmTraderTO.getShoppingListsList()
                                 .get(indexOfComputeShoppingList);
-
-                // If the shopping list is not part of CloudVmComputeShoppingListIDs because
-                // the vm is uncontrollable dont generate any action for it.
-                if (!converter.getCloudVmComputeShoppingListIDs().contains(sl.getOid())) {
-                    continue;
-                }
-
                 MinimalOriginalTrader sourceTraderTO = originalTraderTOMap.get(sourceOnDemandMarketTierOid);
 
                 // Compare the comm sold by the current supplier and comm bought by the VM and
