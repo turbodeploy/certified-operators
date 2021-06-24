c82422bab94a70528069a766496ae283c306f5d2
Andrei Astra <andrei.astrakharchik@turbonomic.com>
2021-06-23 23:52:03 -0400

    [OM-71467]: Introduce action delay to ConditionaLSubmitter

--- com.vmturbo.action.orchestrator/src/test/java/com/vmturbo/action/orchestrator/execution/ConditionalSubmitterTest.java
+++ com.vmturbo.action.orchestrator/src/test/java/com/vmturbo/action/orchestrator/execution/ConditionalSubmitterTest.java
@@ -1,5 +1,6 @@
 package com.vmturbo.action.orchestrator.execution;
 
+import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -44,68 +45,73 @@ public class ConditionalSubmitterTest {
      * @throws Exception any test exception
      */
     @Test
-    public void testExecutionOrder() throws Exception {
+    public void testExecutionOrder() {
         int tasksCount = 100;
         int conditionsCount = 3;
         CountDownLatch countDownLatch = new CountDownLatch(tasksCount * conditionsCount);
-        ConditionalSubmitter executor = new CountDownSubmitter(4, countDownLatch);
+        try (ConditionalSubmitter executor = new CountDownSubmitter(4, countDownLatch)) {
+            List<ConditionalFuture> futures = new ArrayList<>();
 
-        List<ConditionalFuture> futures = new ArrayList<>();
-
-        for (int i = 0; i < tasksCount; i++) {
-            for (int conditionId = 1; conditionId <= conditionsCount; conditionId++) {
-                ConditionalFuture future = new ConditionalFuture(
-                        new TimingTask(conditionId * 100 + i, conditionId));
-                executor.execute(future);
-                futures.add(future);
-            }
-        }
-
-        // Wait for all tasks to be executed
-        countDownLatch.await();
-
-        for (ConditionalFuture future1 : futures) {
-            for (ConditionalFuture future2 : futures) {
-                TimingTask task1 = ((TimingTask)future1.getOriginalTask());
-                TimingTask task2 = ((TimingTask)future2.getOriginalTask());
-
-                if (task1.getConditionId() != task2.getConditionId()) {
-                    continue;
-                }
-
-                if (task1.getTaskId() == task2.getTaskId()) {
-                    continue;
-                }
-
-                if (task1.getTaskId() > task2.getTaskId()) {
-                    Assert.assertTrue(task1.getStartTime().compareTo(task2.getEndTime()) >= 0);
-                }
-
-                if (task2.getTaskId() > task1.getTaskId()) {
-                    Assert.assertTrue(task2.getStartTime().compareTo(task1.getEndTime()) >= 0);
+            for (int i = 0; i < tasksCount; i++) {
+                for (int conditionId = 1; conditionId <= conditionsCount; conditionId++) {
+                    ConditionalFuture future = new ConditionalFuture(
+                            new TimingTask(conditionId * 100 + i, conditionId));
+                    executor.execute(future);
+                    futures.add(future);
                 }
             }
+
+            // Wait for all tasks to be executed
+            countDownLatch.await();
+
+            for (ConditionalFuture future1 : futures) {
+                for (ConditionalFuture future2 : futures) {
+                    TimingTask task1 = ((TimingTask)future1.getOriginalTask());
+                    TimingTask task2 = ((TimingTask)future2.getOriginalTask());
+
+                    if (task1.getConditionId() != task2.getConditionId()) {
+                        continue;
+                    }
+
+                    if (task1.getTaskId() == task2.getTaskId()) {
+                        continue;
+                    }
+
+                    if (task1.getTaskId() > task2.getTaskId()) {
+                        Assert.assertTrue(task1.getStartTime().compareTo(task2.getEndTime()) >= 0);
+                    }
+
+                    if (task2.getTaskId() > task1.getTaskId()) {
+                        Assert.assertTrue(task2.getStartTime().compareTo(task1.getEndTime()) >= 0);
+                    }
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            logger.error("Failed test", e);
         }
     }
 
     /**
      * Test exceptions in tasks.
-     * @throws Exception any test exception
+     *
+     * @throws ExecutionException expected exception
      */
     @Test
-    public void testExceptions() throws Exception {
+    public void testExceptions() throws ExecutionException {
         CountDownLatch countDownLatch = new CountDownLatch(1);
-        ConditionalSubmitter executor = new CountDownSubmitter(2, countDownLatch);
+        try (ConditionalSubmitter executor = new CountDownSubmitter(2, countDownLatch)) {
+            ConditionalFuture future = new ConditionalFuture(new ExceptionTask(1, 1));
+            executor.execute(future);
+            countDownLatch.await();
 
-        ConditionalFuture future = new ConditionalFuture(new ExceptionTask(1, 1));
-        executor.execute(future);
-        countDownLatch.await();
+            exceptionRule.expect(ExecutionException.class);
+            future.get();
 
-        exceptionRule.expect(ExecutionException.class);
-        future.get();
-
-        Assert.assertEquals(0, executor.getRunningFuturesCount());
-        Assert.assertEquals(0, executor.getQueuedFuturesCount());
+            Assert.assertEquals(0, executor.getRunningFuturesCount());
+            Assert.assertEquals(0, executor.getQueuedFuturesCount());
+        } catch (IOException | InterruptedException e) {
+            logger.error("Failed test", e);
+        }
     }
 
     /**
@@ -139,11 +145,11 @@ public class ConditionalSubmitterTest {
      * @throws Exception any test exception
      */
     @Test
-    public void testConditionalExecutorService() throws Exception {
+    public void testConditionalExecutorService() {
         Set<TimingTask> tasks = new HashSet<>();
 
         int conditionsCount = 10;
-        int tasksCount = 100;
+        int tasksCount = 10;
         int totalTaskCount = tasksCount * conditionsCount;
         CountDownLatch countDownLatch = new CountDownLatch(totalTaskCount);
 
@@ -153,42 +159,44 @@ public class ConditionalSubmitterTest {
             }
         }
 
-        ConditionalSubmitter executor = new CountDownSubmitter(5, countDownLatch);
+        try (ConditionalSubmitter executor = new CountDownSubmitter(5, countDownLatch)) {
+            List<ConditionalFuture> futures = new ArrayList<>();
 
-        List<ConditionalFuture> futures = new ArrayList<>();
-
-        for (TimingTask task : tasks) {
-            ConditionalFuture future = new ConditionalFuture(task);
-            executor.execute(future);
-            futures.add(future);
-        }
-
-        Assert.assertEquals(totalTaskCount, futures.size());
-
-        // Wait for all tasks to be executed
-        countDownLatch.await();
-
-        Assert.assertEquals(0, executor.getRunningFuturesCount());
-        Assert.assertEquals(0, executor.getQueuedFuturesCount());
-
-        // Check that the tasks with the same condition were executed one
-        // at a time
-        for (TimingTask task1 : tasks) {
-            Assert.assertEquals(1, task1.getCallCount());
-
-            for (TimingTask task2 : tasks) {
-                if (task1.getTaskId() == task2.getTaskId()) {
-                    continue;
-                }
-
-                if (task1.getConditionId() != task2.getConditionId()) {
-                    continue;
-                }
-
-                Assert.assertTrue("Task 1: " + task1 + ", Task 2: " + task2,
-                        task1.getStartTime().compareTo(task2.getEndTime()) >= 0
-                                || task2.getStartTime().compareTo(task1.getEndTime()) >= 0);
+            for (TimingTask task : tasks) {
+                ConditionalFuture future = new ConditionalFuture(task);
+                executor.execute(future);
+                futures.add(future);
             }
+
+            Assert.assertEquals(totalTaskCount, futures.size());
+
+            // Wait for all tasks to be executed
+            countDownLatch.await();
+
+            Assert.assertEquals(0, executor.getRunningFuturesCount());
+            Assert.assertEquals(0, executor.getQueuedFuturesCount());
+
+            // Check that the tasks with the same condition were executed one
+            // at a time
+            for (TimingTask task1 : tasks) {
+                Assert.assertEquals(1, task1.getCallCount());
+
+                for (TimingTask task2 : tasks) {
+                    if (task1.getTaskId() == task2.getTaskId()) {
+                        continue;
+                    }
+
+                    if (task1.getConditionId() != task2.getConditionId()) {
+                        continue;
+                    }
+
+                    Assert.assertTrue("Task 1: " + task1 + ", Task 2: " + task2,
+                            task1.getStartTime().compareTo(task2.getEndTime()) >= 0
+                                    || task2.getStartTime().compareTo(task1.getEndTime()) >= 0);
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            logger.error("Failed test", e);
         }
     }
 
@@ -211,7 +219,13 @@ public class ConditionalSubmitterTest {
         @Override
         public TimingTask call() throws Exception {
             startTime = Instant.now();
-            Thread.sleep(30);
+            try {
+                Thread.sleep(30);
+            } catch (InterruptedException e) {
+                logger.error("Interrupted " + this, e);
+                throw new InterruptedException();
+            }
+
             callCount++;
             endTime = Instant.now();
             logger.info("Executed: {}", this);
@@ -309,7 +323,8 @@ public class ConditionalSubmitterTest {
         private final CountDownLatch countDownLatch;
 
         CountDownSubmitter(int poolSize, CountDownLatch countDownLatch) {
-            super(poolSize, new ThreadFactoryBuilder().setNameFormat("auto-act-exec-%d").build());
+            super(poolSize, new ThreadFactoryBuilder().setNameFormat("auto-act-exec-%d").build(),
+                    0);
             this.countDownLatch = countDownLatch;
         }
 
