c008bbac2d3f5307ab2fcdbae0b5f6b33f742c04
Zhuoran Bao <zhuoran.bao@turbonomic.com>
2021-07-17 08:44:25 -0400

    [OM-72690] add CoreSocketRatioMode setting

--- com.vmturbo.api.component/src/test/java/com/vmturbo/api/component/SettingsMapperIntegrationTest.java
+++ com.vmturbo.api.component/src/test/java/com/vmturbo/api/component/SettingsMapperIntegrationTest.java
@@ -58,7 +58,8 @@ public class SettingsMapperIntegrationTest {
     // Set of settings that are intentionally hidden from the UI
     private final Set<String> invisibleSettings = ImmutableSet.of(
         EntitySettingSpecs.ScalingGroupMembership.getSettingName(),
-        EntitySettingSpecs.VCPURequestUtilization.getSettingName());
+        EntitySettingSpecs.VCPURequestUtilization.getSettingName(),
+        EntitySettingSpecs.CoreSocketRatioMode.getSettingName());
 
     /**
      * Initialise test configuration.
