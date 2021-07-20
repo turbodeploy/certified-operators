d5f2d92385931281aa65ec19e9e5b1c7037b2d11
Igor Golikov <igor.golikov@turbonomic.com>
2021-07-20 18:14:50 +0300

    [OM-73078]: Cloud DBS actions on/off toggle should be off by default

--- com.vmturbo.components.common/src/main/java/com/vmturbo/components/common/setting/BooleanSettingDataType.java
+++ com.vmturbo.components.common/src/main/java/com/vmturbo/components/common/setting/BooleanSettingDataType.java
@@ -1,5 +1,7 @@
 package com.vmturbo.components.common.setting;
 
+import java.util.Map;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
@@ -7,6 +9,7 @@ import javax.annotation.concurrent.Immutable;
 import com.vmturbo.common.protobuf.setting.SettingProto.BooleanSettingValueType;
 import com.vmturbo.common.protobuf.setting.SettingProto.Setting;
 import com.vmturbo.common.protobuf.setting.SettingProto.SettingSpec.Builder;
+import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.EntityType;
 
 /**
  * Data structure description for boolean settings.
@@ -20,6 +23,16 @@ public class BooleanSettingDataType extends AbstractSettingDataType<Boolean> {
      */
     public BooleanSettingDataType(boolean defaultValue) {
         super(defaultValue);
+    }
+
+    /**
+     * Constructs setting data type holding the specified default value.
+     *
+     * @param defaultValue default value to use.
+     * @param entityDefaults map of default values for different entity types
+     */
+    public BooleanSettingDataType(boolean defaultValue, @Nonnull Map<EntityType, Boolean> entityDefaults) {
+        super(defaultValue, entityDefaults);
     }
 
     @Override
