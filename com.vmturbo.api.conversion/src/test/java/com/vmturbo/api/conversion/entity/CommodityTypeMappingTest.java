e336b83a5de1620f50bb913cf5f570bc975ef571
wei_duan <wei.duan@turbonomic.com>
2021-10-21 16:37:30 -0700

    [OM-76398]: Adds sdk to api commodity type conversion for NET_THROUGHPUT_IN and NET_THROUGHPUT_OUT.

--- com.vmturbo.api.conversion/src/test/java/com/vmturbo/api/conversion/entity/CommodityTypeMappingTest.java
+++ com.vmturbo.api.conversion/src/test/java/com/vmturbo/api/conversion/entity/CommodityTypeMappingTest.java
@@ -42,6 +42,14 @@ public class CommodityTypeMappingTest {
             EntityType.CONTAINER_PLATFORM_CLUSTER_VALUE, CommodityType.VCPU_VALUE);
         assertEquals("mCores", cntClusterVCPUUnit);
 
+        String vmNetThroughputOutUnit = CommodityTypeMapping.getUnitForEntityCommodityType(
+                EntityType.VIRTUAL_MACHINE_VALUE, CommodityType.NET_THROUGHPUT_OUT_VALUE);
+        assertEquals("KByte/sec", vmNetThroughputOutUnit);
+
+        String vmNetThroughputInUnit = CommodityTypeMapping.getUnitForEntityCommodityType(
+                EntityType.VIRTUAL_MACHINE_VALUE, CommodityType.NET_THROUGHPUT_IN_VALUE);
+        assertEquals("KByte/sec", vmNetThroughputInUnit);
+
         String unKnownCommUnit = CommodityTypeMapping.getUnitForEntityCommodityType(
             EntityType.UNKNOWN_VALUE, CommodityType.UNKNOWN_VALUE);
         assertEquals("", unKnownCommUnit);
@@ -49,5 +57,23 @@ public class CommodityTypeMappingTest {
         String nullUnit = CommodityTypeMapping.getUnitForEntityCommodityType(
             EntityType.UNKNOWN_VALUE, -1);
         assertNull(nullUnit);
+    }
+
+    /**
+     * A unit test to test getApiCommodityTypeForVMComputeCommodities for most commonly seen VM
+     * compute resources.
+     */
+    @Test
+    public void testGetApiCommodityTypeForVMComputeCommodities() {
+        assertEquals("VCPU", CommodityTypeMapping.getApiCommodityType(CommodityType.VCPU));
+        assertEquals("CPU", CommodityTypeMapping.getApiCommodityType(CommodityType.CPU));
+        assertEquals("CPUProvisioned", CommodityTypeMapping.getApiCommodityType(CommodityType.CPU_PROVISIONED));
+        assertEquals("VMem", CommodityTypeMapping.getApiCommodityType(CommodityType.VMEM));
+        assertEquals("Mem", CommodityTypeMapping.getApiCommodityType(CommodityType.MEM));
+        assertEquals("MemProvisioned", CommodityTypeMapping.getApiCommodityType(CommodityType.MEM_PROVISIONED));
+        assertEquals("IOThroughput", CommodityTypeMapping.getApiCommodityType(CommodityType.IO_THROUGHPUT));
+        assertEquals("NetThroughput", CommodityTypeMapping.getApiCommodityType(CommodityType.NET_THROUGHPUT));
+        assertEquals("NetThroughputInbound", CommodityTypeMapping.getApiCommodityType(CommodityType.NET_THROUGHPUT_IN));
+        assertEquals("NetThroughputOutbound", CommodityTypeMapping.getApiCommodityType(CommodityType.NET_THROUGHPUT_OUT));
     }
 }
