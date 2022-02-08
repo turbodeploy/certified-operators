5b211e1d17223428578659c510b39ba44acbfb6f
Alexandr Vasin <alexandr.vasin@turbonomic.com>
2022-02-08 17:07:33 +0300

    [OM-79703]: Adjusted reconfigure action generator to keep the current capacity value.

--- com.vmturbo.market.component/src/test/java/com/vmturbo/market/runner/reconfigure/vcpu/VcpuScalingReconfigureActionGeneratorTestUtils.java
+++ com.vmturbo.market.component/src/test/java/com/vmturbo/market/runner/reconfigure/vcpu/VcpuScalingReconfigureActionGeneratorTestUtils.java
@@ -1,6 +1,9 @@
 package com.vmturbo.market.runner.reconfigure.vcpu;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
 
@@ -13,6 +16,7 @@ import com.vmturbo.common.protobuf.action.ActionDTO.ActionEntity;
 import com.vmturbo.common.protobuf.action.ActionDTO.ActionInfo;
 import com.vmturbo.common.protobuf.action.ActionDTO.Explanation;
 import com.vmturbo.common.protobuf.action.ActionDTO.Reconfigure;
+import com.vmturbo.common.protobuf.action.ActionDTO.Reconfigure.SettingChange;
 import com.vmturbo.common.protobuf.action.ActionDTO.Resize;
 import com.vmturbo.common.protobuf.common.EnvironmentTypeEnum.EnvironmentType;
 import com.vmturbo.common.protobuf.setting.SettingProto.EntitySettingGroup;
@@ -22,9 +26,12 @@ import com.vmturbo.common.protobuf.setting.SettingProto.GetEntitySettingsRespons
 import com.vmturbo.common.protobuf.setting.SettingProto.NumericSettingValue;
 import com.vmturbo.common.protobuf.setting.SettingProto.Setting;
 import com.vmturbo.common.protobuf.topology.TopologyDTO.CommodityType;
+import com.vmturbo.common.protobuf.topology.TopologyDTO.EntityAttribute;
 import com.vmturbo.common.protobuf.topology.TopologyDTO.TopologyEntityDTO;
 import com.vmturbo.common.protobuf.topology.TopologyDTO.TopologyEntityDTO.AnalysisSettings;
+import com.vmturbo.common.protobuf.topology.TopologyDTO.TopologyEntityDTO.CommoditiesBoughtFromProvider;
 import com.vmturbo.common.protobuf.topology.TopologyDTO.TypeSpecificInfo;
+import com.vmturbo.common.protobuf.topology.TopologyDTO.TypeSpecificInfo.PhysicalMachineInfo;
 import com.vmturbo.common.protobuf.topology.TopologyDTO.TypeSpecificInfo.VirtualMachineInfo;
 import com.vmturbo.platform.common.dto.CommonDTO.CommodityDTO;
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.EntityType;
@@ -33,7 +40,7 @@ import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.EntityType;
  * Util class for tests.
  */
 public class VcpuScalingReconfigureActionGeneratorTestUtils {
-    TopologyEntityDTO makeVM(long oid, int currentCoresPerSocket, boolean changeable,
+    protected TopologyEntityDTO makeVM(long oid, int currentCoresPerSocket, boolean changeable,
                              boolean stale) {
         return TopologyEntityDTO.newBuilder()
                 .setOid(oid)
@@ -48,6 +55,31 @@ public class VcpuScalingReconfigureActionGeneratorTestUtils {
                                                 .setCoresPerSocketRatio(currentCoresPerSocket)
                                                 .setCoresPerSocketChangeable(changeable))
                 ).build();
+    }
+
+    protected TopologyEntityDTO makeVM(long oid, int currentCoresPerSocket, int numCpu,
+                    boolean changeable, Long pmId) {
+        TopologyEntityDTO.Builder vmBuilder = TopologyEntityDTO.newBuilder()
+                        .setOid(oid)
+                        .setEntityType(EntityType.VIRTUAL_MACHINE_VALUE)
+                        .setEnvironmentType(EnvironmentType.ON_PREM)
+                        .setAnalysisSettings(AnalysisSettings.newBuilder().setReconfigurable(true))
+                        .setTypeSpecificInfo(
+                                        TypeSpecificInfo.newBuilder()
+                                                        .setVirtualMachine(
+                                                                        VirtualMachineInfo.newBuilder()
+                                                                                        .setCoresPerSocketRatio(currentCoresPerSocket)
+                                                                                        .setCoresPerSocketChangeable(changeable)
+                                                                                        .setNumCpus(numCpu))
+                        );
+        if (pmId != null) {
+            vmBuilder.addCommoditiesBoughtFromProviders(
+                            CommoditiesBoughtFromProvider.newBuilder()
+                                            .setProviderId(pmId)
+                                            .setProviderEntityType(EntityType.PHYSICAL_MACHINE_VALUE)
+            );
+        }
+        return vmBuilder.build();
     }
 
     GetEntitySettingsResponse makeGetEntitySettingsResponse(List<Long> vms,
@@ -93,11 +125,42 @@ public class VcpuScalingReconfigureActionGeneratorTestUtils {
         return resizeAction;
     }
 
+
+
     @Nonnull
     protected ActionDTO.Reconfigure.SettingChange getFirstSettingChangeOfTheFirstAction(@Nonnull List<Action> actions) {
         Assert.assertThat(actions.isEmpty(), CoreMatchers.is(false));
         final Reconfigure reconfigure = actions.get(0).getInfo().getReconfigure();
         Assert.assertThat(reconfigure.getSettingChangeCount(), CoreMatchers.is(1));
         return reconfigure.getSettingChange(0);
+    }
+
+    @Nonnull
+    protected Map<EntityAttribute, SettingChange> getChangesOfTheFirstAction(
+                    @Nonnull List<Action> actions) {
+        Assert.assertThat(actions.isEmpty(), CoreMatchers.is(false));
+        final Reconfigure reconfigure = actions.get(0).getInfo().getReconfigure();
+        return reconfigure.getSettingChangeList().stream()
+                        .collect(Collectors.toMap(SettingChange::getEntityAttribute,
+                                        Function.identity()));
+    }
+
+    /**
+     * Creates PM instance with specified oid and specific number of sockets.
+     *
+     * @param oid of the desired PM entity.
+     * @param socketsNum number of sockets for the PM.
+     * @return return {@link TopologyEntityDTO} that represents PM instance.
+     */
+    @Nonnull
+    protected static TopologyEntityDTO makePM(long oid, int socketsNum) {
+        final PhysicalMachineInfo.Builder pmInfoBuilder =
+                        PhysicalMachineInfo.newBuilder().setNumCpuSockets(socketsNum)
+                                        .setNumCpuThreads(socketsNum * 2);
+        return TopologyEntityDTO.newBuilder().setOid(oid)
+                        .setEntityType(EntityType.PHYSICAL_MACHINE_VALUE)
+                        .setEnvironmentType(EnvironmentType.ON_PREM).setTypeSpecificInfo(
+                                        TypeSpecificInfo.newBuilder()
+                                                        .setPhysicalMachine(pmInfoBuilder)).build();
     }
 }
