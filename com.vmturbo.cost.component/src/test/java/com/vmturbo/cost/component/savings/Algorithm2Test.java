2de6b82f517e7e78ce33e9337a449491d3b336d3
Professor Git <builder@turbonomic.com>
2022-02-09 19:05:33 -0500

    Reverted from xl-staging-ci-build-1465 and reopened jira issues (OM-80510 ) and reverted commits(245258c4d6a49919297c436040b6ed3489b028b3 5c881244821a9f3984705096f371059c8e3d492f )

--- com.vmturbo.cost.component/src/test/java/com/vmturbo/cost/component/savings/Algorithm2Test.java
+++ com.vmturbo.cost.component/src/test/java/com/vmturbo/cost/component/savings/Algorithm2Test.java
@@ -22,7 +22,7 @@ public class Algorithm2Test {
         List<Double> deltaList = ImmutableList.of(2.0, 2.0, -2.0, 6.0, -4.0, -6.0, 4.0, -8.0, 6.0,
                 -4.0, 10.0, -8.0);
         Algorithm alg = new Algorithm2(0L, 0L);
-        deltaList.stream().forEach(delta -> alg.addAction(0L, delta, 10L));
+        deltaList.stream().forEach(delta -> alg.addAction(delta, 10L));
         alg.endPeriod(0L, 1L);
         SavingsInvestments results = alg.getRealized();
         Assert.assertEquals(10.0, results.getSavings(), 0.0001);
@@ -39,7 +39,7 @@ public class Algorithm2Test {
         EntityState entityState = EntityState.fromJson("{\"entityId\":74612826803208,\"powerFactor\":1,\"actionList\":[2.0, 2.0, -2.0, 6.0, -4.0, -6.0, 4.0, -8.0, 6.0, -4.0, 10.0, -8.0],\"realizedSavings\":10.0,\"realizedInvestments\":4.0,\"missedSavings\":0.0,\"missedInvestments\":0.0}");
         Algorithm alg = new Algorithm2(0L, 0L);
         alg.initState(periodEnd, entityState);
-        Assert.assertTrue(alg.getDeltaList().isEmpty());
+        Assert.assertTrue(alg.getActionList().isEmpty());
         SavingsInvestments results = alg.getRealized();
         Assert.assertEquals(0.0, results.getSavings(), 0.0001);
         Assert.assertEquals(0.0, results.getInvestments(), 0.0001);
