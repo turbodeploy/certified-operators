5e132076f8b6aea54501c6ab84c28a06f4a5e2ef
Marco Berlot <marco.berlot@turbonomic.com>
2022-02-15 13:32:48 -0500

    [OM-80224] Enhance MultiDBMigration tests

--- com.vmturbo.action.orchestrator/src/test/java/com/vmturbo/action/orchestrator/ActionOrchestratorMultiDBMigrationTest.java
+++ com.vmturbo.action.orchestrator/src/test/java/com/vmturbo/action/orchestrator/ActionOrchestratorMultiDBMigrationTest.java
@@ -2,6 +2,7 @@ package com.vmturbo.action.orchestrator;
 
 import org.junit.Test;
 
+import com.vmturbo.commons.Pair;
 import com.vmturbo.sql.utils.MultiDBMigrationTest;
 
 /**
@@ -9,7 +10,12 @@ import com.vmturbo.sql.utils.MultiDBMigrationTest;
  */
 public class ActionOrchestratorMultiDBMigrationTest extends MultiDBMigrationTest {
 
-    private static final int INITIAL_OFFSET = 24;
+    /**
+     * This initial offset represents the last mariadb migration that is included in the base migration for Postgres.
+     * This is intended to NEVER be changed, with the exception of very few occasions. If that happens, we need to make sure
+     * to update both the number, and put the new MariaDB migration name.
+     */
+    private static final Pair<Integer, String> INITIAL_OFFSET = new Pair<>(24, "V1_24__workflow_primary_key.sql");
     private static final String DEFAULT_PATH = "src/main/resources/db/migration";
     private static final String MARIA_DB_PATH = "src/main/resources/db/migrations/action-orchestrator/mariadb";
     private static final String POSTGRES_PATH = "src/main/resources/db/migrations/action-orchestrator/postgres";
@@ -19,6 +25,6 @@ public class ActionOrchestratorMultiDBMigrationTest extends MultiDBMigrationTest
      */
     @Test
     public void testMigrations() {
-        testMultiDBMigrations(INITIAL_OFFSET, DEFAULT_PATH, MARIA_DB_PATH, POSTGRES_PATH);
+        testMultiDBMigrations(INITIAL_OFFSET.first, DEFAULT_PATH, MARIA_DB_PATH, POSTGRES_PATH);
     }
 }
