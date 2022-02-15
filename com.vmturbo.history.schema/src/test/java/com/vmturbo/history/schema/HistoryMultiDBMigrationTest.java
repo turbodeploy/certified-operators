a78e241ed24d07dfff8241deffd5068420973fe8
Marco Berlot <marco.berlot@turbonomic.com>
2022-02-15 13:32:48 -0500

    [OM-80224] Enhance MultiDBMigration tests

--- com.vmturbo.history.schema/src/test/java/com/vmturbo/history/schema/HistoryMultiDBMigrationTest.java
+++ com.vmturbo.history.schema/src/test/java/com/vmturbo/history/schema/HistoryMultiDBMigrationTest.java
@@ -2,6 +2,7 @@ package com.vmturbo.history.schema;
 
 import org.junit.Test;
 
+import com.vmturbo.commons.Pair;
 import com.vmturbo.sql.utils.MultiDBMigrationTest;
 
 /**
@@ -10,7 +11,12 @@ import com.vmturbo.sql.utils.MultiDBMigrationTest;
 public class HistoryMultiDBMigrationTest extends MultiDBMigrationTest {
 
 
-    private static final int INITIAL_OFFSET = 80;
+    /**
+     * This initial offset represents the last mariadb migration that is included in the base migration for Postgres.
+     * This is intended to NEVER be changed, with the exception of very few occasions. If that happens, we need to make sure
+     * to update both the number, and put the new MariaDB migration name.
+     */
+    private static final Pair<Integer, String> INITIAL_OFFSET = new Pair<>(80, "V1.61__fix_cnt_indexes.sql");
     private static final String DEFAULT_PATH = "src/main/resources/db/migration";
     private static final String MARIA_DB_PATH = "src/main/resources/db/migrations/history/mariadb";
     private static final String POSTGRES_PATH = "src/main/resources/db/migrations/history/postgres";
@@ -20,7 +26,7 @@ public class HistoryMultiDBMigrationTest extends MultiDBMigrationTest {
      */
     @Test
     public void testMigrations() {
-        testMultiDBMigrations(INITIAL_OFFSET, DEFAULT_PATH, MARIA_DB_PATH, POSTGRES_PATH);
+        testMultiDBMigrations(INITIAL_OFFSET.first, DEFAULT_PATH, MARIA_DB_PATH, POSTGRES_PATH);
     }
 
 }
