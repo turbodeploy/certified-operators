9b6b50d28b93c8c3b5c534ab8338b01bb3f194dd
Megan Jimenez <megan.jimenez@turbonomic.com>
2022-04-07 15:49:55 -0400

    [OM-82487]: "Feature/ search validation"

--- com.vmturbo.api.component/src/main/java/com/vmturbo/api/component/external/api/util/ValidationUtils.java
+++ com.vmturbo.api.component/src/main/java/com/vmturbo/api/component/external/api/util/ValidationUtils.java
@@ -2,17 +2,19 @@ package com.vmturbo.api.component.external.api.util;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Optional;
 import java.util.Set;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import com.google.common.collect.ImmutableSet;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.util.Assert;
 
+import com.vmturbo.api.component.external.api.mapper.MarketMapper;
 import com.vmturbo.api.enums.EntityType;
+import com.vmturbo.api.enums.GroupType;
 import com.vmturbo.common.protobuf.utils.StringConstants;
 
 /**
@@ -21,14 +23,25 @@ import com.vmturbo.common.protobuf.utils.StringConstants;
 public class ValidationUtils {
 
     private static final Set<String> EXTERNAL_ENTITY_TYPES;
+    private static final Set<String> GROUP_ENTITY_TYPES;
+    private static final Set<String> SEARCHABLE_OBJ_TYPES;
 
     static {
         ImmutableSet.Builder<String> builder = new ImmutableSet.Builder<>();
 
+        // build Entity Types set
         Arrays.stream(EntityType.values()).map(Enum::toString).forEach(builder::add);
         builder.add(StringConstants.WORKLOAD);
-
         EXTERNAL_ENTITY_TYPES = builder.build();
+
+        // build Group Entity Types set (groups, entities)
+        Arrays.stream(GroupType.values()).map(Enum::toString).forEach(builder::add);
+        GROUP_ENTITY_TYPES = builder.build();
+
+        // build Searchable generic Types set (groups, entities, market, target)
+        builder.add(MarketMapper.MARKET);
+        builder.add(StringConstants.TARGET);
+        SEARCHABLE_OBJ_TYPES = builder.build();
     }
 
     private ValidationUtils() {}
@@ -40,22 +53,42 @@ public class ValidationUtils {
      * @throws IllegalArgumentException if at least one element is invalid
      */
     public static void validateExternalEntityTypes(@Nullable final Collection<String> entityTypes) {
-        Optional.ofNullable(entityTypes).orElse(Collections.emptyList()).forEach(
-                ValidationUtils::validateExternalEntityType);
+        assertIsSubset(entityTypes, EXTERNAL_ENTITY_TYPES, "Invalid entity type(s)");
     }
 
     /**
-     * Validates that the supplied string is a supported entity type.
+     * Validates that all elements in the supplied {@link Collection} are supported searchable group
+     * types.
      *
-     * @param entityType the entity type string
-     * @throws IllegalArgumentException if the entity type is invalid
+     * @param groupTypes a collection of group or entity type strings, refers to the members of a group
+     * @throws IllegalArgumentException if at least one element is invalid
      */
-    public static void validateExternalEntityType(final String entityType) {
-        Assert.isTrue(isValidExternalEntityType(entityType),
-                () -> String.format("'%s' is not a valid entity type", entityType));
+    public static void validateGroupEntityTypes(@Nullable final Collection<String> groupTypes) {
+        assertIsSubset(groupTypes, GROUP_ENTITY_TYPES, "Invalid group type(s)");
     }
 
-    private static boolean isValidExternalEntityType(final String entityType) {
-        return EXTERNAL_ENTITY_TYPES.contains(entityType);
+    /**
+     * Validates that all elements in the supplied {@link Collection} are supported searchable
+     * object types.
+     *
+     * @param types a collection of searchable object type strings
+     * @throws IllegalArgumentException if at least one element is invalid
+     */
+    public static void validateSearchableObjTypes(@Nullable final Collection<String> types) {
+        assertIsSubset(types, SEARCHABLE_OBJ_TYPES, "Invalid type(s)");
+    }
+
+    /**
+     * Validates that the supplied {@link Collection} contains only expected values.
+     * @param actual a collection of strings to be validated
+     * @param expected a static set of allowable strings
+     * @param message error message used upon failure
+     * @throws IllegalArgumentException if at least one element is invalid
+     */
+    private static void assertIsSubset(@Nullable Collection<String> actual, @Nonnull Collection<String> expected,
+            String message) throws IllegalArgumentException {
+        Collection<String> invalidValues = CollectionUtils.subtract(CollectionUtils.emptyIfNull(actual), expected);
+        Assert.isTrue(invalidValues.isEmpty(), String.format("%s: %s", message,
+                String.join(", ", invalidValues)));
     }
 }
--- com.vmturbo.api.component/src/test/java/com/vmturbo/api/component/external/api/util/ValidationUtilsTest.java
+++ com.vmturbo.api.component/src/test/java/com/vmturbo/api/component/external/api/util/ValidationUtilsTest.java
@@ -7,7 +7,9 @@ import java.util.Collections;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import com.vmturbo.api.component.external.api.mapper.MarketMapper;
 import com.vmturbo.api.enums.EntityType;
+import com.vmturbo.api.enums.GroupType;
 import com.vmturbo.common.protobuf.utils.StringConstants;
 
 /**
@@ -16,40 +18,41 @@ import com.vmturbo.common.protobuf.utils.StringConstants;
 public class ValidationUtilsTest {
 
     /**
-     * Test that {@link ValidationUtils#validateExternalEntityType(String)} does not throw an
+     * Test that {@link ValidationUtils#validateExternalEntityTypes(Collection)} does not throw an
      * exception when a valid {@link EntityType} string is passed to the function.
      */
     @Test
     public void testApiEntityTypeStringIsValid() {
-        ValidationUtils.validateExternalEntityType(EntityType.VirtualMachine.toString());
+        ValidationUtils.validateExternalEntityTypes(
+                Collections.singleton(EntityType.VirtualMachine.toString()));
     }
 
     /**
-     * Test that {@link ValidationUtils#validateExternalEntityType(String)} does not throw an
+     * Test that {@link ValidationUtils#validateExternalEntityTypes(Collection)} does not throw an
      * exception when the value {@link StringConstants#WORKLOAD} is passed to the function.
      */
     @Test
     public void testWorkloadStringIsValid() {
-        ValidationUtils.validateExternalEntityType(StringConstants.WORKLOAD);
+        ValidationUtils.validateExternalEntityTypes(Collections.singleton(StringConstants.WORKLOAD));
     }
 
     /**
-     * Test that {@link ValidationUtils#validateExternalEntityType(String)} throws an
+     * Test that {@link ValidationUtils#validateExternalEntityTypes(Collection)} throws an
      * {@link IllegalArgumentException} when an invalid string is passed to the function.
      */
     @Test
     public void testExceptionThrownWhenEntityTypeStringIsInvalid() {
-        Assertions.assertThatThrownBy(() -> ValidationUtils.validateExternalEntityType("invalid"))
+        Assertions.assertThatThrownBy(() -> ValidationUtils.validateExternalEntityTypes(Collections.singleton("invalid")))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     /**
-     * Test that {@link ValidationUtils#validateExternalEntityType(String)} throws an
+     * Test that {@link ValidationUtils#validateExternalEntityTypes(Collection)} throws an
      * {@link IllegalArgumentException} when null string is passed to the function.
      */
     @Test
     public void testExceptionThrownWhenEntityTypesStringIsNull() {
-        Assertions.assertThatThrownBy(() -> ValidationUtils.validateExternalEntityType(null))
+        Assertions.assertThatThrownBy(() -> ValidationUtils.validateExternalEntityTypes(Collections.singleton(null)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -74,6 +77,15 @@ public class ValidationUtilsTest {
     }
 
     /**
+     * Test that {@link ValidationUtils#validateExternalEntityTypes(Collection)} does not throw an
+     * exception when null is passed to the function.
+     */
+    @Test
+    public void testNullEntityTypesCollectionIsValid() {
+        ValidationUtils.validateExternalEntityTypes(null);
+    }
+
+    /**
      * Test that {@link ValidationUtils#validateExternalEntityTypes(Collection)} throws an
      * {@link IllegalArgumentException} when a collection containing at least one invalid string
      * is passed to the function.
@@ -95,5 +107,150 @@ public class ValidationUtilsTest {
         Assertions.assertThatThrownBy(() -> ValidationUtils.validateExternalEntityTypes(
                 Arrays.asList(EntityType.VirtualMachine.toString(), null))).isInstanceOf(
                 IllegalArgumentException.class);
+    }
+
+    /**
+     * Test that {@link ValidationUtils#validateGroupEntityTypes(Collection)} does not throw an
+     * exception when an empty collection is passed to the function.
+     */
+    @Test
+    public void testEmptyGroupEntityTypesCollectionIsValid() {
+        ValidationUtils.validateGroupEntityTypes(Collections.emptyList());
+    }
+
+    /**
+     * Test that {@link ValidationUtils#validateGroupEntityTypes(Collection)} does not throw an
+     * exception when null is passed to the function.
+     */
+    @Test
+    public void testNullGroupEntityTypesCollectionIsValid() {
+        ValidationUtils.validateGroupEntityTypes(null);
+    }
+
+    /**
+     * Test that {@link ValidationUtils#validateGroupEntityTypes(Collection)} does not throw an
+     * exception when a valid {@link EntityType} string is passed to the function.
+     */
+    @Test
+    public void testEntityTypeAsValidGroupEntityType() {
+        ValidationUtils.validateGroupEntityTypes(Collections.singleton(EntityType.VirtualMachine.toString()));
+    }
+
+    /**
+     * Test that {@link ValidationUtils#validateGroupEntityTypes(Collection)} does not throw an
+     * exception when a valid {@link GroupType} string is passed to the function.
+     */
+    @Test public void testGroupTypeAsValidGroupEntityType() {
+        ValidationUtils.validateGroupEntityTypes(Collections.singleton(GroupType.Group.toString()));
+    }
+
+    /**
+     * Test that {@link ValidationUtils#validateGroupEntityTypes(Collection)} throws an
+     * {@link IllegalArgumentException} when a collection containing an invalid string
+     * is passed to the function.
+     */
+    @Test
+    public void testExceptionThrownWhenGroupEntityTypeStringIsInvalid() {
+        Assertions.assertThatThrownBy(() -> ValidationUtils.validateGroupEntityTypes(Collections.singleton("invalid")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    /**
+     * Test that {@link ValidationUtils#validateGroupEntityTypes(Collection)} throws an
+     * {@link IllegalArgumentException} when null string is passed to the function.
+     */
+    @Test
+    public void testExceptionThrownWhenGroupEntityTypesStringIsNull() {
+        Assertions.assertThatThrownBy(() -> ValidationUtils.validateGroupEntityTypes(Collections.singleton(null)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    /**
+     * Test that {@link ValidationUtils#validateGroupEntityTypes(Collection)} throws an
+     * {@link IllegalArgumentException} when a collection contains both valid and invalid strings.
+     */
+    @Test
+    public void testExceptionThrowWhenGroupEntityTypesIncludesInvalid() {
+        Assertions.assertThatThrownBy(() -> ValidationUtils.validateGroupEntityTypes(
+                Arrays.asList(GroupType.Group.toString(), EntityType.VirtualMachine.toString(), null)
+        )).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    /**
+     * Test that {@link ValidationUtils#validateSearchableObjTypes(Collection)} does not throw an
+     * exception when an empty collection is passed to the funciton.
+     */
+    @Test
+    public void testEmptyObjTypeCollectionIsValid() {
+        ValidationUtils.validateSearchableObjTypes(Collections.emptyList());
+    }
+
+    /**
+     * Test that {@link ValidationUtils#validateSearchableObjTypes(Collection)} does not throw an
+     * exception when null is passed to the function.
+     */
+    @Test
+    public void testNullObjTypeCollectionIsValid() {
+        ValidationUtils.validateSearchableObjTypes(null);
+    }
+
+    /**
+     * Test that {@link ValidationUtils#validateSearchableObjTypes(Collection)} does not throw an
+     * exception when a valid {@link EntityType} string is passed to the function.
+     */
+    @Test
+    public void testEntityTypeAsValidObjType() {
+        ValidationUtils.validateSearchableObjTypes(Collections.singleton(EntityType.VirtualMachine.toString()));
+    }
+
+    /**
+     * Test that {@link ValidationUtils#validateSearchableObjTypes(Collection)} does not throw an
+     * exception when a valid {@link GroupType} string is passed to the function.
+     */
+    @Test
+    public void testGroupTypeAsValidObjType() {
+        ValidationUtils.validateSearchableObjTypes(Collections.singleton(GroupType.Group.toString()));
+    }
+
+    /**
+     * Test that {@link ValidationUtils#validateSearchableObjTypes(Collection)} does not throw an
+     * exception when the values {@link StringConstants#WORKLOAD} or {@link MarketMapper#MARKET} is
+     * passed to the function.
+     */
+    @Test
+    public void testConstantsAsValidObjTypes() {
+        ValidationUtils.validateSearchableObjTypes(Arrays.asList(MarketMapper.MARKET, StringConstants.TARGET));
+    }
+
+    /**
+     * Test that {@link ValidationUtils#validateSearchableObjTypes(Collection)} throws an
+     * {@link IllegalArgumentException} when a collection containing an invalid string
+     * is passed to the function.
+     */
+    @Test
+    public void testExceptionThrownWhenObjTypeStringIsInvalid() {
+        Assertions.assertThatThrownBy(() -> ValidationUtils.validateSearchableObjTypes(Collections.singleton("invalid")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    /**
+     * Test that {@link ValidationUtils#validateSearchableObjTypes(Collection)} throws an
+     * {@link IllegalArgumentException} when null string is passed to the function.
+     */
+    @Test
+    public void testExceptionThrownWhenObjTypesStringIsNull() {
+        Assertions.assertThatThrownBy(() -> ValidationUtils.validateSearchableObjTypes(Collections.singleton(null)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    /**
+     * Test that {@link ValidationUtils#validateSearchableObjTypes(Collection)} throws an
+     * {@link IllegalArgumentException} when a collection contains both valid and invalid strings.
+     */
+    @Test
+    public void testExceptionThrowWhenObjTypesIncludesInvalid() {
+        Assertions.assertThatThrownBy(() -> ValidationUtils.validateSearchableObjTypes(
+                Arrays.asList(GroupType.Group.toString(), EntityType.VirtualMachine.toString(), null)
+        )).isInstanceOf(IllegalArgumentException.class);
     }
 }
