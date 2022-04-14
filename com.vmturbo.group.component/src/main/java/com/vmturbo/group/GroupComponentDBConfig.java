2d5132615529408ca16341d1ef38fa3708bfd308
Gary Zeng <gary.zeng@turbonomic.com>
2022-04-13 21:00:46 -0400

    [OM-83432]: enable unpooled datasource to support non-default/non-privileged db user (cherry-picking staging)

--- com.vmturbo.group.component/src/main/java/com/vmturbo/group/GroupComponentDBConfig.java
+++ com.vmturbo.group.component/src/main/java/com/vmturbo/group/GroupComponentDBConfig.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.sql.DataSource;
 
 import org.apache.logging.log4j.util.Strings;
+import org.jooq.DSLContext;
+import org.jooq.impl.DefaultDSLContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
@@ -61,5 +63,30 @@ public class GroupComponentDBConfig extends SQLDatabaseConfig {
     @Override
     public String getDbUsername() {
         return groupComponentDbUsername;
+    }
+
+    /**
+     * Get a {@link DataSource} that will produce connections that are not part of the connection
+     * pool. This may be advisable for connections that will be used for potentially long-running
+     * operations, to avoid tying up limited pool connections.
+     *
+     * @return unpooled datasource
+     */
+    @Bean
+    public DataSource unpooledDataSource() {
+        return getUnpooledDataSource(dbSchemaName, groupComponentDbUsername,
+                Optional.ofNullable(!Strings.isEmpty(groupComponentDbPassword) ? groupComponentDbPassword : null));
+    }
+
+
+    /**
+     * Get a {@link DSLContext} that uses unpooled connections to perform database operations.
+     * This may be advisable when performing potentially long-running DB operaitions to avoid
+     * tying up limited pool connections.
+     *
+     * @return DSLContext that uses unpooled connections
+     */
+    public DSLContext unpooledDsl() {
+        return new DefaultDSLContext(configuration(unpooledDataSource()));
     }
 }
