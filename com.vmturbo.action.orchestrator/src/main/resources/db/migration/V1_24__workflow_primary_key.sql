6ddedd581376357e8fec31685e0417ed26b725c8
Gary Zeng <gary.zeng@turbonomic.com>
2022-05-03 19:22:43 -0400

    [OM-84275]: cherry-picking two fix AO V1.24 migration commits from 8.5.1 (staging)

--- com.vmturbo.action.orchestrator/src/main/resources/db/migration/V1_24__workflow_primary_key.sql
+++ com.vmturbo.action.orchestrator/src/main/resources/db/migration/V1_24__workflow_primary_key.sql
@@ -1,5 +1,30 @@
--- Drop the unique index on id
+-- Drop the foreign key and the unique index on id
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- Drop foreign key if exists
+DROP PROCEDURE IF EXISTS PROC_DROP_FOREIGN_KEY;
+DELIMITER $$
+CREATE PROCEDURE PROC_DROP_FOREIGN_KEY()
+BEGIN
+    IF EXISTS(
+        SELECT * FROM information_schema.table_constraints
+        WHERE
+            table_schema    = database()     AND
+            table_name      = 'workflow'      AND
+            constraint_name = 'workflow_ibfk_1' AND
+            constraint_type = 'FOREIGN KEY')
+    THEN
+        SET @query = CONCAT('ALTER TABLE workflow DROP FOREIGN KEY workflow_ibfk_1;');
+        PREPARE stmt FROM @query;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+    END IF;
+END$$
+DELIMITER ;
+
+CALL PROC_DROP_FOREIGN_KEY();
+DROP PROCEDURE IF EXISTS PROC_DROP_FOREIGN_KEY;
+
 -- there's no DROP INDEX IF EXISTS, so...
 DROP PROCEDURE IF EXISTS _drop_index;
 DELIMITER //
@@ -21,3 +46,27 @@ SET FOREIGN_KEY_CHECKS = 1;
 
 -- Make id the primary key
 ALTER TABLE workflow ADD PRIMARY KEY (id);
+
+-- Recreate foreign key constraint
+DROP PROCEDURE IF EXISTS PROC_CREATE_FOREIGN_KEY;
+DELIMITER $$
+CREATE PROCEDURE PROC_CREATE_FOREIGN_KEY()
+BEGIN
+    IF NOT EXISTS(
+        SELECT * FROM information_schema.table_constraints
+        WHERE
+            table_schema    = database()     AND
+            table_name      = 'workflow'      AND
+            constraint_name = 'workflow_ibfk_1' AND
+            constraint_type = 'FOREIGN KEY')
+    THEN
+        SET @query = CONCAT('ALTER TABLE workflow ADD CONSTRAINT workflow_ibfk_1 FOREIGN KEY (id) REFERENCES workflow_oid(id) ON DELETE CASCADE;');
+        PREPARE stmt FROM @query;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+    END IF;
+END$$
+DELIMITER ;
+
+CALL PROC_CREATE_FOREIGN_KEY();
+DROP PROCEDURE IF EXISTS PROC_CREATE_FOREIGN_KEY;
