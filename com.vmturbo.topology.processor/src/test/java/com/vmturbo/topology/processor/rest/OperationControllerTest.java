0009aaae5566cbf07213be85f34c4b3c32de11c8
Marco Berlot <marco.berlot@turbonomic.com>
2022-05-17 10:30:13 -0400

    [OM-83031] Enable Optimized Identity cache by default

--- com.vmturbo.topology.processor/src/test/java/com/vmturbo/topology/processor/rest/OperationControllerTest.java
+++ com.vmturbo.topology.processor/src/test/java/com/vmturbo/topology/processor/rest/OperationControllerTest.java
@@ -131,7 +131,7 @@ public class OperationControllerTest {
         @Bean
         IdentityProvider identityProvider() {
             return new IdentityProviderImpl(new MapKeyValueStore(), new ProbeInfoCompatibilityChecker(), 0L,
-                mock(IdentityDatabaseStore.class), 10, 0, false, mock(StaleOidManagerImpl.class),
+                mock(IdentityDatabaseStore.class), 10, 0, mock(StaleOidManagerImpl.class),
                     false);
         }
 
