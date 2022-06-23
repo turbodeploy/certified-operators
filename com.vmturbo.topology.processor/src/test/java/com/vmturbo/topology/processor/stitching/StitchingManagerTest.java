86d9573a61ffab8aac91401cede843d037209a01
Zhuoran Bao <zhuoran.bao@turbonomic.com>
2022-06-23 14:56:35 -0400

    [OM-85267] Enable the Delay Data detection logic REVERT

--- com.vmturbo.topology.processor/src/test/java/com/vmturbo/topology/processor/stitching/StitchingManagerTest.java
+++ com.vmturbo.topology.processor/src/test/java/com/vmturbo/topology/processor/stitching/StitchingManagerTest.java
@@ -175,7 +175,6 @@ public class StitchingManagerTest {
             .build()));
         when(target.getProbeId()).thenReturn(20L);
         when(target.getProbeInfo()).thenReturn(ProbeInfo.getDefaultInstance());
-        featureFlagTestRule.disable(FeatureFlags.DELAYED_DATA_HANDLING);
     }
 
     @Test
