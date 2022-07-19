fc2e84623c046e5ee0ec25a60039a4a5650d212d
Manos Vekrakis <manos.vekrakis@turbonomic.com>
2022-07-19 09:30:41 -0400

    [OM-84292]: Extend MultiDBMigrationTest to cover Java migrations

--- com.vmturbo.sql.test.utils/src/main/java/com/vmturbo/sql/utils/MultiDBMigrationTest.java
+++ com.vmturbo.sql.test.utils/src/main/java/com/vmturbo/sql/utils/MultiDBMigrationTest.java
@@ -8,28 +8,37 @@ import org.junit.Assert;
 /**
  * Ensures that each migration in the default folder has a corresponding migration for the supported
  * databases used by the DBEndpoint (currently MariaDB and Postgres). Since postgres was added much later
- * each component has an hardcoded offset that represents the number of already existing sql
+ * each component has a hardcoded offset that represents the number of already existing sql
  * migrations at the time the first Postgres migration was introduced.
  */
 public abstract class MultiDBMigrationTest {
 
     /**
      * Test that the number of migrations for the supported databases match.
-     * @param initialOffset number of sql migrations at the time the first postgres migration was introduced
-     * @param defaultPath path with default migrations
-     * @param mariaDBPath path with mariadb migrations
-     * @param postgresPath path with postgres migrations
+     * @param sqlInitialOffset number of sql migrations at the time the first postgres migration was introduced
+     * @param sqlDefaultPath path with default sql migrations
+     * @param sqlMariaDBPath path with mariadb sql migrations
+     * @param sqlPostgresPath path with postgres sql migrations
+     * @param javaInitialOffset number of java migrations at the time the first postgres migration was introduced
+     * @param javaDefaultPath path with default java migrations
+     * @param javaMariaDBPath path with mariadb java migrations
+     * @param javaPostgresPath path with postgres java migrations
      */
-    public void testMultiDBMigrations(final int initialOffset, final String defaultPath, final String mariaDBPath,
-            final String postgresPath) {
-        final File migrationDir = new File(defaultPath);
-        final File mariaDbDir = new File(mariaDBPath);
-        final File postgresDir = new File(postgresPath);
+    public void testMultiDBMigrations(final int sqlInitialOffset, final String sqlDefaultPath, final String sqlMariaDBPath,
+            final String sqlPostgresPath, final int javaInitialOffset, final String javaDefaultPath, final String javaMariaDBPath,
+            final String javaPostgresPath) {
+        Assert.assertEquals(filesCount(sqlDefaultPath), filesCount(sqlMariaDBPath));
+        Assert.assertEquals(filesCount(sqlPostgresPath) + sqlInitialOffset, filesCount(sqlMariaDBPath));
+        Assert.assertEquals(filesCount(javaDefaultPath), filesCount(javaMariaDBPath));
+        Assert.assertEquals(filesCount(javaMariaDBPath), filesCount(javaPostgresPath) + javaInitialOffset);
+    }
 
-
-        Assert.assertEquals(Objects.requireNonNull(migrationDir.listFiles()).length, Objects.requireNonNull(
-                mariaDbDir.listFiles()).length);
-        Assert.assertEquals(Objects.requireNonNull(postgresDir.listFiles()).length + initialOffset, Objects.requireNonNull(
-                mariaDbDir.listFiles()).length);
+    private static int filesCount(final String path) {
+        final File folder = new File(path);
+        // If the folder doesn't exist, the count of the migrations is zero.
+        if (!folder.exists()) {
+            return 0;
+        }
+        return Objects.requireNonNull(folder.listFiles()).length;
     }
 }
