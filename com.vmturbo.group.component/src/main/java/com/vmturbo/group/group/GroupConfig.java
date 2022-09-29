b52e8f0d548f9563ac3a0b29c34a416873be700f
Professor Git <builder@turbonomic.com>
2022-09-29 18:32:10 -0400

    Reverted from xl-staging-ci-build-2464 and reopened jira issues (OM-89953 ) and reverted commits(1c78a9520eb04ed5280961b2151e3300ae307a47 77e9bbcf436556b472f2e9833eff7dc517d7169d )

--- com.vmturbo.group.component/src/main/java/com/vmturbo/group/group/GroupConfig.java
+++ com.vmturbo.group.component/src/main/java/com/vmturbo/group/group/GroupConfig.java
@@ -18,7 +18,6 @@ import com.vmturbo.group.GroupMultiDBConfig;
 import com.vmturbo.group.IdentityProviderConfig;
 import com.vmturbo.group.flyway.ResetJavaMigrationsChecksumsCallback;
 import com.vmturbo.group.flyway.V1_11_Callback;
-import com.vmturbo.group.flyway.VCPUScalingUnitSettingChecksumCallback;
 import com.vmturbo.group.group.pagination.GroupPaginationConfig;
 import com.vmturbo.sql.utils.DbEndpoint.UnsupportedDialectException;
 import com.vmturbo.sql.utils.MultiDB;
@@ -67,8 +66,7 @@ public class GroupConfig {
     public FlywayCallback[] flywayCallbacks() {
         return new FlywayCallback[] {
             new V1_11_Callback(),
-            new ResetJavaMigrationsChecksumsCallback(),
-            new VCPUScalingUnitSettingChecksumCallback()
+            new ResetJavaMigrationsChecksumsCallback()
         };
     }
 
--- com.vmturbo.group.component/src/main/resources/db/migrations/group/postgres/V1_5__change_vcpu_scaling_default.sql
+++ com.vmturbo.group.component/src/main/resources/db/migrations/group/postgres/V1_5__change_vcpu_scaling_default.sql
@@ -1,19 +1,10 @@
--- Migration to change value of VCPUScalingUnits setting in VM default policy to SOCKETS.
--- We change this only if the current value in VM default policy is MHZ and 1800.
+-- Migration to change value of VCPUScalingUnits setting
+-- base value of that setting from MHZ to SOCKETS
 
-DO $$
-DECLARE default_vm_policy_id BIGINT;
-DECLARE default_vm_policy_scaling_unit VARCHAR;
-DECLARE default_vm_policy_used_increment_vcpu VARCHAR;
-BEGIN
-SELECT id INTO default_vm_policy_id FROM setting_policy WHERE name = 'Virtual Machine Defaults';
-
-SELECT setting_value INTO default_vm_policy_scaling_unit FROM setting_policy_setting WHERE policy_id = default_vm_policy_id AND setting_name = 'vcpuScalingUnits';
-SELECT setting_value INTO default_vm_policy_used_increment_vcpu FROM setting_policy_setting WHERE policy_id = default_vm_policy_id AND setting_name = 'usedIncrement_VCPU';
 UPDATE setting_policy_setting
-SET setting_value ='SOCKETS'
-WHERE default_vm_policy_scaling_unit = 'MHZ'
-  AND default_vm_policy_used_increment_vcpu = '1800.0'
-  AND policy_id = @default_vm_policy_id
-  AND setting_name = 'vcpuScalingUnits' ;
-END $$
+SET setting_value='SOCKETS'
+WHERE setting_name='vcpuScalingUnits';
+
+UPDATE settings
+SET setting_value='SOCKETS'
+WHERE setting_name='vcpuScalingUnits';
