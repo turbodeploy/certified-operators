7101dfb0338f7da40401b3053eab50f39f436dff
Evan Frenn <evan.frenn@turbonomic.com>
2022-09-28 20:48:01 -0400

    [OM-90437]: Update tag group identity service and tag identity service to...

---
+++ com.vmturbo.cost.component/src/main/java/com/vmturbo/cost/component/cloud/cost/tag/TagIdentity.java
@@ -0,0 +1,40 @@
+package com.vmturbo.cost.component.cloud.cost.tag;
+
+import javax.annotation.Nonnull;
+
+import org.immutables.value.Value.Immutable;
+
+import com.vmturbo.cloud.common.immutable.HiddenImmutableTupleImplementation;
+
+/**
+ * A {@link Tag} with its persistence ID.
+ */
+@HiddenImmutableTupleImplementation
+@Immutable
+public interface TagIdentity {
+
+    /**
+     * The tag ID.
+     * @return The tag ID.
+     */
+    long tagId();
+
+    /**
+     * The tag.
+     * @return The tag.
+     */
+    @Nonnull
+    Tag tag();
+
+    /**
+     * Constructs a new {@link TagIdentity} instance.
+     * @param tagId The tag ID.
+     * @param tag The tag.
+     * @return The new {@link TagIdentity} instance.
+     */
+    @Nonnull
+    static TagIdentity of(long tagId,
+                          @Nonnull Tag tag) {
+        return TagIdentityTuple.of(tagId, tag);
+    }
+}
