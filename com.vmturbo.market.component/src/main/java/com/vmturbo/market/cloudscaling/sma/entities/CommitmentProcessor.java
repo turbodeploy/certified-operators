d8aafa0597f7cc170cb0442a57b4df14ab16b440
Professor Git <builder@turbonomic.com>
2022-10-17 19:56:23 -0400

    Reverted from xl-staging-ci-build-2528 and reopened jira issues (OM-91366 ) and reverted commits(f4a1ff36ac3307595d889d3c147f9b1b1be08a69 715a33d57a54857e78d2b75f59530668f65c9d52 )

--- com.vmturbo.market.component/src/main/java/com/vmturbo/market/cloudscaling/sma/entities/CommitmentProcessor.java
+++ com.vmturbo.market.component/src/main/java/com/vmturbo/market/cloudscaling/sma/entities/CommitmentProcessor.java
@@ -19,7 +19,6 @@ import com.vmturbo.common.protobuf.topology.TopologyDTO.TypeSpecificInfo.CloudCo
 import com.vmturbo.market.cloudscaling.sma.analysis.SMAUtils;
 import com.vmturbo.market.topology.conversions.ReservedInstanceKey;
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.CloudCommitmentData.CloudCommitmentScope;
-import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.CloudCommitmentData.CloudCommitmentStatus;
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.CloudCommitmentData.FamilyRestricted;
 import com.vmturbo.platform.sdk.common.CloudCostDTO.OSType;
 import com.vmturbo.platform.sdk.common.CloudCostDTO.Tenancy;
@@ -68,10 +67,6 @@ public class CommitmentProcessor {
 
         if (!familyRestricted.hasInstanceFamily()) {
             logger.info("Skip commitment {} with unsupported scope", commitmentId);
-            return Optional.empty();
-        } else if (cloudCommitmentData.getCommitmentStatus() != CloudCommitmentStatus.CLOUD_COMMITMENT_STATUS_ACTIVE) {
-            logger.info("Skipping commitment {} with non-active status {}",
-                    commitmentId, cloudCommitmentData.getCommitmentStatus());
             return Optional.empty();
         }
 
--- com.vmturbo.market.component/src/test/java/com/vmturbo/market/cloudscaling/sma/entities/CommitmentProcessorTest.java
+++ com.vmturbo.market.component/src/test/java/com/vmturbo/market/cloudscaling/sma/entities/CommitmentProcessorTest.java
@@ -1,7 +1,5 @@
 package com.vmturbo.market.cloudscaling.sma.entities;
 
-import static org.junit.Assert.assertFalse;
-
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.BiFunction;
@@ -22,7 +20,6 @@ import com.vmturbo.common.protobuf.topology.TopologyDTO.TypeSpecificInfo.CloudCo
 import com.vmturbo.market.cloudscaling.sma.analysis.SMAUtils;
 import com.vmturbo.market.topology.conversions.ReservedInstanceKey;
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.CloudCommitmentData.CloudCommitmentScope;
-import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.CloudCommitmentData.CloudCommitmentStatus;
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.CloudCommitmentData.FamilyRestricted;
 import com.vmturbo.platform.common.dto.CommonDTO.EntityDTO.EntityType;
 import com.vmturbo.platform.sdk.common.util.Pair;
@@ -42,13 +39,12 @@ public class CommitmentProcessorTest {
     private static final long KEY = 5L;
     private static final long COMMITMENT_ID = 6L;
     private static final String COMMITMENT_DISPLAY_NAME = "Commitment Name";
-    private static final TopologyEntityDTO ACTIVE_COMMITMENT = TopologyEntityDTO.newBuilder()
+    private static final TopologyEntityDTO COMMITMENT = TopologyEntityDTO.newBuilder()
             .setEntityType(EntityType.CLOUD_COMMITMENT_VALUE)
             .setDisplayName(COMMITMENT_DISPLAY_NAME)
             .setOid(COMMITMENT_ID)
             .setTypeSpecificInfo(TypeSpecificInfo.newBuilder()
                     .setCloudCommitmentData(CloudCommitmentInfo.newBuilder()
-                            .setCommitmentStatus(CloudCommitmentStatus.CLOUD_COMMITMENT_STATUS_ACTIVE)
                             .setFamilyRestricted(
                                     FamilyRestricted.newBuilder().setInstanceFamily("N2"))
                             .setCommitmentScope(
@@ -60,8 +56,39 @@ public class CommitmentProcessorTest {
      */
     @Test
     public void testProcess() {
+        final LongUnaryOperator regionByCommitmentGetter = Mockito.mock(LongUnaryOperator.class);
+        final LongFunction<Optional<Long>> zoneByEntityGetter = Mockito.mock(LongFunction.class);
+        final LongFunction<SMACSP> cspByRegionResolver = Mockito.mock(LongFunction.class);
+        final LongUnaryOperator businessAccountByCommitmentGetter = Mockito.mock(
+                LongUnaryOperator.class);
+        final LongUnaryOperator billingFamilyByAccountGetter = Mockito.mock(
+                LongUnaryOperator.class);
+        final Function<String, Optional<SMATemplate>>
+                templateByFamilyAndContextGetter = Mockito.mock(Function.class);
+        final LongFunction<Optional<Set<Long>>> coveredAccountsByCommitmentResolver = Mockito.mock(
+                LongFunction.class);
+        final BiFunction<ReservedInstanceKey, Long, Long> reservedInstanceKeyIDGenerator =
+                Mockito.mock(BiFunction.class);
 
-        final Optional<Pair<SMAContext, SMAReservedInstance>> result = processCommitment(ACTIVE_COMMITMENT);
+        Mockito.when(regionByCommitmentGetter.applyAsLong(COMMITMENT_ID)).thenReturn(REGION_ID);
+        Mockito.when(zoneByEntityGetter.apply(COMMITMENT_ID)).thenReturn(Optional.of(ZONE_ID));
+        Mockito.when(cspByRegionResolver.apply(REGION_ID)).thenReturn(CSP);
+        Mockito.when(businessAccountByCommitmentGetter.applyAsLong(COMMITMENT_ID)).thenReturn(
+                BUSINESS_ACCOUNT_ID);
+        Mockito.when(billingFamilyByAccountGetter.applyAsLong(BUSINESS_ACCOUNT_ID)).thenReturn(
+                BILLING_FAMILY_ID);
+        Mockito.when(templateByFamilyAndContextGetter.apply(Mockito.eq("N2"))).thenReturn(Optional.of(TEMPLATE));
+        Mockito.when(coveredAccountsByCommitmentResolver.apply(COMMITMENT_ID)).thenReturn(
+                Optional.of(COVERED_ACCOUNTS));
+        Mockito.when(reservedInstanceKeyIDGenerator.apply(Mockito.any(ReservedInstanceKey.class),
+                Mockito.eq(COMMITMENT_ID))).thenReturn(KEY);
+
+        final Optional<Pair<SMAContext, SMAReservedInstance>> result =
+                CommitmentProcessor.processCommitment(regionByCommitmentGetter, zoneByEntityGetter,
+                        cspByRegionResolver, businessAccountByCommitmentGetter,
+                        billingFamilyByAccountGetter, templateByFamilyAndContextGetter,
+                        coveredAccountsByCommitmentResolver, reservedInstanceKeyIDGenerator,
+                        TopologyCommitmentData.builder().commitment(COMMITMENT).build());
 
         Assert.assertTrue(result.isPresent());
         final Pair<SMAContext, SMAReservedInstance> pair = result.get();
@@ -77,63 +104,8 @@ public class CommitmentProcessorTest {
         Assert.assertSame(COVERED_ACCOUNTS, instance.getApplicableBusinessAccounts());
         Assert.assertSame(TEMPLATE, instance.getTemplate());
         Assert.assertTrue(instance.isIsf());
-        assertFalse(instance.isShared());
+        Assert.assertFalse(instance.isShared());
         Assert.assertTrue(instance.isPlatformFlexible());
         Assert.assertEquals(ZONE_ID, instance.getZoneId());
-    }
-
-    /**
-     * Test that an inactive commitment is skipped.
-     */
-    @Test
-    public void testInactiveCommitment() {
-
-        final TopologyEntityDTO inactiveCommitment = ACTIVE_COMMITMENT.toBuilder()
-                .setTypeSpecificInfo(ACTIVE_COMMITMENT.getTypeSpecificInfo()
-                        .toBuilder()
-                        .setCloudCommitmentData(ACTIVE_COMMITMENT.getTypeSpecificInfo().getCloudCommitmentData()
-                                .toBuilder()
-                                .setCommitmentStatus(CloudCommitmentStatus.CLOUD_COMMITMENT_STATUS_UNKNOWN)))
-                .build();
-
-        final Optional<Pair<SMAContext, SMAReservedInstance>> result = processCommitment(inactiveCommitment);
-
-        assertFalse(result.isPresent());
-    }
-
-    private Optional<Pair<SMAContext, SMAReservedInstance>> processCommitment(TopologyEntityDTO commitment) {
-
-        final LongUnaryOperator regionByCommitmentGetter = Mockito.mock(LongUnaryOperator.class);
-        final LongFunction<Optional<Long>> zoneByEntityGetter = Mockito.mock(LongFunction.class);
-        final LongFunction<SMACSP> cspByRegionResolver = Mockito.mock(LongFunction.class);
-        final LongUnaryOperator businessAccountByCommitmentGetter = Mockito.mock(
-                LongUnaryOperator.class);
-        final LongUnaryOperator billingFamilyByAccountGetter = Mockito.mock(
-                LongUnaryOperator.class);
-        final Function<String, Optional<SMATemplate>>
-                templateByFamilyAndContextGetter = Mockito.mock(Function.class);
-        final LongFunction<Optional<Set<Long>>> coveredAccountsByCommitmentResolver = Mockito.mock(
-                LongFunction.class);
-        final BiFunction<ReservedInstanceKey, Long, Long> reservedInstanceKeyIDGenerator =
-                Mockito.mock(BiFunction.class);
-
-        Mockito.when(regionByCommitmentGetter.applyAsLong(commitment.getOid())).thenReturn(REGION_ID);
-        Mockito.when(zoneByEntityGetter.apply(commitment.getOid())).thenReturn(Optional.of(ZONE_ID));
-        Mockito.when(cspByRegionResolver.apply(REGION_ID)).thenReturn(CSP);
-        Mockito.when(businessAccountByCommitmentGetter.applyAsLong(commitment.getOid())).thenReturn(
-                BUSINESS_ACCOUNT_ID);
-        Mockito.when(billingFamilyByAccountGetter.applyAsLong(BUSINESS_ACCOUNT_ID)).thenReturn(
-                BILLING_FAMILY_ID);
-        Mockito.when(templateByFamilyAndContextGetter.apply(Mockito.eq("N2"))).thenReturn(Optional.of(TEMPLATE));
-        Mockito.when(coveredAccountsByCommitmentResolver.apply(commitment.getOid())).thenReturn(
-                Optional.of(COVERED_ACCOUNTS));
-        Mockito.when(reservedInstanceKeyIDGenerator.apply(Mockito.any(ReservedInstanceKey.class),
-                Mockito.eq(commitment.getOid()))).thenReturn(KEY);
-
-        return CommitmentProcessor.processCommitment(regionByCommitmentGetter, zoneByEntityGetter,
-                        cspByRegionResolver, businessAccountByCommitmentGetter,
-                        billingFamilyByAccountGetter, templateByFamilyAndContextGetter,
-                        coveredAccountsByCommitmentResolver, reservedInstanceKeyIDGenerator,
-                        TopologyCommitmentData.builder().commitment(commitment).build());
     }
 }
