4535c7e806b9edcdef1fac07b2bc8da5292c7f17
Dmitry Glushkov <dmitry.glushkov@ibm.com>
2022-12-08 08:35:07 -0500

    [OM-80346] Add APPLICATION_SERVER to the stitching scope for the type CUSTOM

--- com.vmturbo.topology.processor/src/main/java/com/vmturbo/topology/processor/probes/ProbeStitchingDependencyTracker.java
+++ com.vmturbo.topology.processor/src/main/java/com/vmturbo/topology/processor/probes/ProbeStitchingDependencyTracker.java
@@ -182,6 +182,7 @@ public class ProbeStitchingDependencyTracker {
                 .requireThatProbeCategory(ProbeCategory.BILLING).stitchAfter(ProbeCategory.CLOUD_MANAGEMENT)
                 .requireThatProbeCategory(ProbeCategory.HYPERCONVERGED).stitchAfter(ProbeCategory.HYPERVISOR)
                 .requireThatProbeCategory(ProbeCategory.CUSTOM).stitchAfter(ProbeCategory.GUEST_OS_PROCESSES)
+                .requireThatProbeCategory(ProbeCategory.CUSTOM).stitchAfter(ProbeCategory.APPLICATION_SERVER)
                 // probe type ordering, we only support the probe types comparison if they are
                 // belonged to same category.
                 .requireThatProbeType(SDKProbeType.CLOUD_FOUNDRY).stitchAfter(SDKProbeType.PIVOTAL_OPSMAN)
